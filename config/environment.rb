72164949fdbaf722f41f6bfeb925b2e107394a96
“Smudge” <originalsmudge@gmail.com>
2009-06-01 16:08:15 -0400

    comment about my apache setup

--- config/environment.rb
+++ config/environment.rb
@@ -11,7 +11,9 @@ RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  #THIS IS FOR NATHAN'S APACHE SETUP (comment it out):
   config.action_controller.relative_url_root = "/newstc"
+
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
