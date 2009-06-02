9ebcf0db1a7d17c49c8c074404096f9c8689f601
“Smudge” <originalsmudge@gmail.com>
2009-06-02 18:49:19 -0400

    Added rudimentary department chooser. I disabled it becuase of the way session data is handled. Once we fix the global handling of departments, we can fix the department chooser

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,6 +2,7 @@ class Role < ActiveRecord::Base
   has_and_belongs_to_many :departments
   has_and_belongs_to_many :permissions
   has_and_belongs_to_many :users
+  validates_presence_of :name
   validates_uniqueness_of :name
   validate :must_belong_to_department
 
