e9781f25475b6ada089962d61e5b91d19abf125d
ckildow <cassandra.kildow@yale.edu>
2009-06-03 15:51:43 -0400

    some lingering merging that needed doing

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -6,11 +6,3 @@ Given /^I have a user named (.+), netid (.+)$/ do |name, netid|
   User.create!(:name => name, :netid => netid)
 end
 
-Given /^I have no (.+)$/ do |class_name|
-  class_name.classify.constantize.delete_all
-end
-
-Then /^I should have ([0-9]+) (.+)$/ do |count, class_name|
-  class_name.classify.constantize.count.should == count.to_i
-end
-
