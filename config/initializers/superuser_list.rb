4881756096b88f13e77d944b34e701b67bf82053
Smudge <originalsmudge@gmail.com>
2009-06-03 17:56:36 -0400

    Added myself as superuser. Gave myself a role. Deleted data.yml.

--- config/initializers/superuser_list.rb
+++ config/initializers/superuser_list.rb
@@ -1,3 +1,5 @@
 # insert netids of superusers here, note that only space is needed
-SUPERUSER_LIST = %w{dtt22 alb64}
+SUPERUSER_LIST = %w{dtt22 alb64 njg24}
+
+# this should be fixed later, so that superusers can be changed in the app itself
 
