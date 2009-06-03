e5483d75aad6dbce7c3cd60a7a4bfc11686b8798
Nathan Griffith <smudge@smudge-laptop.(none)>
2009-06-03 12:17:59 -0400

    some updates

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,8 +1,11 @@
 class Department < ActiveRecord::Base
   has_many :loc_groups
   belongs_to :permission
-  before_validation_on_create :create_user_admin_permission
-  before_validation_on_update :update_user_admin_permission
+  belongs_to :deactivated_permission,
+              :class_name => "Permission",
+              :foreign_key => "deactive_perm_id"
+  before_validation_on_create :create_permissions
+  before_validation_on_update :update_permissions
   validates_uniqueness_of :name
   validates_uniqueness_of :permission_id
 
@@ -10,13 +13,15 @@ class Department < ActiveRecord::Base
   has_and_belongs_to_many :roles
 
   private
-  def create_user_admin_permission
+  def create_permissions
     self.create_permission(:name => name + " user admin")
+    self.create_deactivated_permission(:name => "Deactivated in " + name)
   end
 
-  # in case department name is changed, should update accordingly
-  def update_user_admin_permission
+  # in case department name is changed, should update permissions accordingly
+  def update_permissions
     self.permission.update_attribute(:name, name + " user admin")
+    self.deactivated_permission.update_attribute(:name,"Deactivated in " + name)
   end
 
 end
--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,6 +1,7 @@
 class Permission < ActiveRecord::Base
   validates_uniqueness_of :name
-  has_one :department
+  has_one :department, :dependent => :destroy
+  has_one :department, :foreign_key => 'deactive_perm_id', :dependent => :destroy
   #TODO: add associations with loc group here
 end
 
