366468b7d880513454aff2b13d9123855cf9c511
Smudge <originalsmudge@gmail.com>
2009-06-03 20:27:08 -0400

    more locations work. hope nothing broke.

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -8,7 +8,7 @@ class LocationsController < ApplicationController
   end
 
   def new
-    @location = @department.locations.build
+    @location = Location.new
   end
 
   def create
--- app/models/location.rb
+++ app/models/location.rb
@@ -2,7 +2,7 @@ class Location < ActiveRecord::Base
 
   belongs_to :loc_group
 
-  def department #oh poo. belongs_to :through is coming down the channel in edge rails, but not here yet...
+  def department #Sad face -- "belongs_to :through" is coming down the channel in edge rails, but not here yet...
     loc_group.department
   end
 
