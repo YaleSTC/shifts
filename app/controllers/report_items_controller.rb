02bfb2f7ab5887bdef276511d71de2a770374c30
Smudge <originalsmudge@gmail.com>
2009-06-04 15:50:32 -0400

    added shift, report, and report_item models, and re-did departments_users migration in an attempt to fix the deactivated field

---
+++ app/controllers/report_items_controller.rb
@@ -0,0 +1,44 @@
+class ReportItemsController < ApplicationController
+  def index
+    @report_items = ReportItem.all
+  end
+  
+  def show
+    @report_item = ReportItem.find(params[:id])
+  end
+  
+  def new
+    @report_item = ReportItem.new
+  end
+  
+  def create
+    @report_item = ReportItem.new(params[:report_item])
+    if @report_item.save
+      flash[:notice] = "Successfully created reportitem."
+      redirect_to @report_item
+    else
+      render :action => 'new'
+    end
+  end
+  
+  def edit
+    @report_item = ReportItem.find(params[:id])
+  end
+  
+  def update
+    @report_item = ReportItem.find(params[:id])
+    if @report_item.update_attributes(params[:report_item])
+      flash[:notice] = "Successfully updated reportitem."
+      redirect_to @report_item
+    else
+      render :action => 'edit'
+    end
+  end
+  
+  def destroy
+    @report_item = ReportItem.find(params[:id])
+    @report_item.destroy
+    flash[:notice] = "Successfully destroyed reportitem."
+    redirect_to report_items_url
+  end
+end
