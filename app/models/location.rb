042781ef22bea1b0672852a96d54312ba1c46c9e
rofreg <rofreg@gmail.com>
2009-06-04 14:17:01 -0400

    role names are now unique within a department, and the display page for individual roles lists permissions

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,9 +1,23 @@
 class Location < ActiveRecord::Base
   belongs_to :loc_group
+  
   validates_presence_of :loc_group
+  validates_presence_of :name
+  validates_presence_of :short_name
+  validates_numericality_of :max_staff
+  validates_numericality_of :min_staff
+  validates_numericality_of :priority
+  
+  validates_uniqueness_of :name, :scope => :loc_group_id
+  validates_uniqueness_of :short_name, :scope => :loc_group_id
+  validate :max_staff_greater_than_min_staff
 
   def department #Sad face -- "belongs_to :through" is coming down the channel in edge rails, but not here yet...
     loc_group.department
+  end
+  
+  def max_staff_greater_than_min_staff
+    errors.add("The minimum number of staff cannot be larger than the maximum.", "") if (self.min_staff > self.max_staff)
   end
 end
 
--- app/models/role.rb
+++ app/models/role.rb
@@ -4,13 +4,18 @@ class Role < ActiveRecord::Base
   has_and_belongs_to_many :users
   validates_presence_of :name
   validate :must_belong_to_department
-
-  # FIXME: only role of user admin permission can belong to more than one department. should user_admin role and loc_group roles be separated?
+  validate :must_have_unique_name_in_dept #can't use scope because of habtm relationship :(
+  
+  # FIXME: only role of user admin permission can belong to more than one department.
+  # should user_admin role and loc_group roles be separated?
   def must_belong_to_department
     errors.add("Role must belong to a department.", "") if self.departments.empty?
   end
   
   def must_have_unique_name_in_dept
-    errors.add("Role must have a unique name within department.", "") if find(:first, :conditions => {:name => self.name, :department => self.department})
+    #get a list of roles in the same department as this role, excluding this role
+    associated_roles = self.departments.collect{|dept| dept.roles}.flatten - [self]
+    
+    errors.add("Name must be unique in a department.", "") unless associated_roles.select{ |role| role.name == self.name }.empty?
   end
 end
