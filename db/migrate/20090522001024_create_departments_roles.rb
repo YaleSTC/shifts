42a83e4654957fe878a16cd1cb94f2e9ad991df2
Smudge <originalsmudge@gmail.com>
2009-06-04 16:05:09 -0400

    join table migrations are cleaner

--- db/migrate/20090522001024_create_departments_roles.rb
+++ db/migrate/20090522001024_create_departments_roles.rb
@@ -1,15 +1,13 @@
 class CreateDepartmentsRoles < ActiveRecord::Migration
   def self.up
-    create_table "departments_roles", :id => false, :force => true do |t|
-      t.integer "department_id", :limit => 10, :default => 0, :null => false
-      t.integer "role_id",       :limit => 10, :default => 0, :null => false
+    create_table :departments_roles, :id => false do |t|
+      t.references :department
+      t.references :role
     end
-
-    add_index "departments_roles", ["department_id", "role_id"], :name => "departments_roles_FKIndex1", :unique => true
-    add_index "departments_roles", ["role_id"], :name => "departments_roles_FKIndex2"
   end
 
   def self.down
     drop_table :departments_roles
   end
 end
+
