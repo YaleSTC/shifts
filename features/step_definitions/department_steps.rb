132fa29ce128e3ecf8818d60ffe83e2d4e01f7d3
ckildow <cassandra.kildow@yale.edu>
2009-06-05 14:53:32 -0400

    RSpec: setting up testing for User model

--- features/step_definitions/department_steps.rb
+++ features/step_definitions/department_steps.rb
@@ -1,5 +1,8 @@
 Given /^I have a user named "([^\"]*)" in the department "([^\"]*)" with netid "([^\"]*)"$/ do |name, department, netid|
   @department = department
   @netid = netid
+  a = User.new(:name => name, :netid => netid)
+  a.departments << Department.find_by_name(department)
+  a.save
 end
 
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+module User
+  describe "Validation" do
+    it "should have a name" do
+    end
+
+    it "should have a netid" do
+    end
+
+    it "should have a unique netid" do
+    end
+
+    it "should have a department" do
+    end
+
+  end
+
