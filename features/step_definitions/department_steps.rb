e28e285bdc807c41aa10b4dedaa618bc441df9d6
Wei Yan <wei@weilaptop.(none)>
2009-06-05 16:45:03 -0400

    changed another instance of netid to login

--- features/step_definitions/department_steps.rb
+++ features/step_definitions/department_steps.rb
@@ -1,5 +1,5 @@
-Given /^I have a user named "([^\"]*)" in the department "([^\"]*)" with login "([^\"]*)"$/ do |name, department, netid|
-  user = User.new(:netid => netid, :name => name)
+Given /^I have a user named "([^\"]*)" in the department "([^\"]*)" with login "([^\"]*)"$/ do |name, department, login|
+  user = User.new(:login => login, :name => name)
   user.departments << Department.find_by_name(department)
   user.save!
 end
