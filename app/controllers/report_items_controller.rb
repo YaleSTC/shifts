d464c7797e52d83c9e685245dfe23c58e2f1c8fa
Michael Libertin <michael.libertin@yale.edu>
2009-06-05 16:38:46 -0400

    Got some very preliminary report item stuff working

--- app/controllers/report_items_controller.rb
+++ app/controllers/report_items_controller.rb
@@ -2,17 +2,19 @@ class ReportItemsController < ApplicationController
   def index
     @report_items = ReportItem.all
   end
-  
+
   def show
     @report_item = ReportItem.find(params[:id])
   end
-  
+
   def new
     @report_item = ReportItem.new
   end
-  
+
   def create
     @report_item = ReportItem.new(params[:report_item])
+    @report_item.time = Time.now
+    @report_item.ip_address = request.remote_ip
     if @report_item.save
       flash[:notice] = "Successfully created reportitem."
       redirect_to @report_item
@@ -20,11 +22,11 @@ class ReportItemsController < ApplicationController
       render :action => 'new'
     end
   end
-  
+
   def edit
     @report_item = ReportItem.find(params[:id])
   end
-  
+
   def update
     @report_item = ReportItem.find(params[:id])
     if @report_item.update_attributes(params[:report_item])
@@ -34,7 +36,7 @@ class ReportItemsController < ApplicationController
       render :action => 'edit'
     end
   end
-  
+
   def destroy
     @report_item = ReportItem.find(params[:id])
     @report_item.destroy
