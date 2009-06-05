701d70d5d38967c82f541cee978b891290e5b4ad
Smudge <originalsmudge@gmail.com>
2009-06-05 15:02:01 -0400

    modified payform_items in db

--- db/migrate/20090605182951_create_payform_items.rb
+++ db/migrate/20090605182951_create_payform_items.rb
@@ -1,17 +1,27 @@
 class CreatePayformItems < ActiveRecord::Migration
   def self.up
     create_table :payform_items do |t|
-      t.boolean :deleted
-      t.decimal :hours
-      t.references :user
-      t.string :source
-      t.text :escription
       t.references :category
+      t.references :user
+      t.references :payform_item #if this is an edit, the parent is the more recent version
+
+      t.boolean :active, :default => true
+      t.decimal :hours
+
+
+      t.text :description
+      t.text :reason #reason for deletion or edit
+      t.string :source #where it came from (shifts, rt, admin: adam, mass_job, etc)
+
+      t.datetime "submitted"
+      t.datetime "approved"
+      t.datetime "printed"
       t.timestamps
     end
   end
-  
+
   def self.down
     drop_table :payform_items
   end
 end
+
