fd52051ee17c72525d13a12d2370531a059df98e
Bob Qu <bob.qu.md@gmail.com>
2009-06-05 15:36:42 -0400

    Merged master and bwac branches.

--- config/initializers/superuser_list.rb
+++ config/initializers/superuser_list.rb
@@ -1,4 +1,4 @@
 # insert netids of superusers here, note that only space is needed
-SUPERUSER_LIST = %w{dtt22 alb64 rjl39 njg24}
+SUPERUSER_LIST = %w{dtt22 alb64 rjl39 njg24 bq9}
 
 # this should be fixed later, so that superusers can be changed in the app itself
