67c0c8ce8f56d0fd5f0bf5e1d519accf6d1a14a9
Michael Libertin <michael.libertin@yale.edu>
2009-06-06 20:41:44 -0400

    Ok, so I basically just made it so we never need to interact w/ ReportItems directly. When you submit a reportitem, it redirects you back to your report now. As such, I got rid of the ReportItems link in the navbar....

--- app/controllers/report_items_controller.rb
+++ app/controllers/report_items_controller.rb
@@ -18,7 +18,7 @@ class ReportItemsController < ApplicationController
     @report_item.report = params[:report_id] ? Report.find(params[:report_id]) : Shift.find(params[:shift_id]).report
     if @report_item.save
       flash[:notice] = "Successfully created reportitem."
-      redirect_to @report_item
+      redirect_to Report.find(@report_item[:report_id])
     else
       render :action => 'new'
     end
