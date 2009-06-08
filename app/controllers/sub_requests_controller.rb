10baf6a3171175fe2d9d24f8f92e760cf77fe760
Michael Libertin <michael.libertin@yale.edu>
2009-06-08 18:33:42 -0400

    Committing changes that resulted from new sub_requests scaffold, as well as fixture stuff. Not working exactly how  I want, but it is something

--- app/controllers/sub_requests_controller.rb
+++ app/controllers/sub_requests_controller.rb
@@ -1,44 +1,86 @@
 class SubRequestsController < ApplicationController
+  # GET /sub_requests
+  # GET /sub_requests.xml
   def index
     @sub_requests = SubRequest.all
-  end
-  
-  def show
-    @sub_request = SubRequest.find(params[:id])
-  end
-  
-  def new
-    @sub_request = SubRequest.new
-  end
-  
-  def create
-    @sub_request = SubRequest.new(params[:sub_request])
-    if @sub_request.save
-      flash[:notice] = "Successfully created subrequest."
-      redirect_to @sub_request
-    else
-      render :action => 'new'
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @sub_requests }
     end
   end
-  
+
+  # GET /sub_requests/1
+  # GET /sub_requests/1.xml
+  def show
+    @sub_request = SubRequest.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @sub_request }
+    end
+  end
+
+  # GET /sub_requests/new
+  # GET /sub_requests/new.xml
+  def new
+    @shift = Shift.find(params[:shift_id])
+    @sub_request = SubRequest.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @sub_request }
+    end
+  end
+
+  # GET /sub_requests/1/edit
   def edit
     @sub_request = SubRequest.find(params[:id])
   end
-  
-  def update
-    @sub_request = SubRequest.find(params[:id])
-    if @sub_request.update_attributes(params[:sub_request])
-      flash[:notice] = "Successfully updated subrequest."
-      redirect_to @sub_request
-    else
-      render :action => 'edit'
+
+  # POST /sub_requests
+  # POST /sub_requests.xml
+  def create
+    @sub_request = SubRequest.new(params[:sub_request])
+
+    respond_to do |format|
+      if @sub_request.save
+        flash[:notice] = 'SubRequest was successfully created.'
+        format.html { redirect_to(@sub_request) }
+        format.xml  { render :xml => @sub_request, :status => :created, :location => @sub_request }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @sub_request.errors, :status => :unprocessable_entity }
+      end
     end
   end
-  
+
+  # PUT /sub_requests/1
+  # PUT /sub_requests/1.xml
+  def update
+    @sub_request = SubRequest.find(params[:id])
+
+    respond_to do |format|
+      if @sub_request.update_attributes(params[:sub_request])
+        flash[:notice] = 'SubRequest was successfully updated.'
+        format.html { redirect_to(@sub_request) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @sub_request.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /sub_requests/1
+  # DELETE /sub_requests/1.xml
   def destroy
     @sub_request = SubRequest.find(params[:id])
     @sub_request.destroy
-    flash[:notice] = "Successfully destroyed subrequest."
-    redirect_to sub_requests_url
+
+    respond_to do |format|
+      format.html { redirect_to(sub_requests_url) }
+      format.xml  { head :ok }
+    end
   end
 end
--- app/models/sub_request.rb
+++ app/models/sub_request.rb
@@ -1,2 +1,4 @@
 class SubRequest < ActiveRecord::Base
+  belongs_to :shift
+  validates_presence_of :reason
 end
