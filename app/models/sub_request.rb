2d11b96bbde32a7c644518336ecedc369c23b874
Michael Libertin <michael.libertin@yale.edu>
2009-06-08 19:38:44 -0400

    Have very very super basic sub requests working...

--- app/models/sub_request.rb
+++ app/models/sub_request.rb
@@ -1,4 +1,12 @@
 class SubRequest < ActiveRecord::Base
   belongs_to :shift
   validates_presence_of :reason
+  validate :start_and_end_are_within_shift
+
+  private
+  def start_and_end_are_within_shift
+    unless self.start.between?(self.shift.start, self.shift.end) && self.end.between?(self.shift.start, self.shift.end)
+      errors.add("Sub Request must be within shift.", "")
+    end
+  end
 end
