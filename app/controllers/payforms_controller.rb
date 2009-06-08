e3055321b29fc2d572bde7de399edaa7ceb55c81
Smudge <originalsmudge@gmail.com>
2009-06-08 19:48:11 -0400

    Handled the way payforms are viewed. Also added a comment to "notices" in layout when I realized it was handled the way I wanted it to be handled, but without the option to fade it out.

--- app/controllers/payforms_controller.rb
+++ app/controllers/payforms_controller.rb
@@ -1,10 +1,14 @@
 class PayformsController < ApplicationController
   def index
-    redirect_to Payform.current(current_department, current_user)
+    @payforms = Payform.find(:all)
   end
 
   def show
     @payform = Payform.find(params[:id])
+  end
+
+  def new
+    redirect_to Payform.current(current_department, current_user)
   end
 
 end
