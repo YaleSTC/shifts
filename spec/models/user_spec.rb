66f0d3deba1f236dc97b633d53584b386f4f39ba
ckildow <cassandra.kildow@yale.edu>
2009-06-07 22:25:20 -0400

    RSpec testing for Users started

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,18 +1,28 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-module User
-  describe "Validation" do
+describe User do
+  before(:each) do
+# the commented bits below don't work yet. -cmk
+#    Department.new(:name => "STC")
+    @user = User.new(:name => "Wee Willy", :login => "ww")
+#    @user.departments << Department.find_by_name("STC")
+#    @user.save!
+  end
     it "should have a name" do
+      @user.name.should == "Wee Willy"
     end
 
-    it "should have a netid" do
+    it "should have a login" do
+      @user.login.should == "ww"
     end
 
-    it "should have a unique netid" do
+    it "should have a unique login" do
+
     end
 
     it "should have a department" do
+#      @user.departments == "STC"
     end
 
-  end
+end
 
