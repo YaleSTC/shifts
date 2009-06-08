43c68903c1bd00a8728a4761512c2ade1cb45e32
Smudge <originalsmudge@gmail.com>
2009-06-08 14:11:40 -0400

    payform period information added

--- app/models/payform.rb
+++ app/models/payform.rb
@@ -1,2 +1,13 @@
 class Payform < ActiveRecord::Base
+
+  has_many :payform_items
+  belongs_to :department
+  belongs_to :user
+
+  def current(current_department, current_user)
+    Payform.find_by_w
+  end
+
+
 end
+
--- db/migrate/20090605203853_create_payforms.rb
+++ db/migrate/20090605203853_create_payforms.rb
@@ -2,9 +2,16 @@ class CreatePayforms < ActiveRecord::Migration
   def self.up
     create_table :payforms do |t|
       t.datetime :date
+
+      #PAYFORM PERIOD INFO
       t.boolean  :monthly, :default => false #defaults to WEEKLY
       t.boolean  :type, :default => true #where false = bi-weekly or semi-monthly, and true = weekly or monthly
+      t.integer  :day #if it is weekly, this stores the day of the week, and if this is monthly, day of the month
       t.datetime :printed #date and time printed
+
+      t.references :department
+      t.references :user
+
       t.timestamps
     end
   end
