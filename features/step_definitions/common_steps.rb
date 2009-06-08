ba8b075a0c2369a44771a16b04c0adb35422dcfa
ckildow <cassandra.kildow@yale.edu>
2009-06-08 14:25:18 -0400

    more cucumber work

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -9,3 +9,14 @@ end
 When /^I choose to view (.+)$/ do |department|
   visit path_to(department_users_path(@department))
 end
+
+Given /^that CAS is happy$/ do
+#  request_login_ticket
+#  Department.create!(:name => "test-ground")
+#  @current_user = User.new(:login => "studcomp", :name => "Test")
+#  @current_user.departments << Department.find_by_name("test-ground")
+#  @current_user.is_superuser? == true
+#  @current_user.save!
+#  User.find_by_login(session[:cas_user])
+end
+
--- features/step_definitions/department_steps.rb
+++ features/step_definitions/department_steps.rb
@@ -3,3 +3,16 @@ Given /^I have a user named "([^\"]*)" in the department "([^\"]*)" with login "
   user.departments << Department.find_by_name(department)
   user.save!
 end
+
+Given /^that I am a superuser$/ do
+  @current_user = User.find_by_login("studcomp")
+end
+
+
+#  Department.new(:name => "testing ground")
+#  user = @current_user
+#  user = User.new(:login => "studcomp", :name => "test")
+#  user.departments << Department.find_by_name("testing ground")
+#  user.save!
+#  user.is_superuser? == true
+
