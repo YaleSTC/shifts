8c277c043ce44541a92aa103adff46305aee1eea
Wei Yan <wei@weilaptop.(none)>
2009-06-09 01:20:04 -0400

    added one more payform test

--- features/step_definitions/payform_steps.rb
+++ features/step_definitions/payform_steps.rb
@@ -1,4 +1,4 @@
-Given /^I am user "([^\"]*)"$/ do |login, dept_string|
+Given /^I am user "([^\"]*)"$/ do |login|
   @user = User.find_by_login(login)
 end
 
@@ -12,6 +12,13 @@ Given /^I have a payform for the week "([^\"]*)"$/ do |week|
   date = week.to_date
   @payform = Payform.new(:date => date,
                          :user_id => @user.id,
-                         :department_id => @user.departments[1].id)
+                         :department_id => @user.departments[0].id)
+end
+
+Given /^I have the following "([^\"]*)"$/ do |object, table|
+  object_class = object.classify.constantize
+  table.hashes.each do |hash|
+    object_class.new(hash)
+  end
 end
 
