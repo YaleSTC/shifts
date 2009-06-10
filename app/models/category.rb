0a4482a6bab2fc891f29763cf28edc58a02e0659
Smudge <originalsmudge@gmail.com>
2009-06-10 18:02:00 -0400

    Worked more on payform views. Started importing old code.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
 class Category < ActiveRecord::Base
+  belongs_to :department
+  has_many :payform_items
+
 end
+
--- db/migrate/20090522000934_create_departments.rb
+++ db/migrate/20090522000934_create_departments.rb
@@ -8,7 +8,7 @@ class CreateDepartments < ActiveRecord::Migration
       #=========================
       t.boolean  :monthly, :default => false #defaults to WEEKLY
       t.boolean  :complex, :default => true #where false = bi-weekly or semi-monthly, and true = weekly or monthly
-      t.integer  :day, :default => 0 #if it is weekly, this stores the day of the week, and if this is monthly, day of the month
+      t.integer  :day, :default => 6 #if it is weekly, this stores the day of the week, and if this is monthly, day of the month
       t.integer  :day2, :default => 15  #if it is semi-monthly, this stores the second day of the month. (not used otherwise)
       #=========================
 
