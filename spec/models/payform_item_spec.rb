20011401760d3d9cd714d4cc41a09dec018e8c6c
Wei Yan <wei@weilaptop.(none)>
2009-06-09 22:37:14 -0400

    wrote specs for PayformItem and Payform

--- spec/models/payform_item_spec.rb
+++ spec/models/payform_item_spec.rb
@@ -1,7 +1,37 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe PayformItem do
-  it "should be valid" do
-    PayformItem.new.should be_valid
+module PayformItemHelper
+  def valid_payform_item_attributes
+    { :category_id => 1,
+      :user_id => 1,
+      :hours => 3,
+      :description => "Fun"
+    }
   end
 end
+
+describe PayformItem do
+  describe "when newly created" do
+    include PayformItemHelper
+
+    before(:each) do
+      @payform_item = PayformItem.new
+    end
+
+    it "should be valid with valid attributes" do
+      @payform_item.attributes = valid_payform_item_attributes
+      @payform_item.should be_valid
+    end
+
+    [:category_id, :user_id, :hours, :description].each do |attribute|
+      it "should be invalid without #{attribute}" do
+        @payform_item.attributes = valid_payform_item_attributes.except(attribute)
+        @payform_item.should_not be_valid
+      end
+    end
+  end
+
+  describe "already created" do
+  end
+end
+
