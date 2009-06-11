e902d226c8e16d519621c239160e096119bd42b5
Derek Zhao <derekzhao@derekzhao-laptop.(none)>
2009-06-11 14:28:18 -0400

    adding untracked files

---
+++ app/controllers/notices_controller.rb
@@ -0,0 +1,93 @@
+class NoticesController < ApplicationController
+
+  # GET /notices
+  # GET /notices.xml
+  def index
+    @notices = Notice.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @notices }
+    end
+  end
+
+  # GET /notices/1
+  # GET /notices/1.xml
+  def show
+    @notice = Notice.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @notice }
+    end
+  end
+
+  # GET /notices/new
+  # GET /notices/new.xml
+  def new
+    fetch_data
+    @notice = Notice.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @notice }
+    end
+  end
+
+  # GET /notices/1/edit
+  def edit
+    @notice = Notice.find(params[:id])
+  end
+
+  # POST /notices
+  # POST /notices.xml
+  def create
+    @notice = Notice.new(params[:notice])
+
+    respond_to do |format|
+      if @notice.save
+        flash[:notice] = 'Notice was successfully created.'
+        format.html { redirect_to(@notice) }
+        format.xml  { render :xml => @notice, :status => :created, :location => @notice }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @notice.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /notices/1
+  # PUT /notices/1.xml
+  def update
+    @notice = Notice.find(params[:id])
+
+    respond_to do |format|
+      if @notice.update_attributes(params[:notice])
+        flash[:notice] = 'Notice was successfully updated.'
+        format.html { redirect_to(@notice) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @notice.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /notices/1
+  # DELETE /notices/1.xml
+  def destroy
+    @notice = Notice.find(params[:id])
+    @notice.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(notices_url) }
+      format.xml  { head :ok }
+    end
+  end
+
+  def fetch_data
+    @loc_groups = @department.loc_groups.select { |lg| current_user.can_admin?(lg) }
+  end
+
+end
+
