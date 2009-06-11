b0b3d83f27b747142b32ed27f979bf062fda15f7
rofreg <rofreg@gmail.com>
2009-06-11 16:03:36 -0400

    added option to show unscheduled shifts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-
+  def make_popup(hash)
+    hash[:width] ||= 600
+    "Modalbox.show(this.href, {title: '#{hash[:title]}', width: #{hash[:width]}}); return false;"
+  end
 end
 
