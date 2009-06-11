1672c7aa4630bef02bfe8df892decf805b084a6a
ckildow <cassandra.kildow@yale.edu>
2009-06-11 12:57:57 -0400

    Table in cucumber payformadmin test

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -10,9 +10,18 @@ When /^I choose to view (.+)$/ do |department|
   visit path_to(department_users_path(@department))
 end
 
+Given /^I have a user named "([A-Za-z]+)", department "(.+)", login "(.+)"$/ do |name, department, login|
+  d = Department.find_by_name("#{department}") or Department.create!(:name => department)
+
+  u = User.new(:name => name, :login => login)
+  u.departments << Department.find_by_name("#{department}")
+  u.save!
+
+end
+
 Given /^I have a user named (.+), login (.+)$/ do |name, login|
-  Department.create!(:name => )
-  User.create!(:name => name, :login => login, :department =>
+
+  User.create!(:name => name, :login => login)
 end
 
 Given /^that CAS is happy$/ do
--- features/step_definitions/payform_admin_steps.rb
+++ features/step_definitions/payform_admin_steps.rb
@@ -1,19 +1,22 @@
 Given /^I am a payform administrator$/ do
-  @current_user = User.new
   #this needs to be changed once we have payform administrator role/permission
-  @current_user = Permission.all
-end
-
-Given /^I have a user named "([A-Za-z]+)", department "(.+)", login "(.+)"$/ do |name, department, login|
-  d = Department.find_by_name("#{department}") or Department.create!(:name => department)
-  end
-  u = User.new(:name => name, :login => login)
-  u.departments << Department.find_by_name("#{department}")
-  u.save!
-
 end
 
 Given /^I have a category "([^\"]*)"$/ do |category|
   @category = Category.create!(:name => category, :id => 1, :active => true)
+end
+
+
+Given /^I have the following payforms:$/ do |table|
+  table.hashes.each do |row|
+      d = row[:date]
+      u = User.find_by_name(row[:user]).id
+      @department_id = DepartmentsUser.find_by_user_id(u).department_id
+      a = Payform.create!(:date => d , :department_id => @department_id, :user_id => u, :submitted => Time.now)
+    end
+end
+
+Then /^I should see "([^\"]*)" under "([^\"]*)"$/ do |arg1, arg2|
+  pending
 end
 
