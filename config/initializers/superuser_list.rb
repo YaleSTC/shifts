512d3523fed761179cb1b55e301de0e6e2dbc4f6
Derek Zhao <derekzhao@derekzhao-laptop.(none)>
2009-06-11 14:15:28 -0400

    fixing adding notices

--- config/initializers/superuser_list.rb
+++ config/initializers/superuser_list.rb
@@ -1,4 +1,5 @@
 # insert netids of superusers here, note that only space is needed
-SUPERUSER_LIST = %w{dtt22 alb64 rjl39 njg24 mpl36}
+SUPERUSER_LIST = %w{dtt22 alb64 rjl39 njg24 mpl36 dz65}
 
 # this should be fixed later, so that superusers can be changed in the app itself
+
