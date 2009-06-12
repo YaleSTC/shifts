e345af6d38d75d352ea7ff0261ae9635fc71f1fa
Bob Qu <bob.qu.md@gmail.com>
2009-06-12 14:18:03 -0400

    Shifts cucumber test

--- features/step_definitions/shift_steps.rb
+++ features/step_definitions/shift_steps.rb
@@ -1,9 +1,8 @@
-Given /^I am logged into apps with netid "(.+)" in department "(.+)"$/ do |login, department|
-  @current_user =  User.import_from_ldap("login", "department", true)
-end
-
 Given /^I am not logged into a shift report$/ do
-  @current_user.shifts.reports == nil?
+  @current_user = User.find_by_login(session[:cas_user])  
+  @current_user.shifts.each do |n|
+    n.report == nil
+  end      
 end
 
 Given /^I am logged into a shift report$/ do
