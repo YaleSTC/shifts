8a918e1616d495067e35e2c6f2f2d404396bec7e
AJ Espinosa <james2c19v@aj-laptop.(none)>
2009-06-15 11:06:58 -0400

    don't even remember what I changed

--- features/step_definitions/shift_steps.rb
+++ features/step_definitions/shift_steps.rb
@@ -1,8 +1,8 @@
 Given /^I am not logged into a shift report$/ do
-  @current_user = User.find_by_login(session[:cas_user])  
+  @current_user = User.find_by_login(session[:cas_user])
   @current_user.shifts.each do |n|
     n.report == nil
-  end      
+  end
 end
 
 Given /^I am logged into a shift report$/ do
