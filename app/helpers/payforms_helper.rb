e11ee91f6b54bd9bb5501328d2533b404e979954
Smudge <originalsmudge@gmail.com>
2009-06-15 13:52:32 -0400

    payform work

--- app/helpers/payforms_helper.rb
+++ app/helpers/payforms_helper.rb
@@ -2,12 +2,18 @@ module PayformsHelper
 
   def last_10_dates
     payforms = []
+    subtract = (@department.monthly ? 1.month : 1.week)
     date = Date.today
     for i in 0..9
       payforms << Payform.default_period_date(date, @department)
-      date -= 1.week
+      date -= subtract
     end
     payforms
+  end
+  
+  def start_of_period(date)
+    subtract = (@department.monthly ? 1.month : 1.week)
+    date - subtract + 1.day
   end
 
 end
--- db/migrate/20090522000934_create_departments.rb
+++ db/migrate/20090522000934_create_departments.rb
@@ -7,10 +7,11 @@ class CreateDepartments < ActiveRecord::Migration
 
       #Payform Configuration
       #=========================
-      t.boolean  :monthly, :default => false 
+      t.boolean  :monthly, :default => false
       t.boolean  :complex, :default => false
+      t.boolean  :end_of_month, :default => false
       t.integer  :day, :default => 6 #if it is weekly, this stores the day of the week, and if this is monthly, day of the month
-      t.integer  :day2, :default => 15  #if it is semi-monthly, this stores the second day of the month. (not used otherwise)
+      t.integer  :day2, :default => 1  #if it is semi-monthly, this stores the second day of the month. (not used otherwise)
       #=========================
 
       t.timestamps
