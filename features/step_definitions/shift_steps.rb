b8a6b9f41bf83a6137023fc60fd5acd4f83c8148
Bob Qu <bob.qu.md@gmail.com>
2009-06-15 15:55:51 -0400

    Polishing shift login tests

--- features/step_definitions/shift_steps.rb
+++ features/step_definitions/shift_steps.rb
@@ -1,14 +1,17 @@
 Given /^I am not logged into a shift report$/ do
-  @department = @current_user.departments[0]  
   @current_user.shifts.each do |n|
     n.report == nil
   end
 end
 
 Given /^I am logged into a shift report$/ do
-  @department = @current_user.departments[0]
   @current_user.shifts.each do |n|
     n.report == nil?
   end
+end
+
+Then /^my shift report should have ([0-9]+) comment$/ do |count|
+  @shift = @current_user.shifts[0]
+  @shift.report.report_items.count.should == count.to_i
 end
 
