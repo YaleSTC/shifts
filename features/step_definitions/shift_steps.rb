9e2bd037850b9ef2cdf885340adac1719b9bdeb7
Bob Qu <bob.qu.md@gmail.com>
2009-06-15 11:23:15 -0400

    Progress on shift login test, plus database hijinks

--- features/step_definitions/shift_steps.rb
+++ features/step_definitions/shift_steps.rb
@@ -1,12 +1,12 @@
 Given /^I am not logged into a shift report$/ do
-  @current_user = User.find_by_login(session[:cas_user])  
+  @current_user = User.find_by_login("aje29")  
   @current_user.shifts.each do |n|
     n.report == nil
   end      
 end
 
 Given /^I am logged into a shift report$/ do
-  @current_user = User.find_by_login(session[:cas_user])
+  @current_user = User.find_by_login("aje29")
   @current_user.shifts.each do |n|
     n.report == nil?
   end
