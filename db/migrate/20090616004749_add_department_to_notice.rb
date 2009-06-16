219d67b22c43ae8d0e5987f4cbca8b4b5fb25091
Bob Qu <bob.qu.md@gmail.com>
2009-06-16 15:03:47 -0400

    Progress on test, removed redundant migrations

--- db/migrate/20090616004749_add_department_to_notice.rb
+++
@@ -1,9 +0,0 @@
-class AddDepartmentToNotice < ActiveRecord::Migration
-  def self.up
-    add_column :notices, :department, :integer
-  end
-
-  def self.down
-    remove_column :notices, :department
-  end
-end
