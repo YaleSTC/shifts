800e85421bdd6dbee05aa1e5c8f375b1be31a9c1
Derek Zhao <derekzhao@derekzhao-laptop.(none)>
2009-06-17 15:41:59 -0400

    adding un tracked files

---
+++ db/migrate/20090617154250_add_deparment_wide_to_notice.rb
@@ -0,0 +1,9 @@
+class AddDeparmentWideToNotice < ActiveRecord::Migration
+  def self.up
+    add_column :notices, :department_wide, :boolean
+  end
+
+  def self.down
+    remove_column :notices, :department_wide
+  end
+end
