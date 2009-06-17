2e24c4e04da2ba01b35cb454ed8a3e3cadfea4d9
Harley Trung <harley.trung@yale.edu>
2009-06-16 21:02:16 -0700

    pushing some changes i made last week but didnt have the internet to commit till now:     - add the developers to superuser list and to list of users     - use link_to_unless_current method to disable a link when on current page     - some css changes and code comments here and there     - hmm the nav.css doesnt seem to be used anymore right?

--- config/initializers/superuser_list.rb
+++ config/initializers/superuser_list.rb
@@ -1,4 +1,6 @@
-# insert netids of superusers here, note that only space is needed
-SUPERUSER_LIST = %w{dtt22 alb64 rjl39 njg24 mpl36 bq9 aje29}
 
-# this should be fixed later, so that superusers can be changed in the app itself
+# insert netids of superusers here, note that only space is needed
+SUPERUSER_LIST = %w{dtt22 alb64 rjl39 njg24 mpl36 bq9 aje29 cmk49 kaa43 am846 brs35}
+
+# FIXME: this should be fixed later, so that superusers can be changed in the app itself.  H: I think we should just make a superuser attribute in User model
+
