6bf4b83ecf281f78af614f9710d07a336367a05a
Wei Yan <wei@weilaptop.(none)>
2009-06-18 00:35:40 -0400

    added one more user settings test

--- features/step_definitions/settings_steps.rb
+++ features/step_definitions/settings_steps.rb
@@ -1,3 +1,13 @@
+Given /^I have a shift yesterday$/ do
+  Shift.create!(:start => Date.yesterday + " 1PM", :end => Date.yesterday + " 3 PM",
+                :user_id => @user, :location_id => @department.locations.first,
+                :scheduled => true)
+end
+
+Given /^today is not Sunday$/ do
+  Date::DAYNAMES[Date.today.wday].should_not == "Sunday"
+end
+
 Then /^the page should indicate that I am in the department "([^\"]*)"$/ do |department|
 
   # ATTN: the dev team should choose one of the following lines and use it.
@@ -8,5 +18,13 @@ Then /^the page should indicate that I am in the department "([^\"]*)"$/ do |dep
   response.should contain("Current department: " + department)
 
   response.should_not have_selector("a", :content => department)
+end
+
+Then /^I should see all the days of the week$/ do
+#  days_of_week = ["Sunday", "Monday", "Tuesday",
+#                  "Wednesday", "Thursday", "Friday", "Saturday"]
+  Date::DAYNAMES.each do |day|
+    response.should contain(day)
+  end
 end
 
