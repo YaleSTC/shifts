b826c98d5fe1be79dadc6769396ee92125469cd0
Smudge <originalsmudge@gmail.com>
2009-06-18 14:44:51 -0400

    adding validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,19 @@ class Category < ActiveRecord::Base
   belongs_to :department
   has_many :payform_items
 
-  validates_presence_of :department
+  validates_presence_of :name, :department_id
+  
+  named_scope :active,   lambda { |dept_id| { :conditions => ['department_id = ? and active = ?',  dept_id, 1]}}
+  named_scope :disabled, lambda { |dept_id| { :conditions => ['department_id = ? and active = ?', dept_id, 0]}}    
+  
+  
+  protected
+  
+  def validate
+    for category in department.categories
+        errors.add(:category, "name already taken") if name == category.name and id != category.id
+    end
+  end
 
 end
 
--- app/models/payform.rb
+++ app/models/payform.rb
@@ -4,17 +4,25 @@ class Payform < ActiveRecord::Base
   belongs_to :department
   belongs_to :user
   belongs_to :approved_by, :class_name => "User", :foreign_key => "approved_by_id"
+  
+  validates_presence_of :department_id, :user_id
+
+  named_scope :unsubmitted, lambda { |dept_id| {
+    :conditions => ["department_id = ? AND submitted IS ?", dept_id, nil]
+  }}
+  named_scope :unapproved, lambda { |dept_id| {
+    :conditions => ["department_id = ? AND submitted IS NOT ? AND approved IS ?", dept_id, nil, nil]
+  }}
+  named_scope :unprinted, lambda { |dept_id| {
+    :conditions => ["department_id = ? AND approved IS NOT ? AND printed IS ?", dept_id, nil, nil]
+  }}
+  named_scope :printed, lambda { |dept_id| {
+    :conditions => ["department_id = ? AND printed IS NOT ?", dept_id, nil]
+  }}
+
 
   def status
-    if printed
-      "printed"
-    elsif approved
-      "approved"
-    elsif submitted
-      "submitted"
-    else
-      "unsubmitted"
-    end
+    self.printed ? 'printed' : self.approved ? 'approved' : self.submitted ? 'submitted' : 'unsubmitted'
   end
 
   #CUSTOM URL -- STILL REQUIRES ID AT FRONT, BUT LOOKS FRIENDLIER
@@ -44,6 +52,21 @@ class Payform < ActiveRecord::Base
       end
     end
     given_date - given_date_day.days + dept.day.days
+  end
+  
+  
+  protected
+  
+  def validate
+    if (approved or printed) and !submitted
+      errors.add("Cannot approve or print unsubmitted payform, so the submitted field")
+    end
+    if printed and !approved
+      errors.add("Cannot print unapproved payform, so the approved field")
+    end
+    unless user.authorized?(department.payform_configuration.payform_permission.name)
+      errors.add("Payform owner is not authorized for payform department")
+    end
   end
 
 end
