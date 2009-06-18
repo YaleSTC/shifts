30f36f44e25550eaaa4c8cb2dbff105a7f3503aa
Aashish Manchanda <aashish@dhcp128036149050.central.yale.edu>
2009-06-18 17:37:21 -0400

    Temp mass add

--- app/models/payform_item.rb
+++ app/models/payform_item.rb
@@ -2,6 +2,7 @@ class PayformItem < ActiveRecord::Base
 
   belongs_to :payform
   belongs_to :payform_item
+  belongs_to :payform_item_set
 
   delegate :department, :to => :category
   delegate :user, :to => :payform
--- app/models/payform_item_set.rb
+++ app/models/payform_item_set.rb
@@ -1,14 +1,11 @@
 class PayformItemSet < ActiveRecord::Base
 
-  belongs_to :payform  
+  has_many :payform_items
   
   delegate :department, :to => :category
   delegate :user, :to => :payform
-
+  
+  validates_presence_of :description, :hours, :date, :category_id
+  
 end
 
-def self.build(dept, usr, date)
-  period_date = PayformItemSet.default_period_date(date, dept)
-  PayformItemSet.find(:first, :conditions => {:user_id => usr, :department_id => dept, :date => period_date}) ||
-  PayformItemSet.create(:user_id => usr, :department_id => dept, :date => period_date)
-end
