41d86c430d79026434b1207c9413f86df0935118
Smudge <originalsmudge@gmail.com>
2009-06-19 15:09:57 -0400

    fixed categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,19 +2,15 @@ class Category < ActiveRecord::Base
   belongs_to :department
   has_many :payform_items
 
-  validates_presence_of :name, :department_id
+  validates_presence_of :name, :department_id, :active
+  validates_uniqueness_of :name, :scope => :department_id
   
-  named_scope :active,   lambda { |dept_id| { :conditions => ['department_id = ? and active = ?',  dept_id, 1]}}
-  named_scope :disabled, lambda { |dept_id| { :conditions => ['department_id = ? and active = ?', dept_id, 0]}}    
+  named_scope :active,   { :conditions => ['active = ?', true]}
+  named_scope :disabled, { :conditions => ['active = ?', false]}
   
   
   protected
   
-  def validate
-    for category in department.categories
-        errors.add(:category, "name already taken") if name == category.name and id != category.id
-    end
-  end
 
 end
 
