4d076141648efb8f1a5cde8df4f3c3e0a0d7b84b
Derek Zhao <derekzhao@derekzhao-laptop.(none)>
2009-06-18 23:58:59 -0400

    more tinkering with remove and validations

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -50,22 +50,22 @@ class NoticesController < ApplicationController
     @notice.department = @department
     @notice.start_time = Time.now if @notice.is_sticky
     @notice.end_time = nil if params[:indefinite] || @notice.is_sticky
-    params[:for_users].split(/\W+/).each do |login|
-      if a=User.find_by_login(login)
-        @notice.add_viewer_source(a)
+    params[:for_users].split(",").each do |login_or_name|
+      if (viewer = User.find_by_login(login_or_name.strip!) || User.find_by_name(login_or_name.strip!))
+        @notice.add_viewer_source(viewer)
+      else
+        @notice.errors.add_to_base "\'#{login_or_name}\' is not a valid name or NetID." unless login_or_name.blank?
       end
-# => TODO add validation for not usernames/logins
     end
     @notice.add_display_location_source(@department) if params[:department_wide_locations] && current_user.is_admin_of?(@department)
-#    @notice.add_viewer_source(@department) if params[:department_wide_users] && current_user.is_admin_of?(@department)
     if params[:for_locations]
       params[:for_locations].each do |loc|
-         @notice.add_display_location_source(Location.find_by_id(loc))
+        @notice.add_display_location_source(Location.find_by_id(loc))
       end
     end
     if params[:for_location_groups]
       params[:for_location_groups].each do |loc_group|
-       @notice.add_display_location_source(LocGroup.find_by_id(loc_group))
+        @notice.add_display_location_source(LocGroup.find_by_id(loc_group))
       end
     end
     respond_to do |format|
@@ -78,7 +78,6 @@ class NoticesController < ApplicationController
         format.xml  { render :xml => @notice.errors, :status => :unprocessable_entity }
       end
     end
-
   end
 
   # PUT /notices/1
@@ -99,13 +98,13 @@ class NoticesController < ApplicationController
 
   # DELETE /notices/1_id
   # DELETE /notices/1.xml
-  def destroy
+  def remove
     @notice = Notice.find(params[:id])
     unless @notice.is_sticky || current_user.is_admin_of?(@notice.department)
-        redirect_with_flash("You are not authorized to remove this notice") and return
+      redirect_with_flash("You are not authorized to remove this notice") and return
     end
     unless @notice.is_current?
-        redirect_with_flash("This notice was already removed on #{@notice.end_time}") and return
+      redirect_with_flash("This notice was already removed on #{@notice.end_time}") and return
     end
     redirect_with_flash("Notice successfully removed") if @notice.remove(current_user)
     @notice.save
