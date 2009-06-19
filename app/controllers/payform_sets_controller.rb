87843938f85167bd46089cb01e677fdf182b4a21
Smudge <originalsmudge@gmail.com>
2009-06-18 21:15:34 -0400

    working with payform sets

--- app/controllers/payform_sets_controller.rb
+++ app/controllers/payform_sets_controller.rb
@@ -1,4 +1,5 @@
 class PayformSetsController < ApplicationController
+  
   def index
     @payform_sets = PayformSet.all
   end
@@ -7,38 +8,18 @@ class PayformSetsController < ApplicationController
     @payform_set = PayformSet.find(params[:id])
   end
   
-  def new
-    @payform_set = PayformSet.new
-  end
-  
   def create
-    @payform_set = PayformSet.new(params[:payform_set])
+    @payform_set = PayformSet.new
+    @payform_set.department = current_department
+    @payform_set.payforms = current_department.payforms.unprinted
+    @payform_set.payforms.map {|p| p.printed = Time.now }
     if @payform_set.save
       flash[:notice] = "Successfully created payform set."
       redirect_to @payform_set
     else
-      render :action => 'new'
+      flash[:notice] = "Error saving print job."
+      redirect_to payforms_path
     end
   end
   
-  def edit
-    @payform_set = PayformSet.find(params[:id])
-  end
-  
-  def update
-    @payform_set = PayformSet.find(params[:id])
-    if @payform_set.update_attributes(params[:payform_set])
-      flash[:notice] = "Successfully updated payform set."
-      redirect_to @payform_set
-    else
-      render :action => 'edit'
-    end
-  end
-  
-  def destroy
-    @payform_set = PayformSet.find(params[:id])
-    @payform_set.destroy
-    flash[:notice] = "Successfully destroyed payform set."
-    redirect_to payform_sets_url
-  end
 end
--- app/models/payform_set.rb
+++ app/models/payform_set.rb
@@ -1,2 +1,6 @@
 class PayformSet < ActiveRecord::Base
+  
+  has_many :payforms
+  belongs_to :department
+  
 end
