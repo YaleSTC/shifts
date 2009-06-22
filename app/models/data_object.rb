0d769cf3befed22dce3316757e20275301a163b3
Ben Somers <somers.ben@gmail.com>
2009-06-22 14:30:18 -0400

    Finally finished first draft of data objects index; table needs css but is otherwise ready for unajaxed display

--- app/models/data_object.rb
+++ app/models/data_object.rb
@@ -2,18 +2,19 @@ class DataObject < ActiveRecord::Base
   belongs_to :data_type
   has_many :data_entries, :dependent => :destroy
   has_and_belongs_to_many :locations
-
+  
   validates_presence_of   :name
   #validates_presence_of   :data_type_id  #does not work well because of issues with nested routes
   validates_uniqueness_of :name
   
-  def self.by_department(dept)
-    dept.locations.map{|loc| loc.data_objects}.flatten.compact
-  end
-  
+#  GROUP_TYPE_OPTIONS = {"Data type"     =>  "data_types",
+#                      "Location"        =>  "locations",
+#                      "Location Group"  =>  "loc_groups",
+#                      "Department"      =>  "departments"}
+              
   def self.by_location_group(loc_group)
     loc_group.locations.map{|loc| loc.data_objects}.flatten.compact
   end
-  
+    
 end
 
