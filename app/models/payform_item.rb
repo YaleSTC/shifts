68c31c3399698b625fa545c4960385a6d1016b09
Smudge <originalsmudge@gmail.com>
2009-06-23 10:37:39 -0400

    modified payform items'

--- app/models/payform_item.rb
+++ app/models/payform_item.rb
@@ -22,6 +22,7 @@ class PayformItem < ActiveRecord::Base
     errors.add(:description, "seems too short") if description.length < 10 and description.length > 0
     errors.add(:date, "is invalid") if !Date.valid_civil?(date.year, date.month, date.day)
     errors.add(:date, "cannot be in the future") if date > Time.now.to_date
+    errors.add(:reason, "cannot be blank") if !active && !payform.nil?
     #TODO: If it's active, it must belong to a payform. If not, it's an edit.
   end
 
