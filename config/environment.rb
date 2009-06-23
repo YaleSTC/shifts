df0d98d9994a88e7075a0960d2c7e22a72b072b1
Michael Libertin <michael.libertin@yale.edu>
2009-06-23 16:12:26 -0400

    Edited environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -7,9 +7,6 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 
-TASKR4RAILS_AUTH = "stc_493"
-TASKR4RAILS_ALLOWED_HOSTS = ['127.0.0.1']
-
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
@@ -49,6 +46,7 @@ Rails::Initializer.run do |config|
   config.gem "icalendar", :lib  => false
   config.gem "prawn"
   config.gem "chronic"
+  config.gem "authlogic"
 
   # Only load the plugins named here, in the order given. By default, all plugins
   # in vendor/plugins are loaded in alphabetical order.
