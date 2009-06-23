61184683537f462d82fca14e2df7d631c8fd5e5f
Ben Somers <somers.ben@gmail.com>
2009-06-23 13:06:17 -0400

    Fixes for Kobby

--- config/routes.rb
+++ config/routes.rb
@@ -19,15 +19,16 @@ ActionController::Routing::Routes.draw do |map|
   end
 
   map.view_all_data_objects "data_objects/view_all_data_objects", :controller => "data_objects", :action => "view_all"
+
+  map.resources :data_types do |data_type|
+    data_type.resources :data_fields
+    data_type.resources :data_objects, :only => :new
+  end
   
   map.resources :data_objects do |data_object|
     data_object.resources :data_entries
   end
-
-  map.resources :data_types do |data_type|
-    data_type.resources :data_fields
-  end
-
+  
   map.resources :departments, :shallow => true do |departments|
     departments.resources :loc_groups
     departments.resources :locations
