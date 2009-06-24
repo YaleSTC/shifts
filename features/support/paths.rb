bbb1ac761bd12a9b7834fb85386f0cd03968718c
ckildow <cassandra.kildow@yale.edu>
2009-06-24 16:29:18 -0400

    fixed some lingering conflicts from the merge

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -39,18 +39,3 @@ end
 
 World(NavigationHelpers)
 
-    # Add more mappings here.
-    # Here is a more fancy example:
-    #
-    #   when /^(.*)'s profile page$/i
-    #     user_profile_path(User.find_by_login($1))
-
-    else
-      raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
-        "Now, go and add a mapping in #{__FILE__}"
-    end
-  end
-end
-
-World(NavigationHelpers)
-
