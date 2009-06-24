06832d085539d7d212ee7b6a4a318c1dad898a0a
rofreg <rofreg@gmail.com>
2009-06-24 15:51:53 -0400

    added ajax for unscheduled shift signup

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ ActionController::Routing::Routes.draw do |map|
     shifts.resources :sub_requests, :member => {:take => :post, :get_take_info => :get}, :as => "subs"
   end
 
-  map.resources :reports do |report|
+  map.resources :reports, :member => {:popup => :get} do |report|
     report.resources :report_items
   end
 
