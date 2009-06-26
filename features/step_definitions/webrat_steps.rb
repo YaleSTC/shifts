b66686117646a3ac8d09cbd5b276f10f1a3e7f83
ckildow <cassandra.kildow@yale.edu>
2009-06-26 15:38:25 -0400

    some changes to dept_shifts, need to checkout auth branch, so committing

--- features/step_definitions/webrat_steps.rb
+++ features/step_definitions/webrat_steps.rb
@@ -91,7 +91,6 @@ When /^I attach the file at "([^\"]*)" to "([^\"]*)"$/ do |path, field|
 end
 
 Then /^I should see "([^\"]*)"$/ do |text|
-#  raise @user.roles.to_yaml
   response.should contain(text)
 end
 
