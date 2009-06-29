b1056bf3c43b21bf5c9c27d4fa5e29e798df9062
Derek Zhao <derekzhao@derekzhao-laptop.(none)>
2009-06-29 14:18:52 -0400

    various dashboard changes

--- app/helpers/shifts_helper.rb
+++ app/helpers/shifts_helper.rb
@@ -1,9 +1,4 @@
 module ShiftsHelper
-  def early_late_info(start)
-    now = Time.now
-    m = distance_of_time_in_words(now - start)
-    m += (now > start) ? " ago" : " later"
-  end
 
   def load_variables(loc_group)
     #TODO: clean this up?
