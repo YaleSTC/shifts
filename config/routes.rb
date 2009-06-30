32b3e1ae58bfd355dccd75bc872cf9d953a70632
Laura Tomas <laura.tomas@yale.edu>
2009-06-30 15:35:58 -0400

    Fixed routes.rb.

--- config/routes.rb
+++ config/routes.rb
@@ -101,7 +101,6 @@ ActionController::Routing::Routes.draw do |map|
   # consider removing the them or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
-end
 
   # map.root :controller => "welcome"
   map.root :controller => "dashboard"
