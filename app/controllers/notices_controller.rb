81c9a72916b79883762a30d9c85baf76e36843ac
Derek Zhao <derekzhao@derekzhao-laptop.(none)>
2009-06-30 16:42:45 -0400

    merging notice controller

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -128,10 +128,10 @@ class NoticesController < ApplicationController
       #parse autocomplete
       params[:for_users].split(",").each do |l|
         l = l.split("||")
-        @notice.add_viewer_source(l[0].constantize.find(l[1])) if l.length == 2
+        @notice.user_sources << l[0].constantize.find(l[1]) if l.length == 2
       end
     end
-    @notice.add_viewer_source(@department) if params[:department_wide_viewers] && !@notice.is_sticky
+    @notice.user_sources << @department if params[:department_wide_viewers] && !@notice.is_sticky
     @notice.remove_all_display_location_sources if update
     @notice.add_display_location_source(@department) if params[:department_wide_locations] && current_user.is_admin_of?(@department)
     if params[:for_locations]
