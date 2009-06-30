83bc055619b3de100a8c525f4d37cdbdee96ffd0
Ben Somers <somers.ben@gmail.com>
2009-06-30 10:05:04 -0400

    Added user observer, changed forms for data objects and user configs

--- app/controllers/user_configs_controller.rb
+++ app/controllers/user_configs_controller.rb
@@ -1,16 +1,20 @@
 class UserConfigsController < ApplicationController   
+
   def edit
     @user_config = UserConfig.find(params[:id])
+    @dept_select = current_user.departments.map{|d| [d.name, d.id]}
+    @loc_group_select = {}
+    current_user.departments.each do |dept|
+      @loc_group_select.store(dept.id, current_user.loc_groups(dept))
+    end    
   end
   
   def update
     @user_config = UserConfig.find(params[:id])
-    if params[:user_config]
-      params[:user_config][:default_dept] = Department.find_by_name(params[:user_config][:default_dept])
-      params[:user_config][:view_loc_groups] = params[:user_config][:view_loc_groups].split(',').map{|name|LocGroup.find_by_name(name).id}
-    end
+    raise params.to_yaml
     if @user_config.update_attributes(params[:user_config])
       flash[:notice] = "Successfully updated user config."
+      raise @user_config.to_yaml
       redirect_to edit_user_config_path
     else
       render :action => 'edit'
