f018408345f7d37809564d12c5fbfecffd872fbb
Smudge <originalsmudge@gmail.com>
2009-07-01 16:31:08 -0400

    generated models from has_many_polymorphs are shown in folder 'generated models'

--- generated_models/location.rb
+++ generated_models/location.rb
@@ -18,48 +18,109 @@ class Location
      # called from line 57
 
   has_many_polymorphs :location_sinks,
-     :foreign_key => "location_source_id",
+     :conflicts => [],
      :foreign_type_key => "location_source_type",
      :singular_reverse_association_id => :location_source,
      :as => :location_source,
+     :foreign_key => "location_source_id",
      :through => :location_sinks_location_sources,
      :is_double => true,
-     :conflicts => [],
      :from => [:notices,
      :restrictions]
      # called from line 57
 
   has_many :location_sinks_location_sources_as_location_source,
-     :foreign_key => "location_source_id",
      :conditions => "location_source_type = 'Location'",
      :dependent => :destroy,
      :as => :location_source,
      :extend => [],
-     :class_name => "LocationSinksLocationSource"
-     # called from line 57
-
-  has_many :notices,
-     :conditions => nil,
-     :limit => nil,
-     :source_type => "Notice",
-     :source => :location_sink,
-     :group => nil,
-     :through => :location_sinks_location_sources_as_location_source,
-     :order => nil,
-     :extend => [Location::LocationNoticePolymorphicChildAssociationExtension],
-     :class_name => "Notice"
+     :class_name => "LocationSinksLocationSource",
+     :foreign_key => "location_source_id"
      # called from line 57
 
   has_many :restrictions,
      :conditions => nil,
      :limit => nil,
-     :source_type => "Restriction",
      :source => :location_sink,
      :group => nil,
-     :through => :location_sinks_location_sources_as_location_source,
-     :order => nil,
+     :source_type => "Restriction",
      :extend => [Location::LocationRestrictionPolymorphicChildAssociationExtension],
-     :class_name => "Restriction"
+     :class_name => "Restriction",
+     :through => :location_sinks_location_sources_as_location_source,
+     :order => nil
+     # called from line 57
+
+  has_many :notices,
+     :conditions => nil,
+     :limit => nil,
+     :source => :location_sink,
+     :group => nil,
+     :source_type => "Notice",
+     :extend => [Location::LocationNoticePolymorphicChildAssociationExtension],
+     :class_name => "Notice",
+     :through => :location_sinks_location_sources_as_location_source,
+     :order => nil
+     # called from line 57
+
+  belongs_to ,
+     loc_grou
+     # called from line 57
+
+  has_many ,
+     time_slot
+     # called from line 57
+
+  has_many ,
+     shift
+     # called from line 57
+
+  has_and_belongs_to_many ,
+     data_object
+     # called from line 57
+
+  has_many_polymorphs :location_sinks,
+     :conflicts => [],
+     :foreign_type_key => "location_source_type",
+     :singular_reverse_association_id => :location_source,
+     :as => :location_source,
+     :foreign_key => "location_source_id",
+     :through => :location_sinks_location_sources,
+     :is_double => true,
+     :from => [:notices,
+     :restrictions]
+     # called from line 57
+
+  has_many :location_sinks_location_sources_as_location_source,
+     :conditions => "location_source_type = 'Location'",
+     :dependent => :destroy,
+     :as => :location_source,
+     :extend => [],
+     :class_name => "LocationSinksLocationSource",
+     :foreign_key => "location_source_id"
+     # called from line 57
+
+  has_many :restrictions,
+     :conditions => nil,
+     :limit => nil,
+     :source => :location_sink,
+     :group => nil,
+     :source_type => "Restriction",
+     :extend => [Location::LocationRestrictionPolymorphicChildAssociationExtension],
+     :class_name => "Restriction",
+     :through => :location_sinks_location_sources_as_location_source,
+     :order => nil
+     # called from line 57
+
+  has_many :notices,
+     :conditions => nil,
+     :limit => nil,
+     :source => :location_sink,
+     :group => nil,
+     :source_type => "Notice",
+     :extend => [Location::LocationNoticePolymorphicChildAssociationExtension],
+     :class_name => "Notice",
+     :through => :location_sinks_location_sources_as_location_source,
+     :order => nil
      # called from line 57
 
 end
