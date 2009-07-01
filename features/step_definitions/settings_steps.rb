4cb1078d3932a5bae75c76712a509e95fd44c05a
Wei Yan <wei@weilaptop.(none)>
2009-07-01 18:53:09 -0400

    changed one step definition for how to make sure that a default dept is set

--- features/step_definitions/settings_steps.rb
+++ features/step_definitions/settings_steps.rb
@@ -55,14 +55,8 @@ end
 
 Then /^the page should indicate that I am in the department "([^\"]*)"$/ do |department|
 
-  # ATTN: the dev team should choose one of the following lines and use it.
-  # Use the first line if we want to have something like "Current department: STC" on the page
-  # that indicates the current department.  Use the second line if we want a menu with links
-  # to different departments, except the current department will be displayed but is not
-  # a clickable link (which indicates that we're currently in that department)
-#  response.should contain("Current department: #{department}")
-
-  response.should_not have_selector("a", :content => department)
+  # the following code checks to see that the select box has the correct department selected
+  field_with_id("department_id").value.should == [Department.find_by_name(department).id.to_s]
 end
 
 Then /^I should see all the days of the week$/ do
