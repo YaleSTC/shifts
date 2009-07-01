0a0b8a82590cac8d7e60539376f78f1e5b76f74c
Smudge <originalsmudge@gmail.com>
2009-07-01 16:34:27 -0400

    duplicate lines

--- generated_models/user_sinks_user_source.rb
+++ generated_models/user_sinks_user_source.rb
@@ -18,21 +18,4 @@ class UserSinksUserSource
      :users]
      # called from line 57
 
-  belongs_to :user_source,
-     :polymorphic => true
-     # called from line 57
-
-  belongs_to :user_sink,
-     :polymorphic => true
-     # called from line 57
-
-  acts_as_double_polymorphic_join ,
-     :user_sinks => [:restrictions,
-     :sub_requests,
-     :notices],
-     :user_sources => [:departments,
-     :roles,
-     :users]
-     # called from line 57
-
 end
