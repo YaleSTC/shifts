2c61dd0300adc670be6a759f6a741c4c6eebf819
Derek Zhao <derekzhao@derekzhao-laptop.(none)>
2009-07-02 18:14:14 -0400

    checkboxes works a little more like it should

--- app/helpers/notices_helper.rb
+++ app/helpers/notices_helper.rb
@@ -1,14 +1,17 @@
 module NoticesHelper
 
   def locations_check(location)
-    @notice.departments.locations.include?(location) or current_user.current_shift.location == location or @notice.locations.include?(location) or loc_group.locations.include?(location)
+    @notice.departments.each{|d| d.locations.include?(location)} or
+    current_user.current_shift.location == location or
+    @notice.locations.include?(location) or
+    loc_group.locations.include?(location)
   end
 
   def loc_group_check(loc_group)
-    if @notice.departments.empty?
+    unless @notice.departments.empty?
       @notice.loc_groups.include?(loc_group)
     else
-      @notice.departments.loc_groups.include?(loc_group)
+      @notice.departments.each{|d| d.loc_groups.include?(loc_group)}
     end
   end
 
