1f30d6d779390197ce74032afe6d6957d4b563ea
Michael Libertin <michael.libertin@yale.edu>
2009-07-03 11:37:49 -0400

    added some stuff ofr switching auth_types

--- app/models/app_mailer.rb
+++ app/models/app_mailer.rb
@@ -14,7 +14,7 @@ class AppMailer < ActionMailer::Base
     if new_payform_item == nil && !old_payform_item.active
       subject   "Your payform item has been deleted by #{old_payform_item.source}"
     else
-      subject   "Your payform item has been modified by #{new_payform_item.source}" 
+      subject   "Your payform item has been modified by #{new_payform_item.source}"
     end
     body        :old_payform_item => old_payform_item, :new_payform_item => new_payform_item
   end
@@ -42,5 +42,13 @@ class AppMailer < ActionMailer::Base
     sent_on       Time.now
     body          :edit_new_user_password_url => edit_password_reset_url(user.perishable_token)
   end
-  
+
+  def change_auth_type_password_reset_instructions(user)
+    subject       "Password Creation Instructions"
+    from          "Yale@yale.edu"
+    recipients    user.email
+    sent_on       Time.now
+    body          :edit_password_url => edit_password_reset_url(user.perishable_token)
+  end
+
 end
