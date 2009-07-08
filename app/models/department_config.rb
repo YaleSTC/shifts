8ad9923124f2ff04f7c755e47574a80aaffd89ce
rofreg <rofreg@gmail.com>
2009-07-08 15:50:21 -0400

    department start and end times are now forced to fall on hours

--- app/models/department_config.rb
+++ app/models/department_config.rb
@@ -6,10 +6,25 @@ class DepartmentConfig < ActiveRecord::Base
   validates_numericality_of :schedule_start, :schedule_end
 
   def calibrate_time
+    #allow the schedule for a day to end at, say, 2:00am
     if self
       self.schedule_end += 24*60 if self.schedule_end <= self.schedule_start
       self.save
     end
+  end
+  
+  
+  # methods for use in the schedule view
+  def blocks_per_hour
+    60 / self.time_increment
+  end
+  
+  def blocks_per_day
+    ((self.schedule_end - self.schedule_start) / self.time_increment).to_i
+  end
+  
+  def block_length
+    self.time_increment * 60 #seconds
   end
 
 end
