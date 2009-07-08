a58fed995d7da8a04687886c5ece23abbd8d1da0
Kwabena Antwi-Boasiako <hikwabena@gmail.com>
2009-07-08 14:01:07 -0400

    ar_mailer config

--- config/environment.rb
+++ config/environment.rb
@@ -12,6 +12,7 @@ TASKR4RAILS_ALLOWED_HOSTS = ['127.0.0.1']
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
+require 'action_mailer/ar_mailer'
 
 Rails::Initializer.run do |config|
 
@@ -32,6 +33,7 @@ Rails::Initializer.run do |config|
 
   # Configure Rails Mail options
   #config.action_mailer.delivery_method = :smtp
+  
   config.action_mailer.smtp_settings = {
     :address => "mail.yale.edu",
     :port => 587,
