b9fc6b95b8008facf49e18701d2a744b0cd38ebc
Smudge <originalsmudge@gmail.com>
2009-07-08 14:46:16 -0400

    removed version num

--- config/environment.rb
+++ config/environment.rb
@@ -55,7 +55,7 @@ Rails::Initializer.run do |config|
   config.gem "chronic"
   config.gem "has_many_polymorphs"
   config.gem "authlogic"
-  config.gem 'ar_mailer', :version => '1.3.1', :lib => 'action_mailer/ar_mailer'
+  config.gem 'ar_mailer', :lib => 'action_mailer/ar_mailer'
   #make sure you remove 'ar_mailer' - sudo gem uninstall ar_mailer - this is an old version
   
   # Only load the plugins named here, in the order given. By default, all plugins
