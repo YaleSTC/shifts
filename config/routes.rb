fe450eaea0562ff75239ec3636811e9c5e8c589c
Kwabena Antwi-Boasiako <hikwabena@gmail.com>
2009-07-08 13:29:13 -0400

    Email notifications, ar_mailer ok

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resources :punch_clocks
   map.resources :restrictions
-  map.email_reminders "email_reminders", :controller => 'payforms', :action => 'email_reminders'
+  map.email_reminders "/email_reminders", :controller => 'payforms', :action => 'email_reminders'
 
   map.login "/login", :controller => 'user_sessions', :action => 'new'
   map.logout "/logout", :controller => 'user_sessions', :action => 'destroy'
--- vendor/plugins/ar_mailer/generators/ar_mailer/templates/migration.rb
+++ vendor/plugins/ar_mailer/generators/ar_mailer/templates/migration.rb
@@ -1,6 +1,6 @@
-class AddEmail < ActiveRecord::Migration
+class <%= migration_name %> < ActiveRecord::Migration
   def self.up
-    create_table :emails do |t|
+    create_table :<%= table_name %> do |t|
       t.column :from, :string
       t.column :to, :string
       t.column :last_send_attempt, :integer, :default => 0
@@ -10,6 +10,6 @@ class AddEmail < ActiveRecord::Migration
   end
 
   def self.down
-    drop_table :emails
+    drop_table :<%= table_name %>
   end
 end
