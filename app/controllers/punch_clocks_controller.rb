adb0e65d1b090c7a62c9f567d2dc82e30b99b9f1
Smudge <originalsmudge@gmail.com>
2009-07-14 18:29:12 -0400

    added jquery and jrails to dashboard.
    no need to universally load javascript files when we may not need jrails/jquery everywhere.

--- app/controllers/punch_clocks_controller.rb
+++ app/controllers/punch_clocks_controller.rb
@@ -54,7 +54,7 @@ class PunchClocksController < ApplicationController
     @payform_item = PayformItem.new({:date => Date.today,
                                     :category => Category.find_by_name("Punch Clocks"),
                                     :hours => @time_in_hours,
-                                    :description => "Punch Clock: " + params[:punch_clock]['description']})
+                                    :description => params[:punch_clock]['description']})
     @payform = Payform.build(current_department, @user, Date.today)
     @payform_item.payform = @payform
     # @payform_item.save
@@ -62,10 +62,10 @@ class PunchClocksController < ApplicationController
       flash[:notice] = "Successfully clocked out"
     else
       flash[:notice] = "Could not clock out"
-      redirect_to dashboard_url and return
+      redirect_to dashboard_path and return
     end
-    if current_user == @user
-      redirect_to dashboard_url
+    if current_user == @punch_clock.user
+      redirect_to dashboard_path
     else
       redirect_to punch_clocks_path
     end
