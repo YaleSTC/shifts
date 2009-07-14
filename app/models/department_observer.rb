6b2640a6afced227206f5f4322d939eb0e9c8f03
ckildow <cassandra.kildow@yale.edu>
2009-07-14 15:02:24 -0400

    tweaked fixtures and some cuke tests

--- app/models/department_observer.rb
+++ app/models/department_observer.rb
@@ -4,20 +4,22 @@ class DepartmentObserver < ActiveRecord::Observer
   # Automatically create department config for a department
   def after_create(department)
     DepartmentConfig.create!({:department_id => department.id,
-                        :schedule_start => "9",
-                        :schedule_end => "5",
-                        :time_increment => 15.minutes,
-                        :grace_period => 7.minutes,
+                        :schedule_start => "900",
+                        :schedule_end => "1700",
+                        :time_increment => "15",
+                        :grace_period => "7",
+                        :complex => false,
+                        :day => "6",
+                        :day2 => "1",
                         :edit_report => false,
+                        :end_of_month => false,
+                        :monthly => false,
                         :weekend_shifts => true,
                         :unscheduled_shifts => true,
-                        :printed_message => "This payform has already been printed and may no longer be edited by you.\n" +
-                                            "If there is a problem that needs to be addressed, please talk to the administration.",
-                        :reminder_message => "Please remember to submit your payform for the week.",
-                        :warning_message => "You have not submitted payforms for the weeks ending on the following dates:\n" +
-                                            "\n@weeklist@\n" +
-                                            "Please submit your payforms. If some of the weeks listed are weeks during which you did not work, please just submit a blank payform.",
-                        :warning_weeks => 4,
+                        :printed_message => "This payform has already been printed and may no longer be edited by you.\n If there is a problem that needs to be addressed, please talk to the administration.",
+                        :reminder_message => "Please remember to submit your payform for this week.",
+                        :warning_message => "You have not submitted payforms for the weeks ending on the following dates:\n \n@weeklist@\n Please submit your payforms. If some of the weeks listed are weeks during which you did not work, please just submit a blank payform.",
+                        :warning_weeks => 2,
                         :description_min => 4,
                         :show_disabled_cats => true,
                         :reason_min => 4,
