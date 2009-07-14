90ed124c9c90eca213288d5a9d9b3cdfd99a89d0
Laura Tomas <laura.tomas@yale.edu>
2009-07-14 13:40:57 -0400

    Implemented min description length for payform items.

--- app/models/payform_item.rb
+++ app/models/payform_item.rb
@@ -16,11 +16,11 @@ class PayformItem < ActiveRecord::Base
   validates_numericality_of :hours
 
   named_scope :active, lambda { |*args| { :conditions => ['active = ?',  true] } }
-  
+
   protected
-  
+
   def validate
-    errors.add(:description, "seems too short") if description.length < 10 and description.length > 0
+    errors.add(:description, "seems too short") if description.length < @payform.department.department_config.description_min and description.length > 0
     #errors.add(:reason, "seems too short") if !active and reason and reason.length < 10 and reason.length > 0
     errors.add(:date, "is invalid") if !Date.valid_civil?(date.year, date.month, date.day)
     errors.add(:date, "cannot be in the future") if date > Time.now.to_date
@@ -28,3 +28,4 @@ class PayformItem < ActiveRecord::Base
   end
 
 end
+
