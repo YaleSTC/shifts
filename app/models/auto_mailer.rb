5cf1c4092424467113708a0463ee47dc9bf654f0
Kwabena Antwi-Boasiako <hikwabena@gmail.com>
2009-07-15 19:21:53 -0400

    mailer stuff

--- app/models/auto_mailer.rb
+++ app/models/auto_mailer.rb
@@ -4,11 +4,11 @@ class AutoMailer < ActiveRecord::Base
   departments_that_want_users_reminded = Department.all.select { |d| d.auto_remind }
   
   for department in departments_that_want_users_reminded
-    #
+    send_reminders(department)
   end
 
   for dept in departments_that_want_users_warned
-    #
+    send_warnings(department)
   end
   
   def send_reminders(department)
@@ -20,18 +20,19 @@ class AutoMailer < ActiveRecord::Base
       ArMailer.deliver(ArMailer.create_due_payform_reminder(admin_user, user, message))
       users_reminded << "#{user.name} (#{user.login})"
     end
+    puts "#{users_reminded.length} users in the #{department.name} department "  +
+         "have been reminded to submit their due payforms."
   end
   
   def send_warnings(department)
     message = department.department_config.warning_message
-    start_date = Date.parse(params[:post]["date"])
-    @department = current_department
-    @users = @department.users.sort_by(&:name)
+    start_date = (w = department.department_config.warning_weeks) ? w.weeks.ago : 8.weeks.ago
+    @users = department.users.sort_by(&:name)
     users_warned = []
-    @admin_user = current_user
+    @admin_user = User.find_by_login("kaa43")
     for user in @users     
-      Payform.build(@department, user, Date.today)
-      unsubmitted_payforms = (Payform.all( :conditions => { :user_id => user.id, :department_id => @department.id, :submitted => nil }, :order => 'date' ).select { |p| p if p.date >= start_date && p.date < Date.today }).compact
+      #Payform.build(department, user, Date.today)
+      unsubmitted_payforms = (Payform.all( :conditions => { :user_id => user.id, :department_id => department.id, :submitted => nil }, :order => 'date' ).select { |p| p if p.date >= start_date && p.date < 1.week.ago }).compact
       
       unless unsubmitted_payforms.blank?
         weeklist = ""
@@ -42,7 +43,8 @@ class AutoMailer < ActiveRecord::Base
         ArMailer.deliver(email)
         users_warned << "#{user.name} (#{user.login}) <pre>#{email.encoded}</pre>"
       end
-    end
-    redirect_with_flash "E-mail warnings sent to the following: <br/><br/>#{users_warned.join}", :action => :email_reminders, :id => @department.id
+    end  # currently I am not doing anything with the list of users, it should be displayed somewhere
+    puts "#{users_reminded.length} users in the #{department.name} department "  +
+         "have been warned to submit their late payforms."
   end
 end
