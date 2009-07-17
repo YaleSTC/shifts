a968001a5a048c15d031ae1f2b892a88be7cca73
Kwabena Antwi-Boasiako <hikwabena@gmail.com>
2009-07-17 15:01:14 -0400

    Start of punch_clock_sets

---
+++ db/migrate/20090717162538_create_punch_clock_sets.rb
@@ -0,0 +1,14 @@
+class CreatePunchClockSets < ActiveRecord::Migration
+  def self.up
+    create_table :punch_clock_sets do |t|
+      t.string :description
+      t.
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :punch_clock_sets
+  end
+end
