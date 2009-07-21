3d7d40c54fd61b28d8deb6202f257bb5bf436326
somanyrobots <somers.ben@gmail.com>
2009-07-21 13:02:43 -0400

    Removed unnecessary actions

--- app/controllers/report_items_controller.rb
+++ app/controllers/report_items_controller.rb
@@ -1,15 +1,16 @@
 class ReportItemsController < ApplicationController
-  def index
-    @report_items = ReportItem.all
-  end
+# Commented out all unnecessary actions -ben
+#  def index
+#    @report_items = ReportItem.all
+#  end
 
-  def show
-    @report_item = ReportItem.find(params[:id])
-  end
+#  def show
+#    @report_item = ReportItem.find(params[:id])
+#  end
 
-  def new
-    @report_item = ReportItem.new
-  end
+#  def new
+#    @report_item = ReportItem.new
+#  end
 
   def create
     @report_item = ReportItem.new(params[:report_item])
@@ -32,25 +33,25 @@ class ReportItemsController < ApplicationController
     end
   end
 
-  def edit
-    @report_item = ReportItem.find(params[:id])
-  end
+#  def edit
+#    @report_item = ReportItem.find(params[:id])
+#  end
 
-  def update
-    @report_item = ReportItem.find(params[:id])
-    if @report_item.update_attributes(params[:report_item])
-      flash[:notice] = "Successfully updated reportitem."
-      redirect_to @report_item
-    else
-      render :action => 'edit'
-    end
-  end
+#  def update
+#    @report_item = ReportItem.find(params[:id])
+#    if @report_item.update_attributes(params[:report_item])
+#      flash[:notice] = "Successfully updated report item."
+#      redirect_to @report_item
+#    else
+#      render :action => 'edit'
+#    end
+#  end
 
-  def destroy
-    @report_item = ReportItem.find(params[:id])
-    @report_item.destroy
-    flash[:notice] = "Successfully destroyed reportitem."
-    redirect_to shift_report_report_items_url(@report_item.report.shift)
-  end
+#  def destroy
+#    @report_item = ReportItem.find(params[:id])
+#    @report_item.destroy
+#    flash[:notice] = "Successfully destroyed report item."
+#    redirect_to shift_report_report_items_url(@report_item.report.shift)
+#  end
 end
 
