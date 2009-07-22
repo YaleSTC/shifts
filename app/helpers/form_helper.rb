c91176ea438a992e9393e28e8d4db6b19ccc1c85
rofreg <rofreg@gmail.com>
2009-07-22 12:18:42 -0400

    not working yet...

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,5 +1,5 @@
 module FormHelper
-  def text_field(*args)
+  def date_select(*args)
     #args[0] == the name of the object
     #args[1] == the name of the field
     #args[2] == datepicker: true/false
@@ -11,10 +11,22 @@ module FormHelper
     end
   end
   
-  def datetime_picker(*args)
+  def timepicker(field_name)
+    time_choices = (0..1440).step(@department.department_config.time_increment).map{|t| [t.min_to_am_pm, t.min_to_am_pm]}
+    select(field_name.symbolize, time_choices)
     #args[0] == the name of the object
     #args[1] == the name of the field
     #args[2] == datepicker: true/false
-    
+  end
+  
+  def select(*args)
+    if args[2] == :timepicker
+      time_choices = (0..1440).step(@department.department_config.time_increment).map{|t| [t.min_to_am_pm, t.min_to_am_pm]}
+      args[2] = time_choices
+      #args.to_json
+      super(*args)
+    else
+      super(*args)
+    end
   end
 end
