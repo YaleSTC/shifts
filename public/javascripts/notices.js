0e217dd6ceee60094e43d896b73e969981f83d6a
Derek Zhao <derekzhao@derekzhao-laptop.(none)>
2009-07-23 02:23:57 -0400

    more notice bug fixes, added upcoming notices

--- public/javascripts/notices.js
+++ public/javascripts/notices.js
@@ -14,11 +14,11 @@ jQuery(document).ready(function(){
     $("#type_sticky").click(function(){
         $("#start_time_choice_date").attr('checked', false);
         $("#start_time_choice_now, #end_time_choice_indefinite").attr('checked', true);
-        $("#start_time_choice_date, #notice_start_time_1i, #notice_start_time_2i, #notice_start_time_3i, #notice_start_time_4i, #notice_start_time_5i, #notice_start_time_7i, #notice_end_time_1i, #end_time_choice_date, #notice_end_time_2i, #notice_end_time_3i, #notice_end_time_4i, #notice_end_time_5i, #notice_end_time_7i").attr("disabled", "disabled");
+        $("#start_time_choice_date, #notice_start_time, #notice_start_time-mm, #notice_start_time-dd, #notice_start_time_4i, #notice_start_time_5i, #notice_start_time_7i, #notice_end_time, #end_time_choice_date, #notice_end_time-mm, #notice_end_time-dd, #notice_end_time_4i, #notice_end_time_5i, #notice_end_time_7i").attr("disabled", "disabled");
     });
 
     $("#type_announcement").click(function(){
-        $("#start_time_choice_date, #notice_start_time_1i, #notice_start_time_2i, #notice_start_time_3i, #notice_start_time_4i, #notice_start_time_5i, #notice_start_time_7i, #notice_end_time_1i, #end_time_choice_date, #notice_end_time_2i, #notice_end_time_3i, #notice_end_time_4i, #notice_end_time_5i, #notice_end_time_7i").removeAttr("disabled");
+        $("#start_time_choice_date, #notice_start_time, #notice_start_time-mm, #notice_start_time-dd, #notice_start_time_4i, #notice_start_time_5i, #notice_start_time_7i, #notice_end_time, #end_time_choice_date, #notice_end_time-mm, #notice_end_time-dd, #notice_end_time_4i, #notice_end_time_5i, #notice_end_time_7i").removeAttr("disabled");
     });
 });
 
