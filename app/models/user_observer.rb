4952fbbbd262f8813aadb6de5304ab30d7fb4424
rofreg <rofreg@gmail.com>
2009-07-23 14:50:03 -0400

    shifts touchups

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -1,9 +1,10 @@
 class UserObserver < ActiveRecord::Observer
 
   # Automatically create user config for a user
+  # default to viewing all location groups
   def after_create(user)
     UserConfig.create!({:user_id => user.id,
-                        :view_loc_groups => "",
+                        :view_loc_groups => (.departments.collect{|d| d.loc_groups}.flatten.collect{|l| l.id} * ", "),
                         :view_week => "",
                         :default_dept => user.departments.first.id
                         })
