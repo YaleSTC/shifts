f0a1704601b1c8e14873cdec61d8ba167c3dc3a6
rofreg <rofreg@gmail.com>
2009-07-23 14:50:03 -0400

    shifts touchups

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -12,7 +12,7 @@ class ShiftsController < ApplicationController
     @loc_group_select = {}
     current_user.departments.each do |dept|
       @loc_group_select.store(dept.id, current_user.loc_groups(dept))
-    end    
+    end
     @selected_loc_groups = current_user.user_config.view_loc_groups.split(', ').map{|lg|LocGroup.find(lg).id}
     
     # figure out what days to display based on user preferences
