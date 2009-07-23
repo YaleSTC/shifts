3a7bbcb2f7f3dbe2ec0344852bd10856f59fb44c
rofreg <rofreg@gmail.com>
2009-07-23 17:50:25 -0400

    rt filter added

--- app/controllers/sub_requests_controller.rb
+++ app/controllers/sub_requests_controller.rb
@@ -57,6 +57,8 @@ class SubRequestsController < ApplicationController
     @sub_request = SubRequest.find(params[:id])
     return unless require_owner_or_dept_admin(@sub_request.shift, current_department)
     @sub_request.destroy
+    flash[:notice] = "Successfully destroyed sub request."
+    redirect_to shifts_url
   end
 
   def get_take_info
