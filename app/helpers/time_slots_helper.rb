bceafd3b1de63cdc7abf1c7d55690ee89243c285
Smudge <originalsmudge@gmail.com>
2009-07-24 16:46:28 -0400

    fixing things

--- app/helpers/time_slots_helper.rb
+++ app/helpers/time_slots_helper.rb
@@ -1,2 +1,15 @@
 module TimeSlotsHelper
+  
+  def time_slot_style(time_slot)
+    left = ((time_slot.start - (time_slot.start.at_beginning_of_day + @dept_start_hour.hours))/3600.0)/@hours_per_day*100
+    width = (time_slot.duration/3600.0) / @hours_per_day * 100
+    if left < 0 
+      width -= left
+      left = 0 
+    end  
+    if 100-left + width > 100
+      width = 100-left
+    end
+    "width: #{width}%; left: #{left}%;"
+  end
 end
