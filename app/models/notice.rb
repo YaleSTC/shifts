7ca80db5ced2ddee0fcaaba53d5b3511a9e0c8f2
Laura Tomas <laura.tomas@yale.edu>
2009-07-27 16:05:01 -0400

    Changes to notices.

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -10,7 +10,7 @@ class Notice < ActiveRecord::Base
   validate :presence_of_locations_or_viewers
 
   named_scope :inactive, lambda {{ :conditions => ["end_time < ?", Time.now.utc] }}
-  named_scope :active_with_end, lambda {{ :conditions => ["start_time < ? and end_time > ?", Time.now.utc, Time.now.utc]}}
+  named_scope :active_with_end, lambda {{ :conditions => ["start_time < ? and end_time < ?", Time.now.utc, Time.now.utc]}}
   named_scope :active_without_end, lambda {{ :conditions => ["start_time < ?", Time.now.utc]}}
   named_scope :upcoming, lambda {{ :conditions => ["start_time > ?", Time.now.utc]}}
   named_scope :stickies, lambda {{ :conditions => ["is_sticky = ?", true]}}
