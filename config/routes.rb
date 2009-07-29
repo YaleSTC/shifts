60639bfbdbeb390a695a88f3c9bd8fa87c2fa1de
somanyrobots <somers.ben@gmail.com>
2009-07-29 17:35:58 -0400

    Finished edits to data_entries show, misc other fixes

--- config/routes.rb
+++ config/routes.rb
@@ -73,8 +73,6 @@ ActionController::Routing::Routes.draw do |map|
     report.resources :report_items
   end
 
-  map.view_all_data_objects "data_objects/view_all_data_objects", :controller => "data_objects", :action => "view_all"
-
   map.resources :data_types do |data_type|
     data_type.resources :data_fields
     data_type.resources :data_objects, :only => [:new, :create]
