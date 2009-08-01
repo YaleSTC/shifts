0063fdbf488ecff1e012aa51603d118046873641
Smudge <originalsmudge@gmail.com>
2009-07-31 23:19:19 -0400

    removed extra file from repository

--- db/migrate/20090731231858_add_signed_in_to_shift.rb
+++
@@ -1,9 +0,0 @@
-class AddSignedInToShift < ActiveRecord::Migration
-  def self.up
-    add_column :shifts, :signed_in, :boolean, :default => :false
-  end
-
-  def self.down
-    remove_column :shifts, :signed_in
-  end
-end
