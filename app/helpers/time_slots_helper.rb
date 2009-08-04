0a9e808295f98e3245faf277bb6324741ffea0f0
rofreg <rofreg@gmail.com>
2009-08-04 16:27:30 -0400

    bugfix for timeslots; display stuff

--- app/helpers/time_slots_helper.rb
+++ app/helpers/time_slots_helper.rb
@@ -5,7 +5,7 @@ module TimeSlotsHelper
     left = ((time_slot.start - (time_slot.start.at_beginning_of_day + @dept_start_hour.hours))/3600.0)/@hours_per_day*100
     width = (time_slot.duration/3600.0) / @hours_per_day * 100
     if left < 0
-      width -= left
+      width += left
       left = 0 
       @left_overflow = true
     end
