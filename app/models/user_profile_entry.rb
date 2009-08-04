f19528cf85f2ff2cf6b17ef977ff36f6f11b012b
ckildow <cassandra.kildow@yale.edu>
2009-08-04 17:55:54 -0400

    and now its back.

--- app/models/user_profile_entry.rb
+++ app/models/user_profile_entry.rb
@@ -16,8 +16,10 @@ class UserProfileEntry < ActiveRecord::Base
   end
 
   def prepare_form_helpers
-    if display_type == "text_field" || "picture_link"
+    if display_type == "text_field"
       return ["user_profile_entries[#{id}]", id, {:value => content}]
+#    elsif display_type == "picture_link"
+#      return ["user_profile_entries[#{id}]", id, {:value => content}]
     elsif display_type == "text_area"
       return ["user_profile_entries[#{id}]", id, {:id => id, :value => content}]
     elsif display_type == "select"
@@ -25,7 +27,11 @@ class UserProfileEntry < ActiveRecord::Base
       return ["user_profile_entries[#{id}]", id, options.map{|opt| [opt, opt]}, {:selected => content}]
     elsif display_type == "check_box"
       options = values.split(',').each{|opt| opt.squish!}
-      return options.map{|v| ["user_profile_entries[#{id}]", v, content.include?(v) ? {v => '', :checked => true} : {v => ''}]}
+      if content
+        return options.map{|v| ["user_profile_entries[#{id}]", v, content.include?(v) ? {v => '', :checked => true} : {v => ''}]}
+      else
+        return options.map{|v| ["user_profile_entries[#{id}]", v]}
+      end
     elsif display_type == "radio_button"
       options = values.split(',').each{|opt| opt.squish!}
       return options.map{|v| ["user_profile_entries[#{id}]", 1, v, content==v ? {v => '', :checked => true} : {v => ''}]}
