aa18661c5738cd75a0d310d10f71818d61b66644
somanyrobots <somers.ben@gmail.com>
2009-08-07 17:06:45 -0400

    report view changes - mostly javascript

--- app/controllers/user_configs_controller.rb
+++ app/controllers/user_configs_controller.rb
@@ -10,7 +10,8 @@ class UserConfigsController < ApplicationController
       @data_objects << dept.data_objects
       @data_objects.flatten!
     end
-    @selected_loc_groups = @user_config.view_loc_groups.split(', ').map{|lg|LocGroup.find(lg).id}
+    @selected_loc_groups = @user_config.view_loc_groups.split(', ').map{|a| a.to_i}
+#    raise @selected_loc_groups.to_yaml
     @selected_data_objects = @user_config.watched_data_objects.split(', ').map{|obj|DataObject.find(obj).id}
   end
 
