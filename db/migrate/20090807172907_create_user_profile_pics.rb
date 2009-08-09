8945e096cc9540160bb1b962e2edce9ac7b1fe7a
Bob <bob@bob-laptop.(none)>
2009-08-08 22:34:08 -0400

    User profile pictures work

--- db/migrate/20090807172907_create_user_profile_pics.rb
+++ db/migrate/20090807172907_create_user_profile_pics.rb
@@ -1,13 +1,13 @@
-class CreateUserProfilePic < ActiveRecord::Migration
+class CreateUserProfilePics < ActiveRecord::Migration
   def self.up
-    create_table :user_profile_pic do |t|
+    create_table :user_profile_pics do |t|
       t.references :user_profile_entry
       t.timestamps
     end
   end
 
   def self.down
-    drop_table :user_profile_pic
+    drop_table :user_profile_pics
   end
 end
 
--- test/unit/user_profile_pics_test.rb
+++ test/unit/user_profile_pics_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class UserProfilePicTest < ActiveSupport::TestCase
+class UserProfilePicsTest < ActiveSupport::TestCase
   # Replace this with your real tests.
   test "the truth" do
     assert true
