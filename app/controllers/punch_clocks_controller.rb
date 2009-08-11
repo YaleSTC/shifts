7c0d7272d61d7c1ba145a70fdd2932adfc8e44a7
somanyrobots <somers.ben@gmail.com>
2009-08-11 19:38:35 -0400

    punch clocks woot!  Also fixed a shift permissions bug.

--- app/controllers/punch_clocks_controller.rb
+++ app/controllers/punch_clocks_controller.rb
@@ -6,13 +6,11 @@ class PunchClocksController < ApplicationController
   
   def show
     @punch_clock = PunchClock.find(params[:id])
-    return unless require_owner(@punch_clock)
+    return unless require_owner_or_dept_admin(@punch_clock, @punch_clock.department)
   end
   
   def create
-    @punch_clock = PunchClock.new
-    @punch_clock.user = current_user
-    @punch_clock.department = @department
+    @punch_clock = PunchClock.new({:user => current_user, :department => current_department, :runtime => 0, :last_touched => Time.now})
     @punch_clock.save ? flash[:notice] = "Successfully clocked in." : flash[:error] = "Could not clock in."
     redirect_to dashboard_url
   end
@@ -23,25 +21,29 @@ class PunchClocksController < ApplicationController
   
   def edit
     @punch_clock = PunchClock.find_by_id(params[:id])
-    return unless require_owner_or_dept_admin(@punch_clock)
+    return unless require_owner_or_dept_admin(@punch_clock, @punch_clock.department)
   end
   
-  # Clocks out the punch clock
+  # Stops, restarts, or submits the punch clock depending on params
   def update
     @punch_clock = PunchClock.find(params[:id])
     return unless require_owner_or_dept_admin(@punch_clock, @punch_clock.department)
-    payform_item = PayformItem.new({:date => Date.today,
-                                    :category => Category.find_by_name("Punch Clocks"),
-                                    :hours => (Time.now - @punch_clock.created_at) / 3600.0, # sec -> hr
-                                    :description => params[:punch_clock][:description]})
-    payform_item.payform = Payform.build(@punch_clock.department, @punch_clock.user, Date.today)
-    if payform_item.save && @punch_clock.destroy 
-      flash[:notice] = "Successfully clocked out." 
-      redirect_to(current_user == @punch_clock.user ? dashboard_path : punch_clocks_path)
-    else
-      flash[:notice] = "Could not clock out."
-      render(:action => :edit)
+    @punch_clock.last_touched = Time.now
+    if params[:pause]
+      @punch_clock.paused = true
+      @punch_clock.runtime += Time.now - @punch_clock.last_touched
+    elsif params[:unpause]
+      @punch_clock.paused = false
+    elsif params[:submit_clock]    
+      payform_item = PayformItem.new({:date => Date.today,
+                                      :category => Category.find_by_name("Punch Clocks"),
+                                      :hours => (@punch_clock.runtime/3600), # sec -> hr
+                                      :description => params[:punch_clock][:description]})
+      payform_item.payform = Payform.build(@punch_clock.department, @punch_clock.user, Date.today)
+      flash[:notice] = "Successfully clocked out." if payform_item.save && @punch_clock.destroy  
     end
+    flash[:notice] = @punch_clock && @punch_clock.save ? "Successfully modified punch clock." : "Could not modify punch clock."
+    redirect_to dashboard_path
   end  
   
 # Cancels out the punch clock w/o adding time to payform
--- app/helpers/punch_clocks_helper.rb
+++ app/helpers/punch_clocks_helper.rb
@@ -1,2 +1,11 @@
 module PunchClocksHelper
+
+  def punch_clock_class(clock)
+    if clock.paused?
+      return "paused"
+    else
+      return "running"
+    end
+  end
+
 end
