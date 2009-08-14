48577f0b752ce57cfb55fc0c681bd5d73ee64d9c
somanyrobots <somers.ben@gmail.com>
2009-08-13 20:20:18 -0400

    more punch clocks, minor payform changes, more flash css

--- app/controllers/punch_clock_sets_controller.rb
+++ app/controllers/punch_clock_sets_controller.rb
@@ -1,4 +1,6 @@
 class PunchClockSetsController < ApplicationController
+  helper :punch_clocks
+
   layout "payforms"
   
   before_filter :require_department_admin
@@ -16,34 +18,75 @@ class PunchClockSetsController < ApplicationController
     @users_select = current_department.users.sort_by(&:name)
   end
 
-#  def edit
-#    @punch_clock_set = PunchClockSet.find(params[:id])
-#  end
-
   def create
-    begin
-      PunchClock.transaction do  
-        @punch_clock_set = PunchClockSet.new(params[:punch_clock_set])
-        @punch_clock_set.department_id = current_department.id
-        @punch_clock_set.save!
-        params[:users].remove_blank.each{|uid| PunchClock.create!(:description => @punch_clock_set.description, :department => current_department, :user_id => uid, :runtime => 0, :last_touched => Time.now) }
-        flash[:notice] = 'Mass punch clock was successfully created.'
+#    begin
+#      PunchClock.transaction do  
+#        @punch_clock_set = PunchClockSet.new(params[:punch_clock_set])
+#        @punch_clock_set.department_id = current_department.id
+#        @punch_clock_set.save!
+#        params[:users].remove_blank.each{|uid| PunchClock.create!(:description => @punch_clock_set.description, :department => current_department, :user_id => uid, :runtime => 0, :last_touched => Time.now) }
+#        flash[:notice] = 'Mass punch clock was successfully created.'
+#      end
+#      redirect_to punch_clock_sets_path
+#    rescue Exception => e
+#      flash[:error] = e.message
+#      redirect_to :action => "new"
+#    end
+    @punch_clock_set = PunchClockSet.new(params[:punch_clock_set])
+    @punch_clock_set.department_id = current_department.id
+    if @punch_clock_set.save
+      flash[:notice] = "Mass punch clock was successfully created."
+      failed = []      
+      params[:users].remove_blank.each do |uid|
+        clock = PunchClock.new({:description => @punch_clock_set.description, 
+                                :department => current_department, 
+                                :user_id => uid, 
+                                :runtime => 0, 
+                                :last_touched => Time.now, 
+                                :punch_clock_set_id => @punch_clock_set.id})
+        failed << User.find(uid).name unless clock.save
       end
-      redirect_to punch_clock_sets_path
-    rescue Exception => e
-      flash[:error] = e.message
-      redirect_to :action => "new"
+      flash[:warning] = "Could not clock in the following users: #{failed.to_sentence}." unless failed.empty?
+#      @punch_clock_sets = PunchClockSet.all
+      redirect_to :action => :index
+    else
+      flash[:error] = "Mass punch clock could not be created."
+      redirect_to :action => :new
     end
   end
 
   def update
-    @punch_clock_set = PunchClockSet.find(params[:id])
-    if @punch_clock_set.update_attributes(params[:punch_clock_set])
-      flash[:notice] = 'PunchClockSet was successfully updated.'
-      redirect_to punch_clock_sets_path
+    @punch_clock_set = PunchClockSet.new(params[:punch_clock_set])
+    @punch_clock_set.department_id = current_department.id
+    if @punch_clock_set.save
+      flash[:notice] = 'Mass punch clock was successfully created.'
+      failed = []      
+      flash[:warning] = "Could not clock in the following users: "
+      params[:users].remove_blank.each do |uid|
+        failed << uid unless PunchClock.create({:description => @punch_clock_set.description, 
+                                   :department => current_department, 
+                                   :user_id => uid, 
+                                   :runtime => 0, 
+                                   :last_touched => Time.now, 
+                                   :punch_clock_set_id => @punch_clock_set.id})
+      end
     else
-      render :action => "edit"
+      raise "squiddy!"
     end
+    
+#    begin
+#      PunchClock.transaction do  
+#        @punch_clock_set = PunchClockSet.new(params[:punch_clock_set])
+#        @punch_clock_set.department_id = current_department.id
+#        @punch_clock_set.save!
+#        params[:users].remove_blank.each{|uid| PunchClock.create!(:description => @punch_clock_set.description, :department => current_department, :user_id => uid, :runtime => 0, :last_touched => Time.now, :punch_clock_set_id => @punch_clock_set.id) }
+#        flash[:notice] = 'Mass punch clock was successfully created.'
+#      end
+#      redirect_to punch_clock_sets_path
+#    rescue Exception => e
+#      flash[:error] = e.message
+#      redirect_to :action => "new"
+#    end
   end
 
   def destroy
