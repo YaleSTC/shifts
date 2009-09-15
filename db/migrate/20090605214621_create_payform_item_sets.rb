f73f43fc6e4fcb2429187d20b422906a2097cee4
Dung Trung <dtt22@termite.zoo.cs.yale.edu>
2009-09-15 17:26:41 -0400

    Adding precision and scale to the hours columns which are decimals.  without this, scale is defaulted to 0, which is not good.

--- db/migrate/20090605214621_create_payform_item_sets.rb
+++ db/migrate/20090605214621_create_payform_item_sets.rb
@@ -3,14 +3,14 @@ class CreatePayformItemSets < ActiveRecord::Migration
     create_table :payform_item_sets do |t|
       t.references :category
       t.date :date
-      t.decimal :hours
+      t.decimal :hours, :precision => 10, :scale => 2
       t.text :description
       t.boolean :active
       t.references :approved_by
-      
+
     end
   end
-  
+
   def self.down
     drop_table :payform_item_sets
   end
