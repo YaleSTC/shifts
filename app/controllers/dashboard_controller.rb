febf746dd8273dddbe3c643e9a17781716ff0ea6
Adam Bray <adam.bray@yale.edu>
2009-09-25 15:41:01 -0400

    fixed my silly code mistake

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -10,7 +10,7 @@ class DashboardController < ApplicationController
 #    @shift = Shift.new
 
     @active_shifts = Shift.find(:all, :conditions => {:signed_in => true, :department_id => current_department.id}, :order => :start)
-    @upcoming_shifts = Shift.find(:all, :conditions => ["#{:user_id.to_sql_column} = ? and #{:end.to_sql_column} > ? and #{:department_id.to_sql_column} = ? and #{:scheduled.to_sql_column} = ? and #{:active.to_sql_column = ?}", current_user.to_sql, Time.now.to_sql, current_department.id.to_sql, true, true], :order => :start, :limit => 5)
+    @upcoming_shifts = Shift.find(:all, :conditions => ["#{:user_id.to_sql_column} = ? and #{:end.to_sql_column} > ? and #{:department_id.to_sql_column} = ? and #{:scheduled.to_sql_column} = ? and #{:active.to_sql_column} = ?", current_user.to_sql, Time.now.to_sql, current_department.id.to_sql, true, true], :order => :start, :limit => 5)
     @subs_you_requested = SubRequest.find(:all, :conditions => ["#{:end.to_sql_column} > ? AND #{:user_id.to_sql_column} = ?", Time.now.to_sql, current_user.id.to_sql]).sort_by(&:start)
     @subs_you_can_take = current_user.available_sub_requests([@department]).select{|sub| sub.end > Time.now}.sort_by(&:start)
 
