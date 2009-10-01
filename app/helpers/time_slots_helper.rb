1b68d6a40f87041137012550e5e23c5293aca4fd
Adam Bray <adam.bray@yale.edu>
2009-10-01 15:17:03 -0400

    fixing time slot view issue for timeslots that start at 8 pm

--- app/helpers/time_slots_helper.rb
+++ app/helpers/time_slots_helper.rb
@@ -25,7 +25,8 @@ module TimeSlotsHelper
   
   def fetch_timeslots(time_slot_day,location)
     result = []
-    timeslots = TimeSlot.all(:conditions => ['start > ? and start < ? and location_id = ?',time_slot_day.beginning_of_day,time_slot_day.end_of_day,location.id])
+    #timeslots = TimeSlot.all(:conditions => ['start >= ? and start < ? and location_id = ?',time_slot_day.beginning_of_day,time_slot_day.end_of_day,location.id])
+    timeslots = TimeSlot.on_day(time_slot_day).in_location(location)
     for timeslot in timeslots do
       if ((timeslot.start < timeslot.start.beginning_of_day + @dept_start_hour.hours) &&
          (timeslot.end    < timeslot.start.beginning_of_day + @dept_start_hour.hours)) ||
