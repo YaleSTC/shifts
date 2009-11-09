8fde2dad5d2f51c88c89ab3913717f834d2fe468
rofreg <rofreg@gmail.com>
2009-11-09 00:54:31 -0800

    report items now register change of ip address via a before_save filter

--- app/models/report_item.rb
+++ app/models/report_item.rb
@@ -1,9 +1,17 @@
 class ReportItem < ActiveRecord::Base
   belongs_to :report
   delegate :user, :to => 'report.shift'
-  validates_presence_of :content
+  validates_presence_of :content, :report_id
+  
+  before_save :check_for_ip_address_change
   
   def content_with_formatting
     content.sanitize_and_format
+  end
+  
+  def check_for_ip_address_change
+    #if ip has changed from previous line item, note this
+    previous_report_items = self.report.report_items
+    self.content += " [IP Address changed to #{self.ip_address}]" if (!previous_report_items.empty? and previous_report_items[-1].ip_address != self.ip_address)
   end
 end
