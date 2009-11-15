c68d2fdd07ef4768dd0235de82830798abda51cb
rofreg <rofreg@gmail.com>
2009-11-14 23:53:29 -0800

    removed unneccessary nested transactions; propagated some changes

--- app/controllers/payform_item_sets_controller.rb
+++ app/controllers/payform_item_sets_controller.rb
@@ -24,25 +24,23 @@ class PayformItemSetsController < ApplicationController
     
     begin
       PayformItemSet.transaction do
-        PayformItem.transaction do  
-          @payform_items = []
-        
-          users = User.find(params[:user_ids])
-          users.each do |user| 
-            payform_item = PayformItem.new(params[:payform_item_set])
-            payform_item.payform = Payform.build(current_department, user, date)
-            @payform_items << payform_item
-          end
-        
-          if @payform_item_set.save and @payform_item_set.payform_items << @payform_items
-            flash[:notice] = "Successfully created payform item set."
-            redirect_to @payform_item_set   
-          else
-            flash[:error] = @payform_item_set.errors.full_messages.to_sentence
-            @users_select = current_department.users.sort_by(&:name)
-            render :action => "new"
-          end 
+        @payform_items = []
+      
+        users = User.find(params[:user_ids])
+        users.each do |user| 
+          payform_item = PayformItem.new(params[:payform_item_set])
+          payform_item.payform = Payform.build(current_department, user, date)
+          @payform_items << payform_item
         end
+      
+        if @payform_item_set.save and @payform_item_set.payform_items << @payform_items
+          flash[:notice] = "Successfully created payform item set."
+          redirect_to @payform_item_set   
+        else
+          flash[:error] = @payform_item_set.errors.full_messages.to_sentence
+          @users_select = current_department.users.sort_by(&:name)
+          render :action => "new"
+        end 
       end
     rescue Exception => e
       flash[:error] = e.message
@@ -71,45 +69,43 @@ class PayformItemSetsController < ApplicationController
     
     begin 
       PayformItemSet.transaction do
-        PayformItem.transaction do
-          @old_payform_items.each do |old_payform_item|
-            if @delete_users.include?(old_payform_item.user) #get rid of it
-              old_payform_item.active = false
-              old_payform_item.source = current_user.name 
-              old_payform_item.reason = "#{current_user.name} removed you from this group job."
-              old_payform_item.save!
-              @payform_item_set.payform_items.delete(old_payform_item)
-            else #update with new values
-              new_item = PayformItem.new(params[:payform_item_set])
-              new_item.payform = Payform.build(current_department, old_payform_item.user, date)
-              new_item.source = current_user.name 
-              new_item.parent = old_payform_item 
+        @old_payform_items.each do |old_payform_item|
+          if @delete_users.include?(old_payform_item.user) #get rid of it
+            old_payform_item.active = false
+            old_payform_item.source = current_user.name 
+            old_payform_item.reason = "#{current_user.name} removed you from this group job."
+            old_payform_item.save!
+            @payform_item_set.payform_items.delete(old_payform_item)
+          else #update with new values
+            new_item = PayformItem.new(params[:payform_item_set])
+            new_item.payform = Payform.build(current_department, old_payform_item.user, date)
+            new_item.source = current_user.name 
+            new_item.parent = old_payform_item 
 
-              old_payform_item.reason = "#{current_user.name} changed this group job."
-              old_payform_item.payform = nil 
+            old_payform_item.reason = "#{current_user.name} changed this group job."
+            old_payform_item.payform = nil 
+      
+            new_item.save(false)
+            old_payform_item.save! 
+            new_item.save!
+            @payform_item_set.payform_items.delete(old_payform_item)
+            @payform_item_set.payform_items << new_item
+          end
+        end
+  
+        @add_users.each do |user| 
+          payform_item = PayformItem.new(params[:payform_item_set])
+          payform_item.payform = Payform.build(current_department, user, date)
+          @payform_item_set.payform_items << payform_item
+        end
         
-              new_item.save(false)
-              old_payform_item.save! 
-              new_item.save!
-              @payform_item_set.payform_items.delete(old_payform_item)
-              @payform_item_set.payform_items << new_item
-            end
-          end
-    
-          @add_users.each do |user| 
-            payform_item = PayformItem.new(params[:payform_item_set])
-            payform_item.payform = Payform.build(current_department, user, date)
-            @payform_item_set.payform_items << payform_item
-          end
-          
-          if @payform_item_set.update_attributes(params[:payform_item_set])
-            flash[:notice] = "Successfully updated payform item set."
-            redirect_to @payform_item_set   
-          else
-            flash[:error] = "Something failed. Please try again."
-            @users_select = current_department.users.sort_by(&:name)
-            render :action => "edit"
-          end
+        if @payform_item_set.update_attributes(params[:payform_item_set])
+          flash[:notice] = "Successfully updated payform item set."
+          redirect_to @payform_item_set   
+        else
+          flash[:error] = @payform_item_set.errors.full_messages.to_sentence
+          @users_select = current_department.users.sort_by(&:name)
+          render :action => "edit"
         end
       end    
     rescue Exception => e
