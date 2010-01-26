928a03ff532148f55296b5633479992158900d81
Adam Bray <adam.bray@yale.edu>
2010-01-26 10:39:00 -0500

    erroneous self in duration method

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -192,7 +192,7 @@ class Shift < ActiveRecord::Base
   # ==================
 
   def duration(actual = false)
-    (self.actual && self.report) ? self.report.duration : ((self.end - self.start)/3600 rescue 0)
+    (actual && self.report) ? self.report.duration : ((self.end - self.start)/3600 rescue 0)
   end
 
   def css_class(current_user = nil)
