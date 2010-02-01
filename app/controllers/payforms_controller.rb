efcb3d36466c5812b3bc317c20bfabdaceeecf0e
Adam Bray <adam.bray@yale.edu>
2010-01-31 23:16:49 -0500

    small improvement to show how many payforms left to approve on a given weekly batch

--- app/controllers/payforms_controller.rb
+++ app/controllers/payforms_controller.rb
@@ -71,7 +71,7 @@ class PayformsController < ApplicationController
     @payform.approved = Time.now
     @payform.approved_by = current_user
     if @payform.save
-      flash[:notice] = "Successfully approved payform."
+      flash[:notice] = "Successfully approved payform. #{Payform.unapproved.select{|p| p.date == @payform.date}.size} payform(s) left for this week."
     end
     @next_unapproved_payform = Payform.unapproved.select{|p| p.date == @payform.date}.sort_by(&:submitted).last
     @next_unapproved_payform ? (redirect_to @next_unapproved_payform and return) : (redirect_to :action => "index" and return)
