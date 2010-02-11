a1296e4ea0c88cd2c3755dd0d9b692cea331bbc1
Adam Bray <adam.bray@yale.edu>
2010-02-10 23:07:47 -0500

    including migration to add active column to data fields

---
+++ db/migrate/20100211015651_add_active_to_data_fields.rb
@@ -0,0 +1,9 @@
+class AddActiveToDataFields < ActiveRecord::Migration
+  def self.up
+    add_column :data_fields, :active, :boolean, :default => true
+  end
+
+  def self.down
+    remove_column :data_fields, :active
+  end
+end
