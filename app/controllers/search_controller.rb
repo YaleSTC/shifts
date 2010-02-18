a6a05294f06ed5d9281f5c3f299abc5b4fce9c13
Adam Bray <adam.bray@yale.edu>
2010-02-17 21:12:22 -0500

    minor changes to searching

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,8 @@
 class SearchController < ApplicationController
   def index
-    @shift_results = ReportItem.search(params[:search_text]).map{|r| r.report.shift}
+    @report_item_results = ReportItem.search(params[:search_text])
+    @shift_results = @report_item_results.map{|r| r.report.shift}
+    
     @user_results = User.search(params[:search_text])
   end
 
