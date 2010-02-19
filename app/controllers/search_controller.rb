dc1ca83cf63a8dfb41009f9425a9862d979f35e6
Adam Bray <adam.bray@yale.edu>
2010-02-19 01:55:10 -0500

    Improved parsing and display of search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,12 @@
 class SearchController < ApplicationController
   def index
     @report_item_results = ReportItem.search(params[:search_text])
-    @shift_results = @report_item_results.map{|r| r.report.shift}
+    @shift_results = @report_item_results.map{|r| r.report.shift}.uniq
+    
+    @results_hash = {}
+    @shift_results.each do |s|
+      @results_hash[s] = @report_item_results.select{|r| r.report.shift == s}
+    end
     
     @user_results = User.search(params[:search_text])
   end
