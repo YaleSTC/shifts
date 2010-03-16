2b1fa0c459be11cad044973d7b40bae718365f34
Adam Bray <adam.bray@yale.edu>
2010-03-16 11:13:06 -0400

    Improving test fixtures, cleaning up project root

--- test/unit/CAS_test.rb
+++ test/unit/CAS_test.rb
@@ -7,16 +7,12 @@ end
 module CasTestHelper
   def assert_redirected_to_login
     assert_response :redirect
-    assert_match(/
-#{CASClient::Frameworks::Rails::Filter.config[:login_url]}/,
-redirect_to_url)
+    assert_match(/#{CASClient::Frameworks::Rails::Filter.config[:login_url]}/,redirect_to_url)
   end
 
   def stub_cas_logged_in(user)
     unstub_cas
-
-CASClient::Frameworks::Rails::Filter.singleton_class.send :define_method, :filter
-do |controller|
+    CASClient::Frameworks::Rails::Filter.singleton_class.send :define_method, :filter do |controller|
       controller.session[:cas_user] = user.cas_username
       controller.session[:user] = user.id
       true
@@ -25,9 +21,7 @@ do |controller|
 
   def stub_cas_logged_out
     unstub_cas
-
-CASClient::Frameworks::Rails::Filter.singleton_class.send :define_method, :filter
-do |controller|
+    CASClient::Frameworks::Rails::Filter.singleton_class.send :define_method, :filter do |controller|
       controller.send(:redirect_to, 'access/denied')
       controller.send(:reset_session)
       false
