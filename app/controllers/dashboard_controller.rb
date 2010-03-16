7f2138ca2373c92d4699d1d6dc13b4dcb1f0c2de
Adam Bray <adam.bray@yale.edu>
2010-03-16 14:19:45 -0400

    Admin can now sign out shifts in report view page, not just tooltip view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,7 +7,7 @@ class DashboardController < ApplicationController
     @signed_in_shifts = Shift.signed_in_shifts_by_location_in_department(current_department)
     @upcoming_shifts = Shift.find(:all, :conditions => ["#{:user_id} = ? and #{:end} > ? and #{:department_id} = ? and #{:scheduled} = ? and #{:active} = ?", current_user, Time.now.utc, current_department.id, true, true], :order => :start, :limit => 5)
 
-    @subs_you_requested = SubRequest.find(:all, :conditions => ["#{:end} > ? AND #{:user_id} = ?", Time.now.utc, current_user.id]).sort_by(&:start)
+    @subs_you_requested = SubRequest.find(:all, :conditions => ["end > ? AND user_id = ?", Time.now.utc, current_user.id]).sort_by(&:start)
     @subs_you_can_take = current_user.available_sub_requests([@department]).select{|sub| sub.end > Time.now.utc}.sort_by(&:start)
 
     @watched_objects = DataObject.find(current_user.user_config.watched_data_objects.split(', ')).group_by(&:data_type)
