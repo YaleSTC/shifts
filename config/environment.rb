36c997c38f6121f1579f45c27ae477c834e4f114
Adam Bray <adam.bray@yale.edu>
2010-03-15 22:15:42 -0400

    Cleaned up environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,13 +1,7 @@
 # Be sure to restart your server when you modify this file
 
-# Uncomment below to force Rails into production mode when
-# you don't control web/app server and can't set it the proper way
-# ENV['RAILS_ENV'] ||= 'production'
-
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
-# for Heroku:
-# RAILS_GEM_VERSION = '2.3.2'
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -50,6 +44,7 @@ Rails::Initializer.run do |config|
   config.gem 'ar_mailer', :lib => 'action_mailer/ar_mailer'
   config.gem "whenever", :lib => false
   config.gem 'hoptoad_notifier'
+  
   # Only load the plugins named here, in the order given. By default, all plugins
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
@@ -66,16 +61,6 @@ Rails::Initializer.run do |config|
   # in the database in UTC, and return them converted to the specified local zone.
   # Run "rake -D time" for a list of tasks for finding time zone names. Comment line to use default local time.
   config.time_zone = 'Eastern Time (US & Canada)'
-
-  # The internationalization framework can be changed to have another default locale (standard is :en) or more load paths.
-  # All files from config/locales/*.rb,yml are added automatically.
-  # config.i18n.load_path << Dir[File.join(RAILS_ROOT, 'my', 'locales', '*.{rb,yml}')]
-  # config.i18n.default_locale = :de
-
-  # Use SQL instead of Active Record's schema dumper when creating the test database.
-  # This is necessary if your schema can't be completely dumped by the schema dumper,
-  # like if you have constraints or database-specific column types
-  # config.active_record.schema_format = :sql
 
   # Activate observers that should always be running
   # Please note that observers generated using script/generate observer need to have an _observer suffix
