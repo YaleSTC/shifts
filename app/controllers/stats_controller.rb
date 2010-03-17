a3b4812e48886604b3a52c7de036f6af186581ea
Adam Bray <adam.bray@yale.edu>
2010-03-17 01:55:23 -0400

    Prelim stats feature is complete

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,28 +1,34 @@
 class StatsController < ApplicationController
+
   def index
-      @start_date = params[:start_date] || 1.week.ago
-      @start_date = params[:end_date] || Date.today
+      return unless user_is_admin_of(current_department)
+      @start_date = params[:start_date] ? Date.parse(params[:start_date]) : 1.week.ago.to_date
+      @end_date = params[:end_date] ? Date.parse(params[:end_date]) : 1.day.ago.to_date
+
       @stats = {}
 
-      all_users = current_department.active_users
+      users = current_department.active_users.sort_by(&:last_name)
 
-      all_users.each do |u|
+      users.each do |u|
   	    
-  	    @user_stats = {}
+  	    user_stats = {}
+  	    
   	    shifts = u.shifts.on_days(@start_date, @end_date).active
-  	    reports = shifts.map( &:report).compact
 
-  	    @user_stats[:num_shifts] = shifts.size
-  	    @user_stats[:num_reports] = reports.size
-  	    @user_stats[:num_late] = reports.select{|r| r.late?}.size
-  	    @user_stats[:num_left_early] = reports.select{|r| r.left_early?}.size 
-        @user_stats[:num_missed] = num_shifts - num_reports
-                                         
-  	    @user_stats[:scheduled_duration] = shifts.map{|s| s.duration(actual = false)}.sum
-  	    @user_stats[:real_duration] = shifts.map{|s| s.duration(actual = true)}.sum
+  	    user_stats[:name] = u.name
+        
+        user_stats[:num_shifts] = shifts.size
+  	    user_stats[:num_late] = shifts.select{|s| s.late?}.size
+        user_stats[:num_missed] = shifts.select{|s| s.missed?}.size
+  	    user_stats[:num_left_early] = shifts.select{|s| s.left_early?}.size 
+        #user_stats[:scheduled_duration] = shifts.map{|s| s.duration(actual = false)}.sum
+  	    #user_stats[:actual_duration] = shifts.map{|s| s.duration(actual = true)}.sum
+                
+        @stats[u.id] = user_stats
 
-  	    print "\t", num_shifts, "\t", num_reports, "\t", num_missed, "\t", num_late, "\t",  num_left_early, "\t", scheduled_duration,"\t", real_duration, "\n"
       end
+  
+
     end
 
   def show
