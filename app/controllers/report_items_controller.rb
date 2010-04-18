db94382ab535071fcfb565d44ae3b0dbb05f385d
Adam Bray <adam.bray@yale.edu>
2010-04-17 23:17:08 -0400

    fixed a bug that lead to nilMethodErrors when adding new report item with no content

--- app/controllers/report_items_controller.rb
+++ app/controllers/report_items_controller.rb
@@ -18,13 +18,14 @@ class ReportItemsController < ApplicationController
     @report_item.ip_address = request.remote_ip
     @report_item.report = params[:report_id] ? Report.find(params[:report_id]) : Shift.find(params[:shift_id]).report
     respond_to do |format|
+      @report = Report.find(@report_item.report_id)
       if current_user==@report_item.user && @report_item.save
-        @report = Report.find(@report_item.report_id)
         format.html {redirect_to @report}
         format.js
       else
         flash[:notice] = "You can't add things to someone else's report!" if @report_item.user != current_user
-        redirect_to @report_item.report
+        format.html {redirect_to @report}
+        format.js {redirect_to @report}
       end
     end
   end
