80e6f9d6c3a01b99a905f3604aff70ad3a367cc1
Derek Zhao <derek.zhao@yale.edu>
2010-06-12 22:12:11 -0400

    Errors now work ajaxly again, errors for links cleaned up, sticky controller cleaned up

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,42 +1,19 @@
 class LinksController < NoticesController
-  # GET /links
-  # GET /links.xml
+
   def index
-    @links = Link.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @links }
-    end
+    redirect_to(notices_path)
   end
 
-  # GET /links/1
-  # GET /links/1.xml
-  def show
-    @link = Link.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @link }
-    end
-  end
-
-  # GET /links/new
-  # GET /links/new.xml
   def new
     @link = Link.new
-
-
 		layout_check
   end
 
-  # GET /links/1/edit
   def edit
     @link = Link.find(params[:id])
   end
 
-  # POST /links
-  # POST /links.xml
   def create
     @link = Link.new(params[:link])
 		@link.author = current_user		
@@ -66,25 +43,6 @@ class LinksController < NoticesController
     end
   end
 
-  # PUT /links/1
-  # PUT /links/1.xml
-  def update
-    @link = Link.find(params[:id])
-
-    respond_to do |format|
-      if @link.update_attributes(params[:link])
-        flash[:notice] = 'Link was successfully updated.'
-        format.html { redirect_to(@link) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @link.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /links/1
-  # DELETE /links/1.xml
   def destroy
 		redirect_to :controller => 'notice', :action => 'destroy'
   end
