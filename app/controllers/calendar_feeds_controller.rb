8f82514b1331f1131fd179053f5bda90e7432e3c
Bay Gross <bay.gross@yale.edu>
2010-06-18 12:30:45 -0400

    eric just showed up.  pushing so we can collaborate, etc

--- app/controllers/calendar_feeds_controller.rb
+++ app/controllers/calendar_feeds_controller.rb
@@ -11,7 +11,7 @@ class CalendarFeedsController < ApplicationController
   end
 
   def grab
-    @source = resolve_token(params[:token])
+    @source = resolve_token(params[:token], params[:user_id])
     @user = User.find(params[:user_id])
 
     # @shifts = @source.get_viewable_shifts(@user)     #model after shifts/calendar logic we already have
@@ -24,17 +24,33 @@ class CalendarFeedsController < ApplicationController
   private
   
   def generate_token(source)
-    if current_user.calendar_feed_hash.blank?
-      current_user.calendar_feed_hash = SecureRandom.hex(25)
+    if !current_user.calendar_feed_hash
+      current_user.calendar_feed_hash = SecureRandom.hex(32)    #must be 32 char long to match cipher algorithm
+      current_user.save!
     end
-      #Encryptor.encrypt(source.class.name + "," + source.id.to_s, :key => current_user.calendar_feed_hash)
-      1234567890
+      require 'openssl'
+      require 'digest/sha2'
+      cipher = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
+      cipher.encrypt
+      cipher.key = "gd345z68k223h70xkcdgd345z68k223h"
+      cipher.iv = current_user.calendar_feed_hash
+      @token = cipher.update(source.class.name.to_s + ',' +  source.id.to_s)
+      @token << cipher.final
+      @token.unpack("H*")
   end
   
-  def resolve_token(token)
-   # @decrypted_value = Encryptor.decrypt(:value => encrypted_value, :key => secret_key) 
-   # @decrypted_value.split('.')[0].constantize.find(@decrypted_value.split('.')[1])
-   Department.first
+  def resolve_token(token, user_id)
+    require 'openssl'
+    require 'digest/sha2'
+    cipher = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
+    cipher.decrypt
+    cipher.key = "gd345z68k223h70xkcdgd345z68k223h"
+    cipher.iv = User.find(user_id).calendar_feed_hash
+    @token=token.to_a.pack("H*")
+
+    @source_string = cipher.update(@token)
+    @source_sting << cipher.final
+    @source_string.split('.')[0].constantize.find(@decrypted_value.split('.')[1])   #this will fail ugly with bad url
   end
 
   def generate_ical
