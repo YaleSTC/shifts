94bf411ded4840b5ba6212b606729211d4da98a5
Smudge <originalsmudge@gmail.com>
2010-06-18 20:02:50 -0400

    more unobtrusive-ifying: made more use of my toggle methods

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,22 +5,32 @@
 $(document).ready(function() {
     // If javascript is enabled, anything with the class 'no_js' will be hidden
     $('.no_js').hide();
-    
+
+    // If javascript is enabled, anything with the class 'no_js' will be hidden
+    $('.only_js').show();
+ 
     //$('#day_#{day.to_s}').slideToggle('200')
     
     // When any form with the class "onchange_submit" is altered, the form gets submitted.
     $('.onchange_submit').change(function() { $
         (this).submit() 
     });
-    
+
+
+    //Anything of class "trigger" will cause the next thing to be toggled. (Use if you have a header directly above the thing it toggles)
+    //Also, the trigger will gain the class "triggered" in case any styling needs to be changed on the trigger
+	$(".trigger").click(function(){
+		$(this).toggleClass("triggered").next().slideToggle('200');
+		event.preventDefault(); //don't actually follow the link/action (even '#' goes to top of page in some cases)
+	});  
     //Anything of class "trigger-<id>" will cause something of class "toggle-<id>" with the same <id> to be toggled
     //Also, the trigger will gain the class "triggered" in case any styling needs to be changed on the trigger
-	$("[class^=trigger-]").click(function(){
+	$("[class*=trigger-]").click(function(){
 		$(".toggle-"+$(this).toggleClass("triggered").attr("class").match(/trigger-((\w|-)+)\b/)[1]).slideToggle('200');
-		event.preventDefault(); //don't actually follow the link (even '#' goes to top of page in some cases)
+		event.preventDefault(); //don't actually follow the link/action (even '#' goes to top of page in some cases)
 	});
     //Also, make all triggers links
-    $("[class^=trigger-]").each(function() {
+    $("[class*=trigger]").each(function() {
         $(this).html("<a href='#'>"+$(this).text()+"</a>");
     });
 
