c97898e10f65942e3f5580befa08afbcfdf1fddb
Smudge <originalsmudge@gmail.com>
2010-06-18 20:17:30 -0400

    more simplification of toggling. sliding looks different -- timeslots get smooshed while shifts just slide. i think more info can be found here: http://www.learningjquery.com/2009/02/slide-elements-in-different-directions

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -20,13 +20,13 @@ $(document).ready(function() {
     //Anything of class "trigger" will cause the next thing to be toggled. (Use if you have a header directly above the thing it toggles)
     //Also, the trigger will gain the class "triggered" in case any styling needs to be changed on the trigger
 	$(".trigger").click(function(){
-		$(this).toggleClass("triggered").next().slideToggle('200');
+		$(this).toggleClass("triggered").next().slideToggle('fast');
 		event.preventDefault(); //don't actually follow the link/action (even '#' goes to top of page in some cases)
 	});  
     //Anything of class "trigger-<id>" will cause something of class "toggle-<id>" with the same <id> to be toggled
     //Also, the trigger will gain the class "triggered" in case any styling needs to be changed on the trigger
 	$("[class*=trigger-]").click(function(){
-		$(".toggle-"+$(this).toggleClass("triggered").attr("class").match(/trigger-((\w|-)+)\b/)[1]).slideToggle('200');
+		$(".toggle-"+$(this).toggleClass("triggered").attr("class").match(/trigger-((\w|-)+)\b/)[1]).slideToggle('fast');
 		event.preventDefault(); //don't actually follow the link/action (even '#' goes to top of page in some cases)
 	});
     //Also, make all triggers links
