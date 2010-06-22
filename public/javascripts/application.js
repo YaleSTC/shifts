9bdaca2155901129407a4981e9ea407f178ff2e1
Smudge <originalsmudge@gmail.com>
2010-06-22 15:46:38 -0400

    unobtrusive-ifying clean-up

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,25 +8,23 @@ $(document).ready(function() {
 
     // If javascript is enabled, anything with the class 'no_js' will be hidden
     $('.only_js').show();
- 
-    //$('#day_#{day.to_s}').slideToggle('200')
     
     // When any form with the class "onchange_submit" is altered, the form gets submitted.
     $('.onchange_submit').change(function() { $
-        (this).submit() 
+        (this).submit();
     });
 
 
     //Anything of class "trigger" will cause the next thing to be toggled. (Use if you have a header directly above the thing it toggles)
     //Also, the trigger will gain the class "triggered" in case any styling needs to be changed on the trigger
 	$(".trigger").click(function(){
-		$(this).toggleClass("triggered").next().slideToggle('fast');
+		$(this).toggleClass("triggered").next().slideToggle('200');
 		event.preventDefault(); //don't actually follow the link/action (even '#' goes to top of page in some cases)
 	});  
     //Anything of class "trigger-<id>" will cause something of class "toggle-<id>" with the same <id> to be toggled
     //Also, the trigger will gain the class "triggered" in case any styling needs to be changed on the trigger
 	$("[class*=trigger-]").click(function(){
-		$(".toggle-"+$(this).toggleClass("triggered").attr("class").match(/trigger-((\w|-)+)\b/)[1]).slideToggle('fast');
+		$(".toggle-"+$(this).toggleClass("triggered").attr("class").match(/trigger-((\w|-)+)\b/)[1]).slideToggle('200');
 		event.preventDefault(); //don't actually follow the link/action (even '#' goes to top of page in some cases)
 	});
     //Also, make all triggers links
