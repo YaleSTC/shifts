5c6d2ecd9b008595308656cd9096aa60f5d96888
Smudge <originalsmudge@gmail.com>
2010-06-22 17:06:39 -0400

    added options to tokenized autocomplete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -28,33 +28,53 @@ module ApplicationHelper
     current_user.current_shift
   end
 
-  def tokenized_users_autocomplete(object, field, id)
+  def tokenized_users_autocomplete(object, field, options = {})
+    #set defaults
+    options.reverse_merge!({
+      :id => "list_of_logins",
+      :hint_text => "Type a name, NetID, role or department",
+      :style => "vertical" #default to vertical style -- seems more appropriate
+    })
+    
+
+    if (options[:style] == "facebook")
+      style = 'tokenList: "token-input-list-facebook",
+              token: "token-input-token-facebook",
+              tokenDelete: "token-input-delete-token-facebook",
+              selectedToken: "token-input-selected-token-facebook",
+              highlightedToken: "token-input-highlighted-token-facebook",
+              dropdown: "token-input-dropdown-facebook",
+              dropdownItem: "token-input-dropdown-item-facebook",
+              dropdownItem2: "token-input-dropdown-item2-facebook",
+              selectedDropdownItem: "token-input-selected-dropdown-item-facebook",
+              inputToken: "token-input-input-token-facebook"'
+      css_file = 'tokeninput-facebook'
+    else
+      style = ''
+      css_file = 'token-input'
+    end
+    
     json_string = ""
     unless object.nil? or field.nil?
       object.send(field).each do |user_source|
         json_string += "{name: '#{user_source.name}', id: '#{user_source.class}||#{user_source.id}'},\n"
       end
     end
-
-    '<script type="text/javascript">
-        $(document).ready(function() {
-            $("#'+id+'").tokenInput("'+autocomplete_department_users_path(current_department)+'", {
-                prePopulate: ['+json_string+'],
-                classes: {
-                    tokenList: "token-input-list-facebook",
-                    token: "token-input-token-facebook",
-                    tokenDelete: "token-input-delete-token-facebook",
-                    selectedToken: "token-input-selected-token-facebook",
-                    highlightedToken: "token-input-highlighted-token-facebook",
-                    dropdown: "token-input-dropdown-facebook",
-                    dropdownItem: "token-input-dropdown-item-facebook",
-                    dropdownItem2: "token-input-dropdown-item2-facebook",
-                    selectedDropdownItem: "token-input-selected-dropdown-item-facebook",
-                    inputToken: "token-input-input-token-facebook"
-                }
-            });
-        });
-        </script>' + text_field_tag(id)
+    
+    #Tell the app to put javascript info at top and bottom of pages (Unobtrusive Javascript - style)
+    content_for :javascript, 
+      '$(document).ready(function() {
+        $("#'+options[:id]+'").tokenInput("'+autocomplete_department_users_path(current_department)+'", {
+            prePopulate: ['+json_string+'],
+            hintText: "'+options[:hint_text]+'",
+            classes: {
+              '+style+'
+            }
+          });
+        });'
+    content_for :head, javascript_include_tag('jquery.tokeninput')
+    content_for :head, stylesheet_link_tag(css_file)
+    text_field_tag(options[:id])
   end
 
 
