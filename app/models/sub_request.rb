8b575caf2e528c842dff5e1545b560c898a6aed1
Bay Gross <bay.gross@yale.edu>
2010-06-24 14:03:06 -0400

    picking cherries all day yo

--- app/models/sub_request.rb
+++ app/models/sub_request.rb
@@ -112,7 +112,6 @@ class SubRequest < ActiveRecord::Base
   def mandatory_start_and_end_are_within_subrequest
     unless self.mandatory_start.between?(self.start, self.end) && self.mandatory_end.between?(self.start, self.end)
       errors.add_to_base("The mandatory portion of this sub request must be within the optional portion.")
-
     end
   end
 
