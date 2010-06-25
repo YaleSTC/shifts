2aaa4b01e4f88cd14780e10efd74cb78ca5ec3ce
Casey Watts <casey.s.watts@gmail.com>
2010-06-25 13:18:57 -0400

    every time input now has a date and a time field separately. The models have these respective attributes which aren't saved in the database, but used to construct start and end which are. The parsing function was updated to interpret each, and functions in the model are used to join them. Shifts, sub requests, and time slots have been modified.

--- app/models/restriction.rb
+++ app/models/restriction.rb
@@ -2,15 +2,18 @@ class Restriction < ActiveRecord::Base
   belongs_to :department
 
   validates_presence_of :starts, :expires
-  
   validates_presence_of :max_hours,  :unless => :max_subs, :message => "and Max subs can't both be blank"
+  attr_accessor :start_date
+  attr_accessor :start_time
+  attr_accessor :end_date
+  attr_accessor :end_time
 
   named_scope :current, lambda {{ :conditions => ["#{:starts.to_sql_column} <= #{Time.now.to_sql} and #{:expires.to_sql_column} >= #{Time.now.to_sql}"]}}
 
   def users
     self.user_sources.collect{|s| s.users}.flatten.uniq
   end
-  
+
   def locations
     self.location_sources.collect{|s| s.locations}.flatten.uniq
   end
