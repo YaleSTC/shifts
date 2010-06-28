62feab8eebd4614416380dac94d2895ee84a9f6f
Smudge <originalsmudge@gmail.com>
2010-06-28 04:39:01 -0400

    added script for updating old payform_items to versioned payform_items -- may or may not be working

---
+++ db/migrate/20100628071549_update_payform_items_to_versioned.rb
@@ -0,0 +1,58 @@
+class UpdatePayformItemsToVersioned < ActiveRecord::Migration
+  def self.up
+    say_with_time "Converting payform_items to versioned items" do
+      #find "version one" items
+      PayformItem.find(:all,:conditions => {:parent_id => nil}).each do |p_i|
+        
+        #NOTE: probably make sure payform_items aren't already versioned when you run this script
+        
+        #ALSO,  BACKUP YOUR DATABASE BEFORE RUNNING THIS!!!
+        
+        #merge in old acts_as_tree records
+        kids =  PayformItem.all.select{|p| p.parent_id == p_i.id}
+        # IF DELETED
+        ############
+        if (kids.empty? and !p_i.active) 
+          p_i.active = true
+          reason = p_i.reason
+          p_i.updated_by = p_i.source
+          p_i.reason = nil
+          p_i.save! #activate versioned on this item, initially active
+          p_i.active = false
+          p_i.reason = reason
+          p_i.updated_by = p_i.source
+          p_i.save! #now we delete this item with versioning
+        # ELSE RECURSE THROUGH KIDS
+        ###########################
+        else 
+          p_i.updated_by = p_i.source
+          reason = p_i.reason
+          p_i.reason = nil
+          p_i.active = true
+          p_i.save!
+          while !kids.empty?
+            kid = kids.first
+            new_reason = reason
+            reason = kid.reason
+            p_i.attributes = kid.attributes
+            p_i.reason = new_reason
+            p_i.updated_by = kid.source
+            kids =  PayformItem.all.select{|p| p.parent_id == kid.id}
+            unless (kids.empty?)
+              p_i.active = true
+            end
+            p_i.save!
+            kid.destroy
+          end
+        end
+      end
+    end
+  end
+
+  def self.down
+    #THIS IS IRREVERSABLE (BE WARNED)
+    #(unless you want to write this method.)
+    #(it is theoretically possible...)
+  end
+  
+end
