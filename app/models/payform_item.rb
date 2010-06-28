19151823623b14dac9b8c2dc8c4ebaff8d73e3ce
Smudge <originalsmudge@gmail.com>
2010-06-28 16:51:59 -0400

    cleaned up and modified versioning script to generate new payform items (this makes it much easier)

--- app/models/payform_item.rb
+++ app/models/payform_item.rb
@@ -18,11 +18,11 @@ class PayformItem < ActiveRecord::Base
   delegate :user, :to => :payform  
   
   validates_presence_of :date, :category_id, :payform_id
+  validates_numericality_of :hours, :greater_than => 0
+  validates_presence_of :reason, :on => :update
   validate :length_of_description
-  validate_on_update :proper_reason
-  validates_numericality_of :hours
-  validate :hours_greater_than_zero
-  
+  validate_on_update :length_of_reason
+
   named_scope :active, :conditions => {:active =>  true}
   named_scope :in_category, lambda { |category| { :conditions => {:category_id => category.id}}}
   
@@ -34,29 +34,15 @@ class PayformItem < ActiveRecord::Base
   end
   
   protected
-  
-  def hours_greater_than_zero
-    unless self.hours.to_f > 0
-      errors.add_to_base("Your payform item has no hours.") 
-    end
-  end
 
   def length_of_description
-    min =  self.department.department_config.description_min.to_i 
-    if self.description.length < min
-      errors.add_to_base("Description must be at least #{min} characters long.") 
-    end
+    min = self.department.department_config.description_min.to_i 
+    errors.add(:description, "must be at least #{min} characters long.") if self.description.length < min
   end   
   
-  def proper_reason
-    unless self.version == 1 #we haven't done any versioning yet
-      min = self.department.department_config.reason_min.to_i
-      unless (self.reason.nil?)
-        errors.add_to_base("Reason must be at least #{min} characters long.") if self.reason.length < min
-      else
-        errors.add_to_base("You must include a reason.")
-      end
-    end
+  def length_of_reason
+    min = self.department.department_config.reason_min.to_i
+    errors.add(:reason, "must be at least #{min} characters long.") if self.reason.length < min
   end   
 
   def validate
