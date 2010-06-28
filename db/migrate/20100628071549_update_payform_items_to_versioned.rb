913dfb89729f0de0fac462816ae0a154b1ddb402
Smudge <originalsmudge@gmail.com>
2010-06-28 16:51:59 -0400

    cleaned up and modified versioning script to generate new payform items (this makes it much easier)

--- db/migrate/20100628071549_update_payform_items_to_versioned.rb
+++ db/migrate/20100628071549_update_payform_items_to_versioned.rb
@@ -1,14 +1,15 @@
 class UpdatePayformItemsToVersioned < ActiveRecord::Migration
   
   def self.up
-    say_with_time "Converting payform_items to versioned items" do
-      #find "version one" items
+    say_with_time "Adding proper payform_id to all payform_items" do
+      
+      #NOTE: probably make sure payform_items aren't already versioned when you run this script  
+      #ALSO,  BACKUP YOUR DATABASE BEFORE RUNNING THIS!!!      
+      
       PayformItem.find(:all,:conditions => {:parent_id => nil}).each do |p_i|
-        
-        #NOTE: probably make sure payform_items aren't already versioned when you run this script
-        
-        #ALSO,  BACKUP YOUR DATABASE BEFORE RUNNING THIS!!!
-        
+        #make sure payform_id is assigned to all payforms, based off of their kids
+        #this method is made longer because I am only assuming that parent_id exists,
+        #  NOT that acts_as_tree is enabled
         while p_i.payform.nil?
           kid =  PayformItem.all.select{|p| p.parent_id == p_i.id}.first
           if kid.payform.nil?
@@ -16,67 +17,67 @@ class UpdatePayformItemsToVersioned < ActiveRecord::Migration
           else
             payform = kid.payform
             p_i.payform = payform
-            p_i.skip_version do
-              p_i.save(false)
-            end
+            p_i.save(false)
             while !p_i.parent_id.nil?
               p_i = PayformItem.find(p_i.parent_id)
               p_i.payform = payform
-              p_i.skip_version do
-                p_i.save(false)
-              end
+              p_i.save(false)
             end
           end
         end
         
+      end
+    end
+    say_with_time "Regenerating payform_items as versioned items" do
+      PayformItem.find(:all,:conditions => {:parent_id => nil}).each do |p_i| 
+
         #p_i.reset_to!(1)
+        
+        new_p_i = PayformItem.new(p_i.attributes)
         
         #merge in old acts_as_tree records
         kids =  PayformItem.all.select{|p| p.parent_id == p_i.id}
+
+        #set initial version active
+        reason = p_i.reason
+        new_p_i.active = true
+        new_p_i.updated_by = p_i.source
+        new_p_i.reason = nil
+        new_p_i.save!(false)
+     
         # IF DELETED
         ############
         if (kids.empty? and !p_i.active)
-          reason = p_i.reason
-          p_i.skip_version do
-            p_i.active = true
-            p_i.updated_by = p_i.source
-            p_i.reason = nil
-            p_i.save!(false) #set the initial version
-          end
-          p_i.append_version do
-            p_i.active = false
-            p_i.reason = reason
-            p_i.updated_by = p_i.source
-            p_i.save!(false) #now we delete this item with versioning
-          end
+
+          #now we delete it
+          new_p_i.active = false
+          new_p_i.reason = reason
+          new_p_i.updated_by = p_i.user.name #may not be accurate if an admin deleted it
+          new_p_i.save!(false)
+
         # ELSE RECURSE THROUGH KIDS
         ###########################
         else 
-          reason = p_i.reason
-          p_i.skip_version do
-            p_i.updated_by = p_i.source
-            p_i.reason = nil
-            p_i.active = true
-            p_i.save!(false) #set the initial version
-          end
-
-          while !kids.empty?
-            p_i.append_version do
-              kid = kids.first
-              new_reason = reason
-              reason = kid.reason              
-              p_i.attributes = kid.attributes
-              p_i.reason = new_reason
-              p_i.updated_by = kid.source
-              kids =  PayformItem.all.select{|p| p.parent_id == kid.id}
-              unless (kids.empty?)
-                p_i.active = true
-              end
-              p_i.save!(false) #update version based off of kid
-              kid.destroy #no longer need kid
+          while (!kids.empty?)
+            kid = kids.first
+            new_reason = reason
+            reason = kid.reason              
+            new_p_i.attributes = kid.attributes
+            new_p_i.reason = new_reason
+            new_p_i.updated_by = p_i.user.name #may not be accurate if an admin edited it
+            old_kids = kids
+            kids =  PayformItem.all.select{|p| p.parent_id == kid.id}
+            unless (kids.empty?)
+              new_p_i.active = true
             end
+            new_p_i.save!(false) #update version based off of kid
+            old_kids.map{|k| k.destroy } #no longer need this layer of kids (should only have one)
           end
         end
+
+        #destroy the old payform item
+        p_i.destroy
+
       end
     end
   end
