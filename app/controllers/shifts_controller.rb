053e0d9f999899e32840396a3eb37740e6691e69
Casey Watts <casey.s.watts@gmail.com>
2010-06-30 18:31:51 -0400

    put the calculate_default_times function in the helper where it belongs. The past hour hath been wasted on finding the places in the controller it needed to be called.

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -64,7 +64,6 @@ class ShiftsController < ApplicationController
   def show
     begin
       @shift = Shift.find(params[:id])
-      calculate_default_times
     rescue ActiveRecord::RecordNotFound
       respond_to do |format|
         format.html #freak out
@@ -97,7 +96,6 @@ class ShiftsController < ApplicationController
   def new
     params[:shift][:end] ||= params[:shift][:start] if params[:shift] and params[:shift][:start]
     @shift = Shift.new(params[:shift])
-    calculate_default_times
 # TODO - unecessary? they never seem to be called ~Casey
 #    if params[:tooltip]
 #      @shift.user_id = current_user.id
@@ -148,8 +146,7 @@ class ShiftsController < ApplicationController
         format.html{ flash[:notice] = "Successfully created shift."; redirect_to(shifts_path)}
         format.js
       end
-    else
-      calculate_default_times
+  else
       respond_to do |format|
         format.html{ render :action => 'new' }
         format.js do
@@ -168,7 +165,6 @@ class ShiftsController < ApplicationController
   def edit
     @shift = Shift.find(params[:id])
     @report = @shift.report
-    calculate_default_times
 # TODO - unecessary? they never seem to be called ~Casey
 #    return unless user_is_owner_or_admin_of(@shift, @shift.department)
 #    (render :partial => 'shifts/tooltips/edit', :layout => 'none') if params[:tooltip]
@@ -243,33 +239,6 @@ class ShiftsController < ApplicationController
   #   end
   # end
 
-#calculates default_start/end and range_start/end_time
-    def calculate_default_times
-#Case 1 - start with info from the shift being edited
-      if params[:id]
-        @default_start = @shift.start
-        @default_end = @shift.end
-#Case 2 - start with info from timeline
-      elsif params[:xPercentage]
-        @default_start = (params[:date] ? Time.parse(params[:date]) : Time.now).to_date
-        @dept_start_minutes ||= current_department.department_config.schedule_start
-        @dept_end_minutes ||= current_department.department_config.schedule_end
-        @minutes_per_day ||= (@dept_end_minutes - @dept_start_minutes)
-        @default_start += @dept_start_minutes.minutes
-        @default_start += (@minutes_per_day * params[:xPercentage].to_f / 60).to_int * 3600 #truncates the hour
-#if the time slot starts off of the hour (at 9:30), this is not ideal because it will select either 9:00 or 10:00 and the following hour. We need timeslot validation first.
-#if the schedule starts at 9:30, I'm not sure what happens ~Casey
-        @default_end = @default_start + 1.hour
-#Case 3 - nothing to start with (shifts/new)
-      else
-        @default_start = (params[:date] ? Time.parse(params[:date]) : Time.now).to_date.to_time + current_department.department_config.schedule_start.minutes
-        @default_end = @default_start + 1.hour
-      end
-
-      @range_start_time = Time.now.to_date + current_department.department_config.schedule_start.minutes
-      @range_end_time = Time.now.to_date + current_department.department_config.schedule_end.minutes
-
-    end
 
 
 end
