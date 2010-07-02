1069ba1cfdd99d4b8fef54b0fd1a8a9f72ad3192
Casey Watts <casey.s.watts@gmail.com>
2010-07-02 10:53:40 -0400

    simplified calculate_default_times in shifts and time_slots helpers, removed it from sub_requests. It's only necessary when we use xPercentage, as in the timelines

--- app/helpers/sub_requests_helper.rb
+++ app/helpers/sub_requests_helper.rb
@@ -1,19 +1,3 @@
 module SubRequestsHelper
-
-#calculates default_start/end and range_start/end_time
-  def calculate_default_times
-
-      @default_start = @sub_request.shift.start
-      @default_end = @sub_request.shift.end
-      @default_mandatory_start = @default_start
-      @default_mandatory_end = @default_end
-
-
-      @range_start_time = @sub_request.shift.start
-      @range_end_time = @sub_request.shift.end
-
-  end
-
-
 end
 
