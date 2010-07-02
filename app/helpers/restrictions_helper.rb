307d8e6bf444350b5c38de21f7f110278af1040b
Casey Watts <casey.s.watts@gmail.com>
2010-07-02 15:50:35 -0400

    waypoint

--- app/helpers/restrictions_helper.rb
+++ app/helpers/restrictions_helper.rb
@@ -1,2 +1,13 @@
 module RestrictionsHelper
+#calculates default_start/end and range_start/end_time
+  def calculate_default_times
+
+      @restriction.starts ||= (params[:date] ? Time.parse(params[:date]) : Time.now).to_date.to_time + current_department.department_config.schedule_start.minutes
+      @restriction.expires ||= @shift.starts + 1.hour
+
+    @range_start_time = Time.now.to_date + current_department.department_config.schedule_start.minutes
+    @range_end_time = Time.now.to_date + current_department.department_config.schedule_end.minutes
+  end
+
 end
+
