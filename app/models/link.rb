fdb442e5601b9f291998d8b5d7a724dd0d9fcd27
Derek Zhao <derek.zhao@yale.edu>
2010-07-06 11:56:42 -0400

    removing some debug code

--- app/models/link.rb
+++ app/models/link.rb
@@ -6,7 +6,7 @@ class Link < Notice
 		if self.url.empty? || self.url == "http://"
 			errors.add_to_base "Your URL cannot be empty"
 		elsif self.url.split.first != self.url
-			errors.add_to_base "Your URL cannot have a white space " + self.url
+			errors.add_to_base "Your URL cannot have white spaces"
 		end
 	end
 end
