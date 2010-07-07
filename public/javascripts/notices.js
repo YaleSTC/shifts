be2fba5a1596b26e727fdbd7f5c6570e798dfa7d
Bay Gross <bay.gross@yale.edu>
2010-07-07 16:39:33 -0400

    lots of javascript and misc changes to notices

--- public/javascripts/notices.js
+++ public/javascripts/notices.js
@@ -1,5 +1,34 @@
+function DepartmentCheckBoxes(){
+	var dept_status = $("#department_wide_locations").attr('checked');
+	$("div#all_locations :checkbox").each(function(){
+	    $(this).attr('checked', dept_status);
+	   if(dept_status) {
+	        $(this).attr('disabled', 'disabled');
+	    } else {
+	        $(this).removeAttr('disabled');
+	    }
+	});
+}
+
+function LocGroupCheckBoxes(lcg){
+	lcg.each(function(){
+  var locgroup_status = $(this).attr('checked');
+  $(this).siblings('input[type=checkbox]').each(function(){
+      $(this).attr('checked', locgroup_status);
+      if(locgroup_status) {
+          $(this).attr('disabled', 'disabled');
+      } else {
+          $(this).removeAttr('disabled');
+      }
+  });
+});
+}
+
 jQuery(document).ready(function(){
 //By default, only have advanced_options open on the main Notices and Dashboard page (not in a shift report page)
+    DepartmentCheckBoxes();
+		LocGroupCheckBoxes($("input[name^='for_location_group']"));
+		
     if(($("#page_title").text() != "Notices") && ($("#page_title").text() != "My Dashboard")) {
         $("#advanced_options_div").hide();
         $("#toggle_link").html('Show advanced options');
@@ -12,29 +41,20 @@ jQuery(document).ready(function(){
 		$("input[name^=for_location_group],input[name^=department_wide_locations]").each(function(){
   	  $(this).css("display", "inline");		
 		});
-	
-    $("#department_wide_locations").click(function(){
-        var dept_status = $(this).attr('checked');
-        $("div#all_locations :checkbox").each(function(){
-            $(this).attr('checked', dept_status);
-           if(dept_status) {
-                $(this).attr('disabled', 'disabled');
-            } else {
-                $(this).removeAttr('disabled');
-            }
-        });
-    });
+		
+    $("#department_wide_locations").click(DepartmentCheckBoxes);
+    //Tried and failed to extract this to a seperate function. Could not get objects to pass correctly. TODO: for someone with more javascript skillz -bay
     $("input[name^='for_location_group']").click(function(){
-        var locgroup_status = $(this).attr('checked');
-        $(this).siblings('input[type=checkbox]').each(function(){
-            $(this).attr('checked', locgroup_status);
-            if(locgroup_status) {
-                $(this).attr('disabled', 'disabled');
-            } else {
-                $(this).removeAttr('disabled');
-            }
-        });
-    });
+			  var locgroup_status = $(this).attr('checked');
+			  $(this).siblings('input[type=checkbox]').each(function(){
+			      $(this).attr('checked', locgroup_status);
+			      if(locgroup_status) {
+			          $(this).attr('disabled', 'disabled');
+			      } else {
+			          $(this).removeAttr('disabled');
+			      }
+			  });
+		});
 
     $("#type_sticky").click(function(){
         $("#start_time_choice_date").attr('checked', false);
@@ -52,11 +72,12 @@ jQuery(document).ready(function(){
 	       $("#advanced_options_div").toggle(function(){
        		 if($(this).css("display")=="none") {
 		            $("#toggle_link").html('Show advanced options');
-								$("#TB_ajaxContent").animate({ height:'-=290' }, 300 );
+								$("#TB_ajaxContent").animate({ height:'-=240' }, 300 );
 		        } else {
 		            $("#toggle_link").html('Hide advanced options');
-								$("#TB_ajaxContent").animate({ height:'+=290' }, 300 );
+								$("#TB_ajaxContent").animate({ height:'+=240' }, 300 );
 		        }
 				});
     });
+
 });
