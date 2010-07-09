758cbeedc5559eef9d8811155d97b6b2a7afbb4c
Bay Gross <bay.gross@yale.edu>
2010-07-09 16:22:58 -0400

    fixing views and such

--- app/models/sticky.rb
+++ app/models/sticky.rb
@@ -1,5 +1,5 @@
 class Sticky < Notice
-  named_scope :active, lambda {{ :conditions => ["end = ? OR end > ?", nil, Time.now.utc] }}
+  named_scope :active, lambda {{ :conditions => ["end is ? OR end > ?", nil, Time.now.utc] }}
   
   def active?
     self.end == nil || self.end > Time.now
