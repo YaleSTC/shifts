62beaa9862832909eb1d3064048ff9889a7e4141
Derek Zhao <derek.zhao@yale.edu>
2010-07-23 16:46:50 -0400

    added validations to requested shifts and more backend work between requested shifts and templates

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,55 +1,58 @@
 class TemplatesController < ApplicationController
+  before_filter :require_department_admin
   # GET /templates
   # GET /templates.xml
   def index
-    @templates = Template.all
+    @week_templates = Template.all
 
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @templates }
+      format.xml  { render :xml => @week_templates }
     end
   end
 
   # GET /templates/1
   # GET /templates/1.xml
   def show
-    @template = Template.find(params[:id])
-
+    @week_template = Template.find(params[:id])
+		@requested_shifts = @week_template.requested_shifts
     respond_to do |format|
       format.html # show.html.erb
-      format.xml  { render :xml => @template }
+      format.xml  { render :xml => @week_template }
     end
   end
 
   # GET /templates/new
   # GET /templates/new.xml
   def new
-    @template = Template.new
-
+    @week_template = Template.new
+		@locations = current_department.locations
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @template }
+      format.xml  { render :xml => @week_template }
     end
   end
 
   # GET /templates/1/edit
   def edit
-    @template = Template.find(params[:id])
+    @week_template = Template.find(params[:id])
   end
 
   # POST /templates
   # POST /templates.xml
   def create
-    @template = Template.new(params[:template])
-
-    respond_to do |format|
-      if @template.save
+    @week_template = Template.new(params[:template])
+		if params[:for_locations]
+			@week_template.locations << Location.find(params[:for_locations])
+    end
+		respond_to do |format|
+      if @week_template.save
         flash[:notice] = 'Template was successfully created.'
-        format.html { redirect_to(@template) }
-        format.xml  { render :xml => @template, :status => :created, :location => @template }
+        format.html { redirect_to(@week_template) }
+        format.xml  { render :xml => @week_template, :status => :created, :location => @week_template }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @template.errors, :status => :unprocessable_entity }
+        format.xml  { render :xml => @week_template.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -57,16 +60,16 @@ class TemplatesController < ApplicationController
   # PUT /templates/1
   # PUT /templates/1.xml
   def update
-    @template = Template.find(params[:id])
+    @week_template = Template.find(params[:id])
 
     respond_to do |format|
-      if @template.update_attributes(params[:template])
+      if @week_template.update_attributes(params[:template])
         flash[:notice] = 'Template was successfully updated.'
-        format.html { redirect_to(@template) }
+        format.html { redirect_to(@week_template) }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @template.errors, :status => :unprocessable_entity }
+        format.xml  { render :xml => @week_template.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -74,8 +77,8 @@ class TemplatesController < ApplicationController
   # DELETE /templates/1
   # DELETE /templates/1.xml
   def destroy
-    @template = Template.find(params[:id])
-    @template.destroy
+    @week_template = Template.find(params[:id])
+    @week_template.destroy
 
     respond_to do |format|
       format.html { redirect_to(templates_url) }
