e8b3cbb2e890f759614436b222506694ffb0cdad
Casey Watts <casey.s.watts@gmail.com>
2010-07-27 16:02:49 -0400

    more experimenting

--- app/helpers/repeating_events_helper.rb
+++ app/helpers/repeating_events_helper.rb
@@ -1,5 +1,30 @@
 module RepeatingEventsHelper
 
+  def department_check(dept, repeating_event)
+   repeating_event.departments.each do |d|
+      return true if d == dept
+    end
+    false
+  end
+
+  def loc_group_check(loc_group, repeating_event)
+    repeating_event.loc_groups.each do |lg|
+      return true if lg == loc_group
+    end
+    false
+  end
+
+  def location_check(location, repeating_event)
+    return true if @repeating_event.location_ids && @repeating_event.location_ids.include?(location.id)
+#    current_department.locations.each do |loc|
+#      return true if loc == location
+#    end
+		false
+
+
+
+  end
+
 
   #duplicated from time_slots helper, to fix things for the time being. TODO: cleanup.
   def fetch_timeslots(time_slot_day,location)
@@ -24,7 +49,9 @@ module RepeatingEventsHelper
     @repeating_event.end_time ||= @default_start_date.to_time + current_department.department_config.schedule_end.minutes
     @range_start_time = Time.now.to_date + current_department.department_config.schedule_start.minutes
     @range_end_time = Time.now.to_date + current_department.department_config.schedule_end.minutes
+    @repeating_event.location_ids << params[:location_id]
   end
 
 
 end
+
--- public/javascripts/notices.js
+++ public/javascripts/notices.js
@@ -1,5 +1,5 @@
-jQuery($("#TB_ajaxContent")).ready(function(){ 
-		
+jQuery($("#TB_ajaxContent")).ready(function(){
+
     if(($("#page_title").text() != "Notices") && ($("#page_title").text() != "My Dashboard")) {
         $("#advanced_options_div").hide();
         $("#toggle_link").html('Show advanced options');
@@ -9,21 +9,23 @@ jQuery($("#TB_ajaxContent")).ready(function(){
         $("#toggle_link").show();
     }
 
-		$("input[name^=for_location_group],input[name^=department_wide_locations]").each(function(){
-  	  $(this).css("display", "inline");		
-		});
 
-    $("#department_wide_locations").click(function(){
-				var dept_status = $("#department_wide_locations").attr('checked');
-				$("div#all_locations :checkbox").each(function(){
-				   $(this).attr('checked', dept_status);
-				    if(dept_status) {
-				          $(this).attr('disabled', 'disabled');
-				      } else {
-				          $(this).removeAttr('disabled');
-				     }	 	
-				 });
-			});
+    $("#toggle_link").click(function(){
+				 		if($(this).css("display")!="none") {
+	            $("#toggle_link").html('Show advanced options');
+							$("#TB_ajaxContent").animate({ height:'-=' + $("#variable_height_box").outerHeight() }, 300 );
+	        	}
+
+	       $("#advanced_options_div").toggle(function(){
+       				if ($(this).css("display")!="none") {
+				            $("#toggle_link").html('Hide advanced options');
+										$("#TB_ajaxContent").animate({ height:'+='+ $("#variable_height_box").outerHeight() }, 300 );
+				        }
+				});
+    });
+
+
+// TODO some duplication remains between notices.js and locationcheckboxes.js
     $("input[name^='for_location_group']").click(function(){
 			  var locgroup_status = $(this).attr('checked');
 			  $(this).siblings('input[type=checkbox]').each(function(){
@@ -36,19 +38,9 @@ jQuery($("#TB_ajaxContent")).ready(function(){
 			  });
 		});
 
-    $("#toggle_link").click(function(){     
-				 		if($(this).css("display")!="none") {
-	            $("#toggle_link").html('Show advanced options');
-							$("#TB_ajaxContent").animate({ height:'-=' + $("#variable_height_box").outerHeight() }, 300 );
-	        	}
-	       $("#advanced_options_div").toggle(function(){
-       				if ($(this).css("display")!="none") {
-				            $("#toggle_link").html('Hide advanced options');
-										$("#TB_ajaxContent").animate({ height:'+='+ $("#variable_height_box").outerHeight() }, 300 );
-				        }
-				});
-    });
+
 });
+
 jQuery($("#variable_height_box")).ajaxComplete(function(){
 	var height_change = $("#variable_height_box").outerHeight();
 	if (height_change) {
@@ -56,3 +48,4 @@ jQuery($("#variable_height_box")).ajaxComplete(function(){
   $("#TB_ajaxContent").animate({ height:'+='+height_change }, 300 );
 }
 });
+
