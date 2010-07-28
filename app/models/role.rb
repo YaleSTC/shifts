6d4ed69ce76164c36326b6bab1a50b52b3498b7c
James Luo <James@dhcp128036204067.central.yale.edu>
2010-07-28 18:02:04 -0400

    created role template join table and some form work around templates

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,6 +1,7 @@
 class Role < ActiveRecord::Base
 #  has_and_belongs_to_many :departments
   belongs_to :department
+  has_and_belongs_to_many :template
   has_and_belongs_to_many :permissions
   has_and_belongs_to_many :users
 
