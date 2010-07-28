d1e998670e0eb374c48335bdc1bd657fbc477376
Casey Watts <casey.s.watts@gmail.com>
2010-07-28 12:49:23 -0400

    cleanup

--- public/javascripts/notices.js
+++ public/javascripts/notices.js
@@ -9,23 +9,21 @@ jQuery($("#TB_ajaxContent")).ready(function(){
         $("#toggle_link").show();
     }
 
+		$("input[name^=for_location_group],input[name^=department_wide_locations]").each(function(){
+  	  $(this).css("display", "inline");
+		});
 
-    $("#toggle_link").click(function(){
-				 		if($(this).css("display")!="none") {
-	            $("#toggle_link").html('Show advanced options');
-							$("#TB_ajaxContent").animate({ height:'-=' + $("#variable_height_box").outerHeight() }, 300 );
-	        	}
-
-	       $("#advanced_options_div").toggle(function(){
-       				if ($(this).css("display")!="none") {
-				            $("#toggle_link").html('Hide advanced options');
-										$("#TB_ajaxContent").animate({ height:'+='+ $("#variable_height_box").outerHeight() }, 300 );
-				        }
-				});
-    });
-
-
-// TODO some duplication remains between notices.js and locationcheckboxes.js
+    $("#department_wide_locations").click(function(){
+				var dept_status = $("#department_wide_locations").attr('checked');
+				$("div#all_locations :checkbox").each(function(){
+				   $(this).attr('checked', dept_status);
+				    if(dept_status) {
+				          $(this).attr('disabled', 'disabled');
+				      } else {
+				          $(this).removeAttr('disabled');
+				     }
+				 });
+			});
     $("input[name^='for_location_group']").click(function(){
 			  var locgroup_status = $(this).attr('checked');
 			  $(this).siblings('input[type=checkbox]').each(function(){
@@ -38,9 +36,19 @@ jQuery($("#TB_ajaxContent")).ready(function(){
 			  });
 		});
 
-
+    $("#toggle_link").click(function(){
+				 		if($(this).css("display")!="none") {
+	            $("#toggle_link").html('Show advanced options');
+							$("#TB_ajaxContent").animate({ height:'-=' + $("#variable_height_box").outerHeight() }, 300 );
+	        	}
+	       $("#advanced_options_div").toggle(function(){
+       				if ($(this).css("display")!="none") {
+				            $("#toggle_link").html('Hide advanced options');
+										$("#TB_ajaxContent").animate({ height:'+='+ $("#variable_height_box").outerHeight() }, 300 );
+				        }
+				});
+    });
 });
-
 jQuery($("#variable_height_box")).ajaxComplete(function(){
 	var height_change = $("#variable_height_box").outerHeight();
 	if (height_change) {
