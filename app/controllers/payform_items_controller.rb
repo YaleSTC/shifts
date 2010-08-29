e8c210621a2664786dec32f02c1485220dc8d4d9
Adam Bray <adam.bray@yale.edu>
2010-08-28 20:29:25 -0400

    bug fix for not being able to create group jobs

--- app/controllers/payform_items_controller.rb
+++ app/controllers/payform_items_controller.rb
@@ -11,7 +11,7 @@ class PayformItemsController < ApplicationController
   end
 
   def create
-    get_hours
+    set_payform_item_hours("payform_item")
     @payform_item = PayformItem.new(params[:payform_item])
     @payform_item.payform = Payform.find(params[:payform_id])
     return unless user_is_owner_or_admin_of(@payform_item.payform, @payform_item.department)
@@ -32,7 +32,7 @@ class PayformItemsController < ApplicationController
   end
 
   def update
-    get_hours
+    set_payform_item_hours("payform_item")
     @payform_item = PayformItem.find(params[:id])
     return unless user_is_owner_or_admin_of(@payform_item.payform, @payform_item.department)
     @payform_item.attributes = params[:payform_item]
@@ -69,38 +69,6 @@ class PayformItemsController < ApplicationController
       render :action => 'delete'
     end
   end
-
-  protected
-
-  def get_hours
-    if params[:calculate_hours] == 'user_input'
-      params[:payform_item][:hours] = params[:other][:hours].to_f + params[:other][:minutes].to_f/60
-    else
-      start_params = []
-      end_params = []
-      for num in (1..7)
-        unless num == 6 #we skip seconds; meridian is stored in 7
-          start_params << params[:time_input]["start(#{num}i)"].to_i
-          end_params << params[:time_input]["end(#{num}i)"].to_i
-        end
-      end
-      start_time = convert_to_time(start_params)
-      end_time = convert_to_time(end_params)
-      params[:payform_item][:hours] = (end_time-start_time) / 3600.0
-    end
-  end
-
-
-  def convert_to_time(date_array)
-    # 0 = year, 1 = month, 2 = day, 3 = hour, 4 = minute, 5 = meridiem(am/pm)
-    if date_array[3] == 12 #if noon or midnight
-      date_array[3] -= 12
-    end
-    if date_array[5] == -2 #if pm
-      date_array[3] += 12
-    end
-    Time.utc(date_array[0], nil, nil, date_array[3], date_array[4])
-  end
-
+  
 end
 
