59cb5f79d9760e9157e1010d93820be76a1c0828
M. Quintana <machiste.quintana@yale.edu>
2011-07-05 14:15:03 -0400

    Removed data objects' dependency on multiple select plugin - works much better now

--- app/controllers/user_configs_controller.rb
+++ app/controllers/user_configs_controller.rb
@@ -4,21 +4,27 @@ class UserConfigsController < ApplicationController
   def edit
     @dept_select = current_user.departments.map{|d| [d.name, d.id]}
     @loc_group_select = {}
-    @data_objects = []
-    @data_type = DataType
     current_user.departments.each do |dept|
       @loc_group_select.store(dept.id, current_user.loc_groups(dept))
-      @data_objects << dept.data_objects
-      @data_objects.flatten!
+      @data_objects = dept.data_objects
+      @data_types = dept.data_types
     end
     @selected_loc_groups = @user_config.view_loc_groups.collect{|lg| lg.id }
     @selected_data_objects = @user_config.watched_data_objects.split(', ').map{|obj| obj.to_i }
-    
-
+    @data_objects_grouped_by_type = []
+      @data_types.each do |data_type|
+        @data_objects_list = []
+        data_type.data_objects.each do |data_object|
+          @data_objects_list << [data_object.name, data_object.id, @selected_data_objects.include?(data_object.id)]
+        end
+        @data_objects_grouped_by_type << [data_type.name, @data_objects_list]
+      end
    
   end
 
   def update
+    # adding watched data objects to the params hash
+    params[:user_config][:watched_data_objects] = params[:dt].keys.join(",")
     if @user_config.update_attributes(params[:user_config])
       flash[:notice] = "Successfully updated user settings."
       # if we came here from somewhere else, redirect us back
