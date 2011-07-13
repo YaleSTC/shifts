7a7e1fd26dcc46442b0ff0b8ca943d13c6d6f720
M. Quintana <machiste.quintana@yale.edu>
2011-07-13 17:42:23 -0400

    More work on the user config js, getting the multiple select framework ready

--- public/javascripts/user_config.js
+++ public/javascripts/user_config.js
@@ -1,12 +1,53 @@
 jQuery(document).ready(function() {
-  var dept = "STC";
-  $('[name='+dept+']').click(function() {
-    $('[id^="loc"]').attr("checked", true);
-  });
-});
+  var dept = "STC",
+      dept2 = "stc",
+      child_checkboxes = $('[id^="loc"]' && '[class='+dept2+']'),
+      child_checkboxes_checked = $('[id^="loc"]' && '[checked="true"]' && '[class='+dept2+']'),
+      child_checkboxes_length = child_checkboxes.length,
+      child_checkboxes_checked_length = child_checkboxes_checked.length;
+
+
+
+
+// Slide toggle stuff, ignore for now
 //  $(document).ready(function() {
   //  $('[name="STC"]').click(function(){
     //  $('[id^="loc"]').slideToggle();
    // });
  // });
+ 
+  if(child_checkboxes_checked_length === child_checkboxes_length) {
+    $(".header_stc").attr('checked', true);
+  } else {
+    $(".header_stc").attr('checked', false);
+  }
+
+  $('[class^="header"]').click(function(){
+				  var header_status = $(".header_stc").attr('checked');
+				  $('[id^="loc"]' && '[class='+dept2+']').each(function(){
+				     $(this).attr('checked', header_status);
+				      if(header_status) {
+				            $(this).attr('checked', true);
+				        } else {
+				            $(this).attr('checked', false);
+				       }	 	
+				   });
+  });
+  var child_checkboxes_checked = $('[id^="loc"]' && '[checked="true"]' && '[class='+dept2+']');
+  child_checkboxes.click(function(){
+    if(child_checkboxes_checked.length === child_checkboxes.length) {
+      $(".header_stc").attr('checked', true);
+    } else {
+      $(".header_stc").attr('checked', false);
+    }
+  });
+
+//  child_checkboxes.click(function(){
+//    if (child_checkboxes.attr('checked'))
+//  })
+		  console.log(child_checkboxes_length, child_checkboxes_checked_length, child_checkboxes_checked)
+		  console.log(child_checkboxes)
+});
+
+
 
