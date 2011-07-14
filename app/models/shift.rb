ffa78ee616d82e7835ec10018350b83dde879445
Maria Altyeva <maria.altyeva@yale.edu>
2011-07-14 14:57:58 -0400

    replaced .localtime with .in_time_zone, which seems to have fixed the problem?

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -262,9 +262,9 @@ class Shift < ActiveRecord::Base
   
   #to enable the view of unscheduled shifts, a shift that lacks an end attribute is viewed as ending right now
   # else, the end attribute is read (referenced in shifts_helper)
-  # def end
-  #    read_attribute(:end).nil? ? Time.now : read_attribute(:end).to_datetime()
-  # end
+  def end
+      read_attribute(:end).nil? ? Time.now : read_attribute(:end).in_time_zone
+   end
 
   #a shift has been signed in to if it has a report
   # NOTE: this evaluates whether a shift is CURRENTLY signed in
