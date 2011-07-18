e24df54b7112e2cc584b1ac3297ec8e5ab839648
Maria Altyeva <maria.altyeva@yale.edu>
2011-07-18 13:47:44 -0400

    Rudimentary workings of paperclip completed

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -3,7 +3,14 @@ class UserProfile < ActiveRecord::Base
   belongs_to :user
   has_many :user_profile_entries
    
-  has_attached_file :photo 
+  has_attached_file :photo, 
+      :default_url => "/assets/user_profiles/adam.jpg",
+      :url => "/:assets/:user_profiles/:id/:style/:basename.extension", 
+      :path => ":rails_root/public/:assets/:user_profiles/:id/:style/:basename.extension"
+  
+  validates_attachment_size :photo, :less_than => 3.megabytes
+  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png', 'image/jpg']
+                
    
 end
 
