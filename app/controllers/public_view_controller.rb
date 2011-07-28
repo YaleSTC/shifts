1a4066cf88c2848822150357c08ac759bd85729e
Maria Altyeva <maria.altyeva@yale.edu>
2011-07-28 15:24:33 -0400

    First commit to set up page}

--- app/controllers/public_view_controller.rb
+++ app/controllers/public_view_controller.rb
@@ -4,8 +4,10 @@ class PublicViewController < ApplicationController
   skip_before_filter CASClient::Frameworks::Rails::Filter
 
   def index
-    @date = params[:date].to_date
-		@cluster = Location.find_by_short_name(params[:cluster])
+    #@date = params[:date].to_date
+		@loc_group = LocGroup.first #change later
+		@locations = @loc_group.locations.active
+		
   end
 
 end
