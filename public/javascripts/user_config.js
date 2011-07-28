fb5af33be77df29e85d00dfee5e462f5145b0948
Machiste Quintana <machiste.quintana@yale.edu>
2011-07-28 13:50:22 -0400

    checkboxes are now fully functional, next step is abstracting the code to a function

--- public/javascripts/user_config.js
+++ public/javascripts/user_config.js
@@ -1,10 +1,12 @@
 jQuery(document).ready(function() {
-  var dept = "STC",
-      dept2 = "stc",
-      child_checkboxes = $('[class='+dept2+'][id^="loc"]'),
-      child_checkboxes_checked = $('[class='+dept2+'][id^="loc"][checked="checked"]'),
-      child_checkboxes_length = child_checkboxes.length,
-      child_checkboxes_checked_length = child_checkboxes_checked.length;
+//multiple select checkboxes code  
+
+var	dept = $('[class^="header"]')
+    dept2 = "stc",
+    child_checkboxes = $('[class='+dept2+'][id^="loc"]'),
+    child_checkboxes_checked = $('[class='+dept2+'][id^="loc"][checked="checked"]'),
+    child_checkboxes_length = child_checkboxes.length,
+    child_checkboxes_checked_length = child_checkboxes_checked.length;
 
   if(child_checkboxes_checked_length === child_checkboxes_length) {
     $(".header_stc").attr('checked', true);
@@ -16,39 +18,28 @@ jQuery(document).ready(function() {
     var header_status = $(".header_stc").attr('checked');
     child_checkboxes.each(function(){
        $(this).attr('checked', header_status);
-        if(header_status) {
-          $(this).attr('checked', true);
-        } else {
-          $(this).attr('checked', false);
-        }	 	
      });
   });
 
   child_checkboxes.click(function(){
     var child_status = child_checkboxes.attr('checked');
-    var child_checkboxes_checked = $('[class='+dept2+'][id^="loc"][checked="checked"]');
+    var child_checkboxes_checked
+ 	child_checkboxes_checked = 0
+	console.log(child_checkboxes_checked)
     child_checkboxes.each(function(){
       if($(this).attr('checked')){
-        child_checkboxes_checked = child_checkboxes_checked.length --;
-      } else if(!$(this).attr('checked')){
-        child_checkboxes_checked = child_checkboxes_checked.length ++;
+        child_checkboxes_checked++;
       }
+	console.log(child_checkboxes_checked, child_checkboxes.length)
     if(child_checkboxes_checked === child_checkboxes.length) {
       $(".header_stc").attr('checked', true);
     } else {
       $(".header_stc").attr('checked', false);
     }
-
     });
-//    child_checkboxes.each(function(){
-//      if($(this).attr('checked')) {
-//        $(this).attr('checked', false);
-//      } else if (!$(this).attr('checked')){
-//        $(this).attr('checked', true);
-//      }    
-//    });
   });
+		
+//Begin header checkboxes toggle slide
 
-		  console.log(child_checkboxes.length, child_checkboxes_checked.length, child_checkboxes_checked)
-		  console.log(child_checkboxes)
+
 });
