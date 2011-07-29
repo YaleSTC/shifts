11872970b3e3975c557cd248a224267a06edbb24
James Luo <james.luo@yale.edu>
2011-07-29 15:22:22 -0400

    fixing the code with Maria's additions from the backend

--- app/controllers/public_view_controller.rb
+++ app/controllers/public_view_controller.rb
@@ -3,26 +3,12 @@ class PublicViewController < ApplicationController
   skip_before_filter :login_check
   skip_before_filter CASClient::Frameworks::Rails::Filter
   helper :shifts
+  helper :loc_groups
   
   def index
     #@date = params[:date].to_date
-    @loc_group = LocGroup.find(:all, :conditions => ["#{:public} = ?", true])
-		@locations = @loc_group.locations.active
+    @loc_groups = LocGroup.find(:all, :conditions => ["#{:public} = ?", true])
     @view_days = (Date.today..Date.today+7)
-		
-		
-		#TODO:simplify this stuff:
-    @dept_start_hour = @loc_group.department.department_config.schedule_start / 60
-    @dept_end_hour = @loc_group.department.department_config.schedule_end / 60
-    @hours_per_day = (@dept_end_hour - @dept_start_hour)
-    @time_increment = @loc_group.department.department_config.time_increment
-    @blocks_per_hour = 60/@time_increment.to_f
-    @blocks_per_day = @blocks_per_hour * @hours_per_day
-    @day_blockset = []
-    @dept_start_time = Time.now.change(:hour => @dept_start_hour)
-    for block in 1..@blocks_per_day
-      @day_blockset << @dept_start_time + (block - 1)*@time_increment.minutes
-    end
   end
 
 end
