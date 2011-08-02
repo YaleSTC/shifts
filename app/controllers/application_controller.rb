1924deff4fb172d922e19d3d11048e489d9f1e0a
James Luo <james.luo@yale.edu>
2011-08-02 17:41:10 -0400

    added a legacy prototype helper; affected methods will need to be rewritten later, but it's currently helpful for testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,7 @@ class ApplicationController < ActionController::Base
 
 
   helper :layout # include all helpers, all the time (whyy? -Nathan)
+  helper :prototype #TODO including this helper is a stopgap for the shift to Rails 3; contained methods should be rewritten
   helper_method :current_user
   helper_method :current_department
   
