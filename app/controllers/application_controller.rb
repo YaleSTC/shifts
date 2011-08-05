a03778a29c02c0d50514760b337cda101a9e4056
James Luo <james.luo@yale.edu>
2011-08-05 14:37:20 -0400

    automatic detection of mobile browsers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -373,6 +373,14 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param] == "1"
+    else
+      request.user_agent =~ /Mobile|webOS/
+    end
+  end
+  helper_method :mobile_device?
 
   def department_chooser
     if (params[:su_mode] && current_user.superuser?)
