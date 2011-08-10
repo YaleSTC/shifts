ed5d3b158f150d87359df80daeb03aef5a2e9356
Adam Bray <adam.bray@yale.edu>
2011-08-10 11:33:38 -0400

    typo in deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -83,7 +83,7 @@ EOF
       run "ln -nsfF #{shared_path}/sessions/ #{current_path}/tmp/sessions"
       run "ln -nsfF #{shared_path}/system/ #{current_path}/public/system"
       run "ln -nsfF #{shared_path}/assets/user_profiles #{current_path}/public/assets/"
-      run "mv #{current_path}/assets/default.jpg #{shared_path}/assets/user_profiles"
+      run "mv #{current_path}/public/assets/default.jpg #{shared_path}/assets/user_profiles"
     end    
   end  
 end
