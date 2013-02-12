d2234f1b2a81bd6800477ef6ba76763c6d7bedf0
caseywatts <casey.s.watts@gmail.com>
2013-02-12 03:30:49 -0500

    for clarity

--- app/models/payform.rb
+++ app/models/payform.rb
@@ -29,9 +29,9 @@ class Payform < ActiveRecord::Base
   def self.export_payform(options = {})
     FasterCSV.generate(options) do |csv|
       csv << ["End Date", "First Name", "Last Name", "User ID", "Employee ID", "Payrate", "Hours"]
-      all.sort_by{|payform| User.find(payform.user_id).last_name}.sort_by{|payform| payform.date}.each do |payform|
+      sorted_payforms = all.delete_if{|payform| payform.hours == 0}.sort_by{|payform| User.find(payform.user_id).last_name}.sort_by{|payform| payform.date}
+      sorted_payforms.each do |payform|
         user = User.find(payform.user_id)
-        puts payform.date.to_yaml
         csv << [payform.date, user.first_name, user.last_name, user.login, user.employee_id, payform.payrate, payform.hours]
       end
     end
