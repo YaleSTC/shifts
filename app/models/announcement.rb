1d3bdfed1ccc51a881dee8f0baca4d3f33422b4c
Hugh <hkocinneide@gmail.com>
2013-06-13 18:40:17 -0400

    Made migrations to remove dependency on LSLS

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,8 +1,8 @@
 class Announcement < Notice
-  has_many :location_associations, :as => :postable
-  has_many :locations, :through => :location_associations
   has_many :loc_group_associations, :as => :postable
-  has_many :loc_groups, :through => :loc_group_associations
+  has_many :location_associations,  :as => :postable
+  has_many :loc_groups,             :through => :loc_group_associations
+  has_many :locations,              :through => :location_associations
 
   named_scope :active, lambda {{ :conditions => ["start <= ? AND end is ? OR end > ?", Time.now.utc, nil, Time.now.utc] }}
   named_scope :upcoming, lambda {{ :conditions => ["start > ?", Time.now.utc]}}
