4de07b5d0548adef9053383a93046b5d61608498
Hugh <hkocinneide@gmail.com>
2013-06-17 15:16:26 -0400

    Removed bug: habtm returned join tables' ids

--- app/models/loc_groups_notice.rb
+++ app/models/loc_groups_notice.rb
@@ -1,4 +1,5 @@
 class LocGroupsNotice < ActiveRecord::Base
+  self.primary_key = "loc_groups_notices_id"
   belongs_to :loc_group
   belongs_to :notice
 end
--- app/models/locations_notice.rb
+++ app/models/locations_notice.rb
@@ -1,4 +1,5 @@
 class LocationsNotice < ActiveRecord::Base
+  self.primary_key = "locations_notices_id"
   belongs_to :location
   belongs_to :notice
 end
