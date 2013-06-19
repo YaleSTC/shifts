e95542afe094e8c2c94af058c9d6aa6cdbb41458
Hugh <hkocinneide@gmail.com>
2013-06-19 17:33:14 -0400

    Global migration now technically works

--- db/migrate/20130618200548_add_department_wide_to_notices.rb
+++
@@ -1,9 +0,0 @@
-class AddDepartmentWideToNotices < ActiveRecord::Migration
-  def self.up
-    add_column :notices, :department_wide, :boolean
-  end
-
-  def self.down
-    remove_column :notices, :department_wide
-  end
-end
