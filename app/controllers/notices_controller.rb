c13914363bbb0d2fb03ba85c4af3e3012a554f3c
Hugh <hkocinneide@gmail.com>
2013-06-19 13:28:16 -0400

    WIP'

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -36,9 +36,10 @@ class NoticesController < ApplicationController
     # TODO add department wide column on notices -Hugh
     # Also do we want to include add all locations to notice when we add something
     # to the loc group?
-    # if params[:department_wide_locations] && current_user.is_admin_of?(current_department)
-    #   notice.user_sources << current_department
-    # end
+    if params[:department_wide_locations] && current_user.is_admin_of?(current_department)
+      notice.department_wide = true
+      notice.save!
+    end
 		if params[:for_location_groups] 
       params[:for_location_groups].each do |loc_group|
 				@loc_group = LocGroup.find_by_id(loc_group)
@@ -46,7 +47,6 @@ class NoticesController < ApplicationController
           notice.loc_groups << @loc_group
       	end
 			end
-
     end
     if params[:for_locations]
       params[:for_locations].each do |loc|
--- db/migrate/20130618200548_add_global_to_notices.rb
+++
@@ -1,9 +0,0 @@
-class AddGlobalToNotices < ActiveRecord::Migration
-  def self.up
-    add_column :notices, :global, :boolean
-  end
-
-  def self.down
-    remove_column :notices, :global
-  end
-end
