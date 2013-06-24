351236f82be8be38fe79af287885c0d84aebfdf1
Hugh <hkocinneide@gmail.com>
2013-06-24 14:29:30 -0400

    Removed prototype notation from OUR javascript

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -126,7 +126,7 @@ function popup_new_timeslot(parent_element, e, raw_element){
   }
 
   function loading_tooltip(x,y){
-    $('body').append("<div id='tooltip' style='position: absolute; left:"+x+"px; top:"+y+"px'>Loading...</div>");
+    $('#body').append("<div id='tooltip' style='position: absolute; left:"+x+"px; top:"+y+"px'>Loading...</div>");
   }
 
   function getXOffset(element){
