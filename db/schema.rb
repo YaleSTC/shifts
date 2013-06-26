9ec358b8e734aaf188a3928d9befb0984744034b
Hugh <hkocinneide@gmail.com>
2013-06-26 17:43:54 -0400

    fixed migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130620135037) do
+ActiveRecord::Schema.define(:version => 20130619210814) do
 
   create_table "app_configs", :force => true do |t|
     t.string   "footer"
