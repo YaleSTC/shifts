36113b5bc60acd11b99cc4ce5107d032a2924cc7
Hugh <hkocinneide@gmail.com>
2013-06-27 15:49:41 -0400

    Fixed up the erb from the javascript files

--- public/javascripts/punch_clock_timer.js
+++ public/javascripts/punch_clock_timer.js
@@ -1,4 +1,4 @@
 $('#timer').epiclock({format: 'x:i:s', mode: EC_COUNTUP, 
-                      target: '<%= Time.now.to_s(:utc) %>', 
-                      offset:{seconds: <%= no_of_sec %>}}); 
+                      target: time, 
+                      offset:{seconds: sec}}); 
 jQuery.epiclock(EC_RUN)
