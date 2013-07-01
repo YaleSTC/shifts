91e566bc634aece88f7366a8f37c77d8cb8258b2
Tammer Abiyu <tammerabiyu@gmail.com>
2013-07-01 10:47:40 -0400

    Chamged mailers

--- test/unit/user_mailer_test.rb
+++ test/unit/user_mailer_test.rb
@@ -17,10 +17,23 @@ class UserMailerTest < ActionMailer::TestCase
   def admin_password_reset_instructions
   	user = users(:users_002)
 
-    email = UserMailer.password_reset_instructions(user).deliver
+    email = UserMailer.admin_password_reset_instructions(user).deliver
     assert !ActionMailer::Base.deliveries.empty?
 
     assert_equal [user.email], email.to
     assert_equal "Password Reset Instructions", email.subject
     assert_match("A request to reset your password has been made.", email.encoded)
+  end
+
+  def new_user_password_instructions
+  	user = users(:users_003)
+  	department = departments(:depatrment_001)
+
+	email = UserMailer.new_user_password_instructions(user).deliver
+    assert !ActionMailer::Base.deliveries.empty?
+
+    assert_equal [user.email], email.to
+    assert_equal "Password Reset Instructions", email.subject
+    assert_match("A request to reset your password has been made.", email.encoded)
+  end
 end
