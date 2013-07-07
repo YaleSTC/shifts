c7446aaafa443480a76b1e28768cda488bfb9bd1
Hugh <hkocinneide@gmail.com>
2013-07-07 02:43:29 -0400

    more bug squashing

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default :from => Yale@yale.edu
+  default :from => "Yale@yale.edu"
   
   def registration_confirmation(user)
     mail(:to => user.email, :subject => "Registered")
@@ -38,44 +38,50 @@ class UserMailer < ActionMailer::Base
 #  end
 
  def sub_taken_notification(sub_request, new_shift, dept)
-    @sub_request => sub_request, @new_shift => new_shift, @dept => dept
+    @sub_request = sub_request
+    @new_shift = new_shift
+    @dept = dept
     mail(:to => sub_request.shift.user.email, :from => dept.department_config.mailer_address, 
     	:subject => "[Sub Request] #{new_shift.user.name} took your sub!", :cc => new_shift.user.email, :date => Time.now)
   end
 
   def payform_item_modify_notification(new_payform_item, dept)
-  	@new_payform_item => new_payform_item, @dept => dept
+  	@new_payform_item = new_payform_item
+    @dept = dept
   	mail(:to => new_payform_item.user.email, :from => dept.department_config.mailer_address, 
   		:subject => "Your payform has been modified by #{new_payform_item.source}", :date => Time.now)
   end
 
   def payform_item_deletion_notification(old_payform_item, dept)
-    @old_payform_item => old_payform_item, @dept => dept
+    @old_payform_item = old_payform_item
+    @dept = dept
     mail(:to => old_payform_item.user.email, :from => dept.department_config.mailer_address, 
     	:subject=> "Your payform item has been deleted by #{old_payform_item.versions.last.user}", :date => Time.now)
   end
 
   def password_reset_instructions(user)
-    @edit_password_reset_url => edit_password_reset_url(user.perishable_token)
+    @edit_password_reset_url = edit_password_reset_url(user.perishable_token)
     mail(:to => user.email, :subject => "Password Reset Instructions",
     	:date => Time.now)
   end
 
   def admin_password_reset_instructions(user)
-    @edit_admin_password_reset_url => edit_password_reset_url(user.perishable_token)
+    @edit_admin_password_reset_url = edit_password_reset_url(user.perishable_token)
     mail(:to => user.email, :subject => "Password Reset Instructions",
     	:date => Time.now)
   end
 
 # For use when users are imported from csv #duplicate found in ar_mailer, not DRY -ben
   def new_user_password_instructions(user, dept)
-    @edit_new_user_password_url => edit_password_reset_url(user.perishable_token), @user => user, @dept => dept
+    @edit_new_user_password_url = edit_password_reset_url(user.perishable_token)
+    @user = user
+    @dept = dept
     mail(:to => user.email, :from => dept.department_config.mailer_address, 
     	:subject => "Password Creation Instructions", :date => Time.now)
   end
 
   def change_auth_type_password_reset_instructions(user)
-    @edit_password_url => edit_password_reset_url(user.perishable_token)
+    @edit_password_url = edit_password_reset_url(user.perishable_token)
     mail(:to => user.email, :subject => "Password Creation Instructions",
     	:date => Time.now)
   end
