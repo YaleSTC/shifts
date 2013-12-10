65bf7b6586c5a25f4ba278ebbb12bb3a32141134
Adam Bray <adam.bray@yale.edu>
2013-12-09 19:40:51 -0500

    removed mobile stylesheets so they aren't loaded with the asset pipeline

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,8 @@ Shifts::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  config.serve_static_assets = false
+
   # Do not compress assets
   config.assets.compress = false
 
