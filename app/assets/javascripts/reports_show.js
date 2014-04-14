3184c28e25456f0ca6cf2857d735bfefce5edda9
David Yao <njlxyaoxinwei@gmail.com>
2014-04-13 22:33:29 -0400

    Fix auto refreshing of report and correct update frequencies

--- app/assets/javascripts/reports_show.js
+++ app/assets/javascripts/reports_show.js
@@ -15,14 +15,23 @@ $(document).ready(function(){
 	});
 	
 	if ($('#update_notices').length) {
+		var freq1=$('#update_notices').data('frequency')*1000;
 		setInterval(function(){
 			$.get('/notices/update_message_center');
-		},240000);
-	}
+		},freq1);
+	};
 	if ($('#update_tasks').length) {
+		var freq2=$('#update_tasks').data('frequency')*1000;
 		setInterval(function(){
 			$.get('/tasks/update_tasks');
-		},300000);
-	}
+		},freq2);
+	};
+	if ($('#update_report').length) {
+		var freq3=$('#update_report').data('frequency')*1000;
+		setInterval(function(){
+			var id=$('#update_report').data('id');
+			$.get('/reports/'+id+'/update_reports');
+		}, freq3);
+	};
 
 });
