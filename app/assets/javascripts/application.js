733717653c93bb2a8f957e64d8128164e082cf15
David Yao <njlxyaoxinwei@gmail.com>
2014-05-02 22:46:10 -0400

    Fixed Application bar styling with old superfish css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -40,11 +40,10 @@ $(document).ready(function() {
         $(this).html("<a href='#'>"+$(this).text()+"</a>");
     });
 
-    $(document).ready(function(){
-        // $("ul.sf-menu").superfish({
-        //     animation: {height:'show'},   // slide-down effect without fade-in
-        //     delay:     1200               // 1.2 second delay on mouseout
-        // });
-    });
+    // Superfish does NOT work yet!
+    // $("ul.sf-menu").superfish({
+    //         animation: {height:'show'},   // slide-down effect without fade-in
+    //         delay:     1200               // 1.2 second delay on mouseout
+    // });
 
 });
