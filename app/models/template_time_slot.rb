04843c250770f85ec44fc846e0fb5aacefe82a8f
Jason Liu <jason@jasonkliu.com>
2014-05-15 13:12:59 -0700

    Fix on shift_preference/sub_request/template_time_slot

--- app/models/template_time_slot.rb
+++ app/models/template_time_slot.rb
@@ -7,7 +7,7 @@ class TemplateTimeSlot < ActiveRecord::Base
 	validate :no_concurrent_timeslots
 
 	def start_less_than_end
-    errors.add_to_base("Start time must be earlier than end time.") if (self.end_time <= start_time)
+    errors.add(:base, "Start time must be earlier than end time.") if (self.end_time <= start_time)
   end
 
   def no_concurrent_timeslots
@@ -16,6 +16,6 @@ class TemplateTimeSlot < ActiveRecord::Base
     # else
       # c = TimeSlot.count(:all, :conditions => ["#{:start } < #{self.end  } AND #{:end } > #{self.start  } AND #{:location_id } = #{self.location  } AND #{:calendar_id } = #{self.calendar  } #{dont_conflict_with_self}"])
     #end
-		errors.add_to_base("There is a conflicting timeslot at the #{self.location.short_name}.") unless c.zero?
+		errors.add(:base, "There is a conflicting timeslot at the #{self.location.short_name}.") unless c.zero?
   end
 end
