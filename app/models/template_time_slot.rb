038d7d2760ec25521ccc4ad318bf5bb4287be44b
Jason Liu <jason@jasonkliu.com>
2014-05-19 16:02:44 -0700

    Refactor template_time_slot.rb removing indents and making similar to time_slot.rb

--- app/models/template_time_slot.rb
+++ app/models/template_time_slot.rb
@@ -1,21 +1,26 @@
 class TemplateTimeSlot < ActiveRecord::Base
-	belongs_to :template
-	belongs_to :location
+  belongs_to :template
+  belongs_to :location
 
-	validates_presence_of :start_time, :end_time, :day, :location_id, :template_id
-	validate :start_less_than_end
-	validate :no_concurrent_timeslots
+  validates_presence_of :start_time, :end_time, :day, :location_id, :template_id
+  validate :start_less_than_end
+  validate :no_concurrent_timeslots
 
-	def start_less_than_end
-    errors.add(:base, "The start time should not be later than the end time.") if (self.end_time <= start_time)
+  def start_less_than_end
+    errors.add(:start, "The start time should not be later than the end time.") if (self.end_time <= start_time)
   end
 
   def no_concurrent_timeslots
+    # Count the number of TemplateTimeSlots which overlap with each other; log an error to base if there are any.
     dont_conflict_with_self = (self.new_record? ? "" : "AND id != #{self.id}")
-		c = TemplateTimeSlot.where("template_id = ? AND day = ? AND start_time < ? AND end_time  > ? AND location_id  = ? #{dont_conflict_with_self}", self.template_id, self.day, self.end_time, self.start_time, self.location).count
-    # else
-      # c = TimeSlot.count(:all, :conditions => ["#{:start } < #{self.end  } AND #{:end } > #{self.start  } AND #{:location_id } = #{self.location  } AND #{:calendar_id } = #{self.calendar  } #{dont_conflict_with_self}"])
-    #end
-		errors.add(:base, "There is a conflicting timeslot at the #{self.location.short_name}.") unless c.zero?
+
+    c = TemplateTimeSlot.
+        where("template_id = ? AND day = ? AND start_time < ? AND end_time > ? AND location_id = ? #{dont_conflict_with_self}",
+        self.template_id, self.day, self.end_time, self.start_time, self.location).count
+
+    unless c == 0
+      errors.add(:base, "There is a conflicting timeslot at the #{self.location.short_name}.")
+    end
+    # c = TimeSlot.count(:all,:conditions => ["#{:start } < #{self.end} AND #{:end} > #{self.start} AND #{:location_id} = #{self.location} AND #{:calendar_id} = #{self.calendar} #{dont_conflict_with_self}"])
   end
 end
