36b02e1397044c6425899e04c7e1b6c5e79bcc90
David Yao <njlxyaoxinwei@gmail.com>
2014-05-27 17:09:07 -0400

    Removed unused files and code

--- app/helpers/data_types_helper.rb
+++ app/helpers/data_types_helper.rb
@@ -1,10 +1,4 @@
-module DataTypesHelper
-  def add_data_field_link(name)
-    link_to_function name do |page|
-      page.insert_html :bottom, :data_fields, partial: 'data_field', object: DataField.new
-    end
-  end
-  
+module DataTypesHelper  
   def fields_for_data_field(data_field, &block)
     if data_field
       prefix = data_field.new_record? ? 'new' : 'existing'
