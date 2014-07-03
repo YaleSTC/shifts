ad389ff7fa95fe5c15fddc6587ee70eab7228759
Jason Liu <jason@jasonkliu.com>
2014-07-02 20:40:08 -0700

    Applied old patch: 393fcf5 Fix miscellaneous typos - tests now run!

--- spec/controllers/payform_items_controller_spec.rb
+++ spec/controllers/payform_items_controller_spec.rb
@@ -20,13 +20,13 @@ describe PayformItemsController do
   describe ", when making a Parent Payform Item," do
     before(:each) do
       @payform_item = PayformItem.create!(valid_payform_item_attributes)
-      @payform_item.edit (id: 1,
-                           user_id: 1,
-                           category_id: 1,
-                           hours: 2,
-                           date: "2009-5-23".to_date,
-                           description: "Fun Times in New Haven",
-                           reason: "I forgot how long it was")
+      @payform_item.edit(id: 1,
+                         user_id: 1,
+                         category_id: 1,
+                         hours: 2,
+                         date: "2009-5-23".to_date,
+                         description: "Fun Times in New Haven",
+                         reason: "I forgot how long it was")
     end
 
     it "the child should not be active if it has a parent"
