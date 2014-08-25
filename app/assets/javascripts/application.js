c948eee0f03069d9530099ed3e3e7ebbc1e27c58
Machiste Quintana <machiste.quintana@yale.edu>
2014-08-25 16:10:49 -0400

    remove the last of superfish hopefully

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,9 +36,4 @@ $(document).ready(function() {
         $(this).html("<a href='#'>"+$(this).text()+"</a>");
     });
 
-    $("ul.sf-menu").superfish({
-            animation: {height:'show'},   // slide-down effect without fade-in
-            delay:     1200               // 1.2 second delay on mouseout
-    });
-
 });
