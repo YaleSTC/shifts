5750afea80ed3da6621592ca0d1dea76ba99e453
caseywatts <casey.s.watts@gmail.com>
2014-09-25 13:53:46 -0400

    cleanup, abstract sign_in code, include some potentially useful examples

--- spec/features/time_slots_spec.rb
+++ spec/features/time_slots_spec.rb
@@ -4,7 +4,7 @@ require 'capybara/rails'
 describe "timeslot creation process", :type => :feature do
   before :each do
     load Rails.root + "db/seeds.rb" 
-    CASClient::Frameworks::Rails::Filter.fake("csw3") #spoof a CAS login
+    sign_in("csw3")
     # assume that it is Monday [specific date]
   end
 
@@ -20,12 +20,17 @@ describe "timeslot creation process", :type => :feature do
     expect(page).to have_content 'Success'
   end
 
-  xit "displays the timeslot properly" do
-  end
-end
+  xit "displays the timeslot properly on the time slots page" do
+    visit '/time_slots'
 
-def sign_in(netid)
-    RubyCAS::Fake('csw3')
+    #didn't test this at all, but it's a good first guess.
+    #this needs to be abstracted/future-proofed, of course
+    expect(find('#location1_2014-09-25_timeslots')).to have_css('#timeslot346')
+  end
+
+  xit "displays the timeslot properly on the shifts page" do
+    visit '/shifts'
+  end
 end
 
 # feature 'Visitor signs up' do
