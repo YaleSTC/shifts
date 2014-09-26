21854f4ab6b521d8e19cb0d338425257a7b4fab1
caseywatts <casey.s.watts@gmail.com>
2014-09-25 21:10:04 -0400

    putting back application_controller before_filters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_filter :load_app_config
   before_filter :department_chooser
   before_filter :load_user_session
-  # before_filter RubyCAS::Filter, if: Proc.new{|s| s.using_CAS?}, except: 'access_denied'
+  before_filter RubyCAS::Filter, if: Proc.new{|s| s.using_CAS?}, except: 'access_denied'
   before_filter :login_check, except: :access_denied
   before_filter :load_department
   before_filter :prepare_mail_url
