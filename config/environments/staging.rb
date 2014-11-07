a326ce2fdcf3bd2a1fe780a26aad43270a29b578
Simon Podhajsky <simon.podhajsky@gmail.com>
2014-11-07 15:27:05 -0500

    Inserted dummy value for `config.action_controller.relative_url_root`

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -4,4 +4,7 @@ Shifts::Application.configure do
   #config.serve_static_assets = true
   config.action_mailer.perform_deliveries = false
 
+  # Dummy value for deploy script - change as necessary
+  config.action_controller.relative_url_root = ""
+
 end
