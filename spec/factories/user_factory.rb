4f7c87b1269427784020dfbe6a470796cf8cd522
David Yao <njlxyaoxinwei@gmail.com>
2014-12-28 16:48:49 -0500

    added first_run spec

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
     sequence(:login) {|n| "hp#{n}"}
     sequence(:first_name) {|n| "Harry##{n}"}
     last_name "Potter"
-    nick_name ""
+    nick_name "Scarhead"
     email {"#{login}@hogwarts.edu"}
     sequence(:employee_id, 6000000) {|n| "#{n}"} 
     # CAS authentication only
@@ -16,6 +16,7 @@ FactoryGirl.define do
       login "ad12"
       first_name "Albus"
       last_name "Dumbledore"
+      nick_name "Bumblebee"
       superuser true
       # admin has admin_role
       before :create do |admin|
---
+++ spec/features/first_run_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+describe "First Run Application Configuration" do 
+	context "When using CAS authentication, no LDAP" do
+		before :all do 
+			@admin_attrs = attributes_for(:admin)
+			@dpmt_attrs = attributes_for(:department)
+			sign_in(@admin_attrs[:login])
+		end
+
+		it "shows the first run config page" do 
+			visit '/'
+			# AppConfig
+			within "#new_app_config" do
+				select "CAS", from: "app_config_auth_types"
+				uncheck "ldap_check_box"
+				fill_in "App email address", with: @admin_attrs[:email]
+				fill_in "Admin email address", with: @admin_attrs[:email]
+				fill_in "Footer Text", with: "<b>Bold App Footer</b><br><i>Italic App Footer 2</i>"
+			end
+			expect{click_button "Submit"}.to change{AppConfig.count}.from(0).to(1)
+			expect(find("#flash_notice")).to have_content("App Settings have been configured")
+			
+			# New Department
+			within '#new_department' do
+				fill_in "Name", with: @dpmt_attrs[:name]
+			end
+			expect{click_button "Submit"}.to change{Department.count}.from(0).to(1)
+			expect(find("#flash_notice")).to have_content("The first department was successfully created")
+
+			# Superuser
+			within '#first_user_form' do
+				fill_in "Login", with: @admin_attrs[:login]
+				fill_in "First name", with: @admin_attrs[:first_name]
+				fill_in "Nick name", with: @admin_attrs[:nick_name]
+				fill_in "Last name", with: @admin_attrs[:last_name]
+				fill_in "Email", with: @admin_attrs[:email]
+				fill_in "Employee ID", with: @admin_attrs[:employee_id]
+			end
+			expect{click_button "Submit"}.to change{User.count}.from(0).to(1)
+			expect(find("#flash_notice")).to have_content("Successfully set up application")
+		end
+
+	end
+end
