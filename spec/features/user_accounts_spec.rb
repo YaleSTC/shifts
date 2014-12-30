c261540377c6e41ab1c4729f2a7d6d15f03ae4c8
David Yao <njlxyaoxinwei@gmail.com>
2014-12-30 09:55:03 -0500

    added superuser tests

--- spec/features/user_accounts_spec.rb
+++ spec/features/user_accounts_spec.rb
@@ -37,8 +37,8 @@ describe "User accounts", :user do
 			expect(find('#user_list')).to have_css('tbody tr', count: 1)
 		end
 
-		# Strangely this test works only with selenium. WebKit cannot wait until the DB transaction happens, and Rack does not even wait till the page redirects.
-		it "can update another user", driver: :selenium do
+		# Strangely this test works only with Selenium or WebKit, Rack::Test does not even wait till the page redirectst
+		it "can update another user", driver: :webkit do
 			click_on @user.login
 			new_user = attributes_for(:user)
 			#pp @new_user
@@ -46,8 +46,9 @@ describe "User accounts", :user do
 				fill_in_user_form(new_user)
 				fill_in "Payrate", with: 13				
 			end
-			expect{click_button "Update User"}.to change{@user.reload.login}
+			click_button "Update User"
 			expect_flash_notice "Successfully updated user"
+			expect{@user.reload}.to change{@user.login}
 		end
 
 		it "can deactivate a user", js: true do
@@ -63,6 +64,33 @@ describe "User accounts", :user do
 			find("#user#{@user.id}").click_on "Restore"
 			expect(find("#user#{@user.id}")).to have_content("Deactivate")
 			expect(@department.active_users).to include(@user.reload)
+		end
+	end
+
+	context "When superuser is using Shifts" do
+		before :each do
+			sign_in(@superuser.login)
+			visit superusers_path
+		end
+
+		it "can view superusers" do
+			expect(page).to have_css("td", text: @superuser.login) 
+		end
+
+		it "can add new superuser" do 
+			fill_in "new_su_login", with: @admin.login
+			click_button "Add"
+			expect_flash_notice "#{@admin.name} is now a superuser"
+			expect(@admin.reload.superuser).to be_truthy
+		end
+
+		it "can remove superuser" do
+			@admin.update_attribute(:superuser, true)
+			visit current_path
+			check "remove_su_ids_#{@admin.id}"
+			click_button "Remove selected"
+			expect_flash_notice "Removed #{@admin.name} from the list of superusers"
+			expect(@admin.reload.superuser).not_to be_truthy
 		end
 	end
 end
