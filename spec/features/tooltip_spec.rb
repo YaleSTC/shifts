a3faeba7bd7f93abbdc1233df29243bb8a4e5f76
David Yao <njlxyaoxinwei@gmail.com>
2015-01-07 16:20:39 -0500

    refactored time_slot_row and shift_schedule_row

--- spec/features/tooltip_spec.rb
+++ spec/features/tooltip_spec.rb
@@ -12,7 +12,7 @@ describe "Tooltip", js: true do
         context "New TimeSlot tooltip" do 
             it 'can view the timeslot creation page on tooltip' do
                 visit '/time_slots'
-                time_slot_row(@location2.id).find('.click_to_add_new_timeslot').click
+                time_slot_row(@location2.id, @a_local_time).find('.click_to_add_new_timeslot').click
                 expect(page).to have_content "Add New Time Slot"
                 expect(page).to have_select("Location", selected: @location2.name)
             end
@@ -22,14 +22,14 @@ describe "Tooltip", js: true do
     		@slot = create(:time_slot, location: @location, calendar: @calendar)
         	visit '/time_slots'
         	# find created timeslot
-        	time_slot_row(@slot.location.id).first('.click_to_edit_timeslot').click
+        	time_slot_row(@slot.location.id, @a_local_time).first('.click_to_edit_timeslot').click
         	expect(page).to have_content 'Edit Timeslot'
             select "11 AM", from: "time_slot_start_time_4i"
             select "00", from: "time_slot_start_time_5i"
             click_on "Save Changes"
             expect(page).not_to have_selector("#tooltip") #Wait till tooltip closes
             expect{@slot.reload}.to change(@slot, :start)
-            expect_time_slot_to_display_properly(@slot, time_slot_row(@slot.location.id).first('li.timeslot'))
+            expect_time_slot_to_display_properly(@slot, time_slot_row(@slot.location.id, @a_local_time).first('li.timeslot'))
       	end
 	end
 end
--- spec/support/shift_helper.rb
+++ spec/support/shift_helper.rb
@@ -1,6 +1,6 @@
 module ShiftHelper
   # Capybara page finder helpers, does not modify browser state
-  def shift_schedule_row(location_id)
-    page.all("#location#{location_id}_#{@a_local_time.strftime("%Y-%m-%d")}_events")[0]
+  def shift_schedule_row(location_id, date)
+    page.first("#location#{location_id}_#{date.strftime("%Y-%m-%d")}_events")
   end
 end
