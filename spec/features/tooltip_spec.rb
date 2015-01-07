e2234edb81213913f188e10be1fb726efc4650b3
David Yao <njlxyaoxinwei@gmail.com>
2015-01-07 16:46:35 -0500

    added tooltip tests for timeslots

--- spec/features/tooltip_spec.rb
+++ spec/features/tooltip_spec.rb
@@ -10,11 +10,31 @@ describe "Tooltip", js: true do
 		before(:each) {sign_in(@admin.login)}
 
         context "New TimeSlot tooltip" do 
+            def show_new_tooltip
+                visit time_slots_path
+                time_slot_row(@location2.id, @a_local_time+1.day).find('.click_to_add_new_timeslot').click
+            end
+
             it 'can view the timeslot creation page on tooltip' do
-                visit '/time_slots'
-                time_slot_row(@location2.id, @a_local_time).find('.click_to_add_new_timeslot').click
+                show_new_tooltip
                 expect(page).to have_content "Add New Time Slot"
                 expect(page).to have_select("Location", selected: @location2.name)
+            end
+            it 'can create one-time timeslot on tooltip' do 
+                show_new_tooltip
+                within("#new_time_slot") do
+                    fill_in_date("time_slot_start_date", @a_local_time+1.day)
+                    select "10 AM", :from => "time_slot_start_time_4i"
+                    select "5 PM", :from => "time_slot_end_time_4i"
+                end
+                click_on "Create New"
+                expect(time_slot_row(@location2.id, @a_local_time+1.day)).to have_selector('li.timeslot')
+                expect(TimeSlot.count).to eq(1)
+            end
+            it 'can close tooltip' do 
+                show_new_tooltip
+                click_on "[esc]"
+                expect(page).not_to have_selector('#tooltip')
             end
         end
 
