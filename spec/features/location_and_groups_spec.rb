bd14bb9e7c9bf2fa3874ef7eda024a22d84920cf
David Yao <njlxyaoxinwei@gmail.com>
2015-01-08 17:10:03 -0500

    added location group tests

--- spec/features/location_and_groups_spec.rb
+++ spec/features/location_and_groups_spec.rb
@@ -3,16 +3,54 @@ require 'rails_helper'
 describe "Location Groups" do 
 	before :each do 
 		full_setup
+		@location2 = create(:location, loc_group: @loc_group)
 		sign_in(@admin.login)
 	end
 
-	it "can create new location group"
+	it "can create new location group" do 
+		visit department_loc_groups_path(@department)
+		within '#new_loc_group' do 
+			fill_in "Name", with: "New Location Group"
+			uncheck "Public"
+			click_on "Create"
+		end
+		expect_flash_notice "Successfully created location group"
+		expect(LocGroup.count).to eq(2)
+	end
 
 	context "For existing location group" do 
-		it "can update location group"
-		it "can deactivate location group"
-		it "can re-activate location group"
-		it "can delete location group"
+		it "can update location group" do 
+			visit edit_loc_group_path(@loc_group)
+			uncheck "Public"
+			click_on "Update"
+			expect_flash_notice "Successfully updated Location group"
+			expect{@loc_group.reload}.to change(@loc_group, :public)
+		end
+		it "can deactivate location group", js: true do 
+			visit department_loc_groups_path(@department)
+			find("tr#loc_group#{@loc_group.id}").click_on "Deactivate"
+			expect(find("tr#loc_group#{@loc_group.id}")).to have_content("Restore")
+			expect(@loc_group.reload.active).to be false
+			@loc_group.locations.each do |loc|
+				expect(loc.active).to be false
+			end
+		end
+		it "can re-activate location group", js: true do 
+			@loc_group.deactivate
+			visit department_loc_groups_path(@department)
+			find("tr#loc_group#{@loc_group.id}").click_on "Restore"
+			expect(find("tr#loc_group#{@loc_group.id}")).to have_content("Deactivate")
+			expect(@loc_group.reload.active).to be true
+			@loc_group.locations.each do |loc|
+				expect(loc.active).to be true
+			end			
+		end
+		it "can delete location group" do 
+			visit loc_group_path(@loc_group)
+			all('a', text: "Destroy").last.click
+			expect_flash_notice "Successfully destroyed Location group"
+			expect{@loc_group.reload}.to raise_error(ActiveRecord::RecordNotFound)
+		end
 	end
-	
+
 end
