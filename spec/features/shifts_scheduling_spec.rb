1acb219098aa7f441185c26b29963748abf79b9b
David Yao <david.yao@yale.edu>
2015-03-10 21:45:56 -0400

    more tests

--- spec/features/shifts_scheduling_spec.rb
+++ spec/features/shifts_scheduling_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 # Refer to the 'How to Schedule Shifts' google drive doc to learn
 # about the shifts scheduling process in more detail. 
 
-describe "Shifts scheduling", :shifts_scheduling, js: true do
+describe "Shifts scheduling", :shifts_scheduling, :time_slot do
 	start_date = Date.new(2014, 9, 7) # aka 'go live' date
 	end_date = Date.new(2015, 9, 7)
 
@@ -12,7 +12,7 @@ describe "Shifts scheduling", :shifts_scheduling, js: true do
 		sign_in(@admin.login)
 	end
 
-	it "can schedule shifts" do
+	it "can schedule shifts", driver: :selenium do
 
 		## Part 1. Prepare request calendar
 
@@ -23,9 +23,22 @@ describe "Shifts scheduling", :shifts_scheduling, js: true do
 		
 		# Advance in request calendar to week after start date
 		click_link(calendar_name)
-		next_week = start_date + 7
+		next_week = start_date + 7.days
 		formatted_date = "#{next_week.year}-#{next_week.mon}-#{next_week.day}"
-		visit current_path + "?date=" + formatted_date
+		calendar_path_with_date = current_path + "?date=" + formatted_date
+		visit calendar_path_with_date
+
+		# Create time slots for the week
+		time_slot_row(@location.id, next_week).click		
+		fill_repeating_time_slot_form(next_week, next_week+6.days, [@location], calendar_name)
+		expect_flash_notice "Successfully created repeating event"
+
+		## Part 2. Get requests from workers
+		sign_in(@user.login)
+		visit calendar_path_with_date
+				
 		save_and_open_page
+
+
 	end
 end
--- spec/support/shifts_scheduling_helper.rb
+++ spec/support/shifts_scheduling_helper.rb
@@ -10,4 +10,21 @@ module ShiftsSchedulingHelper
 	    end
 	    click_button 'Submit'
 	end
+
+	def fill_repeating_time_slot_form(start_date, end_date, locs, calendar_name)
+		check "Repeating event?"
+		fill_in_date("repeating_event_start_date", start_date)
+		fill_in_date("repeating_event_end_date", end_date)
+		select "10 AM", from: "repeating_event_start_time_4i"
+		select "05 PM", from: "repeating_event_end_time_4i"
+		locs.each do |loc|
+			check loc.short_name
+		end
+		select calendar_name, from: "Calendar"
+		Date::DAYNAMES.each do |dayname|
+			check dayname
+		end
+		uncheck "Wipe conflicts?"
+		click_button "Create New Repeating Event"
+	end
 end
