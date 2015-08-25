55d7921d7cd3034135b794d51361313db6850843
Simon Podhajsky <simon.podhajsky@gmail.com>
2015-08-25 16:16:39 -0400

    Indentation fixes for the #472 fix

--- spec/features/csv_import_spec.rb
+++ spec/features/csv_import_spec.rb
@@ -6,17 +6,17 @@ describe "CSV import" do
     sign_in(@admin.login)
   end
 
-	it 'should import users in correct format' do
-		# CSV file has two users with valid roles, one with invalid role.
-		# Expected behavior: import two, fail to import the third, display errors.
-		visit users_path
-		click_link('Import from CSV')
-		attach_file('file', Rails.root + 'spec/fixtures/user_import.csv')
-		expect {
-			click_button 'Upload'
-		}.to change {
-			User.count
-		}.by(2)
-		expect(page).to have_content('Invalid role', count: 1) # exactly one error
-	end
+  it 'should import users in correct format' do
+    # CSV file has two users with valid roles, one with invalid role.
+    # Expected behavior: import two, fail to import the third, display errors.
+    visit users_path
+    click_link('Import from CSV')
+    attach_file('file', Rails.root + 'spec/fixtures/user_import.csv')
+    expect {
+      click_button 'Upload'
+    }.to change {
+      User.count
+    }.by(2)
+    expect(page).to have_content('Invalid role', count: 1) # exactly one error
+  end
 end
